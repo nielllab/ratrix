39e08c40549de801012c6b75e25c66d00dc1c532
erik <erik@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2008-07-21 08:27:58 +0000

    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@1338 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- classes/util/matlaby/fastBin2Dec.m
+++ classes/util/matlaby/fastBin2Dec.m
@@ -1,5 +1,5 @@
 function x=fastBin2Dec(s)
-erChk=false
+erChk=false;
 if erChk %this actually takes significant time, may prefer to trust input
     if ~ischar(s) || ~ isvector(s) || any(s~='0' & s~='1') || length(s)>52 %matlab's bin2dec doesn't want more than 52, but why not floor(log2(realmax))?  maybe cuz eps(realmax) is too sparse?
         error('must be character vector with only ''0'' and ''1'' with 52 or fewer elements');

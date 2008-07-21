aa2dc6712a732602015ce83e4f10ebd5e0477834
philip <philip@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2008-07-21 18:48:43 +0000

    syncing
    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@1351 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- setup/setupPMM/setupPMM.m
+++ setup/setupPMM/setupPMM.m
@@ -25,16 +25,18 @@ else
 end
 
 %%
-testSubjects={'l','r','s','t','u','v'};
 
-testSubjects={'t','u','v'};
+testSubjects={'t'}
+%testSubjects={'t','u','v'};
+r=setShapingPMM(r,{'273','274'}, 'goToLeftDetection', '2_2')
+
 % r=addTestRats(testSubjects,auth) %maybe defined in oracle db
-% r=setShapingPMM(r,testSubjects, 'tiltDiscrim', '1_5');
 
 
-
-
+testSubjects={'l','r','s','t','u','v'};
 r=setShapingPMM(r,testSubjects, 'goToSide', '1_0');
+
+% r=setShapingPMM(r,testSubjects, 'tiltDiscrim', '1_5');
 %r=setShapingPMM(r,{'213'}, 'goToLeftDetection', '1_1');
 %  r=setCoherentDotSteps(r,{'195'});
 %  r=setAcuity(r,{'117'},'tiltDiscrim','1_0');
@@ -45,26 +47,26 @@ r=setShapingPMM(r,testSubjects, 'goToSide', '1_0');
 
 if 1
 
-     r=setShapingPMM(r,{'144'}, 'tiltDiscrim', '2_0');
-
+    
      r=setShapingPMM(r,{'117'},'tiltDiscrim','2_0'); 
      r=setShapingPMM(r,{'138','139'}, 'goToRightDetection', '1_9'); 
-     r=setShapingPMM(r,{'135'}, 'goToSide','1_3'); 
+     
      r=setShapingPMM(r,{'136','137','131'}, 'goToSide', '1_7');
      r=setCoherentDotSteps(r,{'195','196'});
      
      r=setShapingPMM(r,{'130'}, 'tiltDiscrim', '2_0');
-     r=setShapingPMM(r,{'102','127'}, 'goToRightDetection', '1_3'); 
+     r=setShapingPMM(r,{'102'}, 'goToRightDetection', '1_3');  %127 head fixed
      r=setShapingPMM(r,{'227','228','231','232'}, 'goToLeftDetection', '1_9'); %removed '235','236','214','219' 
-     r=setShapingPMM(r,{'229','230','233','234','237','238'}, 'goToRightDetection', '1_9');
+     r=setShapingPMM(r,{'229','230','233','234','237'}, 'goToRightDetection', '1_9'); %removed '238'
      
      r=setShapingPMM(r,{'271','272'}, 'goToLeftDetection', '2_1');
      r=setShapingPMM(r,{'275','277'}, 'goToRightDetection', '2_1');
      
      r=setShapingPMM(r,{'273','274'}, 'goToLeftDetection', '2_2');
      r=setShapingPMM(r,{'276','278'}, 'goToRightDetection', '2_2');
-
-
+     
+%     r=setShapingPMM(r,{'135'}, 'goToSide','1_3'); % failed july 21,2008
+%     r=setShapingPMM(r,{'144'}, 'tiltDiscrim', '2_0');  % failed july 21,2008
 %     r=setShapingPMM(r,{'213','215'}, 'goToLeftDetection', '1_1'); r=setShapingPMM(r,{'216'}, 'goToRightDetection', '1_1');  r=setCoherentDotSteps(r,{'195'}); r=setShapingPMM(r,{'144'}, 'tiltDiscrim', '1_1');
 %     r=setShapingPMM(r,{'214','219','217'}, 'goToLeftDetection', '1_1'); r=setShapingPMM(r,{'218','220'}, 'goToRightDetection', '1_1');  %r=setShapingPMM(r,{'145','146','147','148','129'}, 'tiltDiscrim', '1_2')
 %     r=setShapingPMM(r,{'221'}, 'goToLeftDetection', '1_1'); r=setShapingPMM(r,{'222'}, 'goToRightDetection', '1_1'); r=setShapingPMM(r,{'106','102'}, 'goToRightDetection', '1_3'); r=setCoherentDotSteps(r,{'196'}); %r=setShapingPMM(r,{'112','126','102','106'}, 'goToRightDetection', '1_3');

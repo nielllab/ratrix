0d9a2dcc4d83b115da4a73cb644bc44eee3a90a4
erik <erik@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2008-07-23 16:16:02 +0000

    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@1364 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- setup/setupEDF/setProtocolEDF.m
+++ setup/setupEDF/setProtocolEDF.m
@@ -1,106 +1,59 @@
-function r= setProtocolEDF(r,subIDs)
+function r = setProtocolEDF(r,subjIDs)
 
-rewardSizeULorMS        =50;
-msPenalty               =500;
-fractionOpenTimeSoundIsOn = 1.0;
-fractionPenaltySoundIsOn = 1.0;
-scalar = 1.0;
-rm=constantReinforcement(rewardSizeULorMS,msPenalty,fractionOpenTimeSoundIsOn,fractionPenaltySoundIsOn,scalar);
-
-msFlushDuration         =1000;
-msMinimumPokeDuration   =10;
-msMinimumClearDuration  =10;
-
-sm=soundManager({soundClip('correctSound','allOctaves',[400],20000), ...
-    soundClip('keepGoingSound','allOctaves',[300],20000), ...
-    soundClip('trySomethingElseSound','gaussianWhiteNoise'), ...
-    soundClip('wrongSound','tritones',[300 400],20000)});
-
-freeDrinkLikelihood=0.1;
-fd1 = freeDrinks(msFlushDuration,msMinimumPokeDuration,msMinimumClearDuration,sm,freeDrinkLikelihood, rm);
-freeDrinkLikelihood=0;
-fd2 = freeDrinks(msFlushDuration,msMinimumPokeDuration,msMinimumClearDuration,sm,freeDrinkLikelihood, rm);
-
-requestRewardSizeULorMS=0;
-percentCorrectionTrials=.5;
-msResponseTimeLimit=0;
-pokeToRequestStim=true;
-maintainPokeToMaintainStim=true;
-msMaximumStimPresentationDuration=0;
-maximumNumberStimPresentations=0;
-doMask=false;
-
-rewardSizeULorMS        =200;
-msRewardSoundDuration   =rewardSizeULorMS;
-
-vh=nAFC(msFlushDuration,msMinimumPokeDuration,msMinimumClearDuration,sm,requestRewardSizeULorMS,...
-    percentCorrectionTrials,msResponseTimeLimit,pokeToRequestStim,maintainPokeToMaintainStim,msMaximumStimPresentationDuration,...
-    maximumNumberStimPresentations,doMask,rm);
-
-pixPerCycs              =[20];
-targetOrientations      =[pi/2];
-distractorOrientations  =[];
-mean                    =.5;
-radius                  =.04;
-contrast                =1;
-thresh                  =.00005;
-yPosPct                 =.65;
-maxWidth                =800;
-maxHeight               =600;
-scaleFactor             =[1 1];
-interTrialLuminance     =.5;
-freeStim = orientedGabors(pixPerCycs,targetOrientations,distractorOrientations,mean,radius,contrast,thresh,yPosPct,maxWidth,maxHeight,scaleFactor,interTrialLuminance);
-
-pixPerCycs=[40];
-distractorOrientations=[];
-radius=.12;
-discrimStim = orientedGabors(pixPerCycs,targetOrientations,distractorOrientations,mean,radius,contrast,thresh,yPosPct,maxWidth,maxHeight,scaleFactor,interTrialLuminance)
-
-imageDir='\\Reinagel-lab.ad.ucsd.edu\rlab\Rodent-Data\PriyaV\other stimuli sets\paintbrushMORPHflashlightEDF';
-background=0;
-ypos=0;
-ims=dir(fullfile(imageDir,'*.png'));
-trialDistribution={};
-for i=1:floor(length(ims)/2)
-    [junk n1 junk junk]=fileparts(ims(i).name);
-    [junk n2 junk junk]=fileparts(ims(length(ims)-(i+1)).name);
-    trialDistribution{end+1}={{n1 n2} 1};
+if ~isa(r,'ratrix')
+    error('need a ratrix')
 end
-imageStim = images(imageDir,ypos,background,maxWidth,maxHeight,scaleFactor,interTrialLuminance,trialDistribution);
 
-ts1 = trainingStep(fd1, freeStim, repeatIndefinitely(), noTimeOff());
-ts2 = trainingStep(fd2, freeStim, repeatIndefinitely(), noTimeOff());
-ts3 = trainingStep(vh, discrimStim, repeatIndefinitely(), noTimeOff());
-ts4 = trainingStep(vh, imageStim, repeatIndefinitely(), noTimeOff());
-ts5 = trainingStep(fd1, imageStim, repeatIndefinitely(), noTimeOff());
+if ~exist('subjIDs','var') || isempty(subjIDs)
+    subjIDs=getEDFids;
+end
 
-p=protocol('free drinks',{ts5});
+if ~all(ismember(subjIDs,getSubjectIDs(r)))
+    error('not all those subject IDs are in that ratrix')
+end
 
-if isvector(subIDs) && ischar(subIDs) && strcmp(subIDs, 'all')
-    rackID=getRackIDFromIP;
-
-    ids={};
-    conn=dbConn;
-    heats=getHeats(conn);
-    for i=1:length(heats)
-        if strcmp(heats{i}.name,'Test')
-            assignments=getAssignments(conn,rackID,heats{i}.name);
-            for j=1:length(assignments)
-                ids{end+1}=assignments{j}.subject_id;
-                ids{end}
-            end
+rackNum=getRackIDFromIP;
+asgns=[];
+conn=dbConn;
+heats=getHeats(conn);
+for i=1:length(heats)
+    assignments=getAssignments(conn,rackNum,heats{i}.name);
+    for j=1:length(assignments)
+        if isempty(asgns)
+            asgns=assignments{j};
+        else
+            asgns(end+1)=assignments{j};
         end
     end
-    closeConn(conn);
-    subIDs=ids;
 end
+closeConn(conn);
 
-if isvector(subIDs) && iscell(subIDs)
-    for i=1:length(subIDs)
-        ratID=subIDs{i};
-        s = getSubjectFromID(r,ratID);
-        [s r]=setProtocolAndStep(s,p,1,0,1,1,r,'first try','edf');
+asgns=asgns(ismember({asgns.owner},{'eflister' 'bsriram' 'pmeier'})); %162, 164 curently pmeier, 173-176 bsriram
+
+for i=1:length(subIDs)
+    ind=find(strcmp(subIDs{i},{asgns.subject_id}));
+    if length(ind)==1 && ismember(asgns(ind).subject_id,getEDFids)
+        switch asgns(ind).experiment
+            case {'goToSide' 'Flicker' 'Rel Cue' 'Rel Block' 'Box Transfer' 'Tube Transfer' 'Tube Physiology'}
+                p=makeProtocol(asgns(ind).experiment,false);
+                stepNum=1;
+            case {'Testing'}
+                p=makeProtocol(asgns(ind).experiment,true);
+                stepNum=1;   
+            case 'Cross Modal'
+                error('cross modal not built yet')
+            otherwise
+                subIDs{i}
+                asgns(ind).experiment
+                error('didn''t recognize experiment for subject')
+        end
+    else
+        subIDs{i}
+        ind
+        error('didn''t find unique subject id in db assignments for this rack or edf rat ids')
     end
-else
-    error('bad id format')
+
+    subj=getSubjectFromID(r,subjIDs{i});
+    [subj r]=setProtocolAndStep(subj,p,true,false,true,stepNum,r,'call to setProtocolEDF','edf');
+    clear('p','stepNum')
 end

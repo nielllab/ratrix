880b9eca5d3754e7631f678a62096090d45faba6
dan <dan@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2008-07-24 07:24:41 +0000

    added 'bs' as possible auth (needed?)
    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@1397 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- classes/@ratrix/authorCheck.m
+++ classes/@ratrix/authorCheck.m
@@ -1,7 +1,7 @@
 function out=authorCheck(r,author) %stupid that all functions in an object directory have to be methods (we don't need ratrix for this)
-    if strcmp(author,'edf') || strcmp(author,'pmm') || strcmp(author,'ear') || strcmp(author,'hvo') || strcmp(author,'pr') || strcmp(author,'dfp') || strcmp(author,'ratrix')
+    if strcmp(author,'edf') || strcmp(author,'pmm') || strcmp(author,'ear') || strcmp(author,'hvo') || strcmp(author,'pr') || strcmp(author,'dfp') || strcmp(author,'ratrix') || strcmp(author,'bs')
         out=1;
     else
         out=0;
-        warning('author must be one of edf, pmm, ear, hvo, pr, dfp')
+        warning('author must be one of edf, pmm, ear, hvo, pr, dfp, bs')
     end

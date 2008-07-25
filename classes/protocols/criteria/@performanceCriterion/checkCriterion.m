10fe8035291c5cb995d9b9aa9aa35a26ec1bc94c
dan <dan@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2008-07-25 11:03:47 +0000

    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@1414 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- classes/protocols/criteria/@performanceCriterion/checkCriterion.m
+++ classes/protocols/criteria/@performanceCriterion/checkCriterion.m
@@ -1,9 +1,36 @@
 function [graduate details] = checkCriterion(c,subject,trainingStep,trialRecords)
 
+fieldNames = fields(trialRecords);
+
+% Old version is guaranteed to have this information. Do I check for this
+% too?
 trialsThisStep=[trialRecords.trainingStepNum]==trialRecords(end).trainingStepNum;
-stochastic = [trialRecords.didStochasticResponse];
-humanResponse = [trialRecords.didHumanResponse];
-forcedRewards = [trialRecords.containedForcedRewards]==1;
+
+
+forcedRewards = 0;
+stochastic = 0;
+humanResponse = 0;
+warnStatus = false;
+% looks ugly
+if any(ismember(fieldNames,{'containedForcedRewards'}))
+    forcedRewards = [trialRecords.containedForcedRewards]==1;
+else 
+    warnStatus = warnStatus | true;
+end
+if any(ismember(fieldNames,{'didStochasticResponse'}))
+    stochastic = [trialRecords.didStochasticResponse];
+else 
+    warnStatus = warnStatus | true;
+end
+if any(ismember(fieldNames,{'didHumanResponse'}))
+    humanResponse = [trialRecords.didHumanResponse];
+else 
+    warnStatus = warnStatus | true;
+end
+
+if warnStatus
+    warning('checkCriterion found trialRecords of the older format. some necessary fields are missing and have been assigned arbitrarily.');
+end
 
 which= trialsThisStep & ~stochastic & ~humanResponse & ~forcedRewards;
 [graduate whichCriteria correct]=aboveThresholdPerformance(c.consecutiveTrials,c.pctCorrect,trialRecords(which));

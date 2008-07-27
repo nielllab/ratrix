9ac5cbef4de7957fe56367f8608b74ee9921f81f
erik <erik@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2008-07-27 10:34:02 +0000

    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@1421 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- classes/@station/setPuff.m
+++ classes/@station/setPuff.m
@@ -1,11 +1,15 @@
 function setPuff(s,state )
-if ~isempty(s.eyePuffPins)
-    if islogical(state)
-        state(s.eyePuffPins.invs)=~state(s.eyePuffPins.invs);
-        lptWriteBits(s.eyePuffPins.decAddr,s.eyePuffPins.bitLocs,state);
+if strcmp(s.responseMethod,'parallelPort')
+    if ~isempty(s.eyePuffPins)
+        if islogical(state)
+            state(s.eyePuffPins.invs)=~state(s.eyePuffPins.invs);
+            lptWriteBits(s.eyePuffPins.decAddr,s.eyePuffPins.bitLocs,state);
+        else
+            error('state must be logical');
+        end
     else
-        error('state must be logical');
+        warning('no airpuff on this station')
     end
 else
-    warning('no airpuff on this station')
+    warning('can''t set puff without parallel port')
 end

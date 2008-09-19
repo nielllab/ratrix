40a13f75806033826d9fee1547d2f61fb9d50270
rlab <rlab@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2008-09-19 03:41:36 +0000

    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@1537 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- analysis/eflister/getCompiledDirForServer.m
+++ analysis/eflister/getCompiledDirForServer.m
@@ -1,14 +1,14 @@
 function out=getCompiledDirForServer(server_name)
 switch server_name
-    case 'rack2' %rack2 server (old)
+    case 'server2F-pmm' %rack2 server (old)
         out='\\Reinagel-lab.ad.ucsd.edu\rlab\Rodent-Data\behavior\pmeierTrialRecords\compiled';
-    case 'rack1' %rack1 server (pmm)
+    case 'server1M-pmm' %rack1 server (pmm)
         out='\\Reinagel-lab.ad.ucsd.edu\rlab\Rodent-Data\ratrixAdmin\compiledRecords\';
-    case 'rack3-edf' %rack3-edf
+    case 'server4F-edf' %rack3-edf
         out='\\Reinagel-lab.ad.ucsd.edu\rlab\Rodent-Data\ratrixAdmin\rack3\compiledTrialRecords\';
     case 'test' %rig1 eyetracking computer on right, 
         out='\\Reinagel-lab.ad.ucsd.edu\rlab\Rodent-Data\behavior\rig1TrialRecords\compiled';
-    case 'rack3-pr' %rack3-pr
+    case 'server3F-pr' %rack3-pr
         out='\\Reinagel-lab.ad.ucsd.edu\rlab\Rodent-Data\ratrixAdmin\rack3\compiledTrialRecords\';
     otherwise
         error('bad')

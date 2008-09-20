9be828c1f8230044fe280464472d4a3107a7f128
rlab <rlab@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2008-09-20 06:30:38 +0000

    fixed server names
    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@1548 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- analysis/eflister/getCompiledDirForServer.m
+++ analysis/eflister/getCompiledDirForServer.m
@@ -1,14 +1,14 @@
 function out=getCompiledDirForServer(server_name)
 switch server_name
-    case 'server2F-pmm' %rack2 server (old)
+    case 'server-02-female-pmm-156' %rack2 server (old)
         out='\\Reinagel-lab.ad.ucsd.edu\rlab\Rodent-Data\behavior\pmeierTrialRecords\compiled';
-    case 'server1M-pmm' %rack1 server (pmm)
+    case 'server-01-male-pmm-154' %rack1 server (pmm)
         out='\\Reinagel-lab.ad.ucsd.edu\rlab\Rodent-Data\ratrixAdmin\compiledRecords\';
-    case 'server4F-edf' %rack3-edf
+    case 'server-03-female-edf-157' %rack3-edf
         out='\\Reinagel-lab.ad.ucsd.edu\rlab\Rodent-Data\ratrixAdmin\rack3\compiledTrialRecords\';
     case 'test' %rig1 eyetracking computer on right, 
         out='\\Reinagel-lab.ad.ucsd.edu\rlab\Rodent-Data\behavior\rig1TrialRecords\compiled';
-    case 'server3F-pr' %rack3-pr
+    case 'server-04-female-pr-155' %rack3-pr
         out='\\Reinagel-lab.ad.ucsd.edu\rlab\Rodent-Data\ratrixAdmin\rack3\compiledTrialRecords\';
     otherwise
         error('bad')
--- analysis/eflister/getSubDirForServer.m
+++ analysis/eflister/getSubDirForServer.m
@@ -2,15 +2,15 @@ function out=getSubDirForServer(server_name)
 
 
 switch server_name
-    case 'server2F-pmm' %rack2 server (old)
+    case 'server-02-female-pmm-156' %rack2 server (old)
         out='\\Reinagel-lab.ad.ucsd.edu\rlab\Rodent-Data\behavior\pmeierTrialRecords\subjects\';
-    case 'server1M-pmm' %rack1 server (pmm)
+    case 'server-01-male-pmm-154' %rack1 server (pmm)
         out='\\Reinagel-lab.ad.ucsd.edu\rlab\Rodent-Data\ratrixAdmin\subjects\';
-    case 'server4F-edf' %rack3-edf
+    case 'server-03-female-edf-157' %rack3-edf
         out='\\Reinagel-lab.ad.ucsd.edu\rlab\Rodent-Data\ratrixAdmin\rack3\subjects\';
     case 'test' %rig1 eyetracking computer on right, 
         out='\\Reinagel-lab.ad.ucsd.edu\rlab\Rodent-Data\behavior\rig1TrialRecords\subjects\';
-    case 'server3F-pr' %rack3-pr
+    case 'server-04-female-pr-155' %rack3-pr
         out='\\Reinagel-lab.ad.ucsd.edu\rlab\Rodent-Data\ratrixAdmin\rack3\subjects\';
     otherwise
         error('bad')

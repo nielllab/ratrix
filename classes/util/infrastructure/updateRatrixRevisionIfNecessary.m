87b753a8ceb47b6cc17659c3b51f923732921fae
rlab <rlab@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2008-10-15 19:57:18 +0000

    only update on stations if affected by revision
    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@1604 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- classes/util/infrastructure/updateRatrixRevisionIfNecessary.m
+++ classes/util/infrastructure/updateRatrixRevisionIfNecessary.m
@@ -1,9 +1,25 @@
 function quit=updateRatrixRevisionIfNecessary(args)
 quit=false;
 [runningSVNversion repositorySVNversion url]=getSVNRevisionFromXML(getRatrixPath);
+properties = getSVNPropertiesForPath(url,{'commit'});
+lastCommitVersion = properties.commit;
 [targetSVNurl targetRevNum] =checkTargetRevision(args);
 
-if ~strcmp(url,targetSVNurl) || ((isempty(targetRevNum) && runningSVNversion~=repositorySVNversion) || (~isempty(targetRevNum) && targetRevNum~=runningSVNversion))
-    writeSVNUpdateCommand(targetSVNurl,targetRevNum);
+
+%     runningSVNversion
+%     repositorySVNversion
+%     url
+%     lastCommitVersion
+%     targetSVNurl
+%     targetRevNum
+
+if ~strcmp(url,targetSVNurl) || ...
+        ((isempty(targetRevNum) && runningSVNversion~=repositorySVNversion && runningSVNversion<lastCommitVersion) || ...
+            (~isempty(targetRevNum) && targetRevNum~=runningSVNversion))
+%     writeSVNUpdateCommand(targetSVNurl,targetRevNum);
+    fprintf('we need to update\n');
+
     quit=true;
+else
+    fprintf('no need to update\n');
 end

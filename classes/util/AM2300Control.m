63b9b9c0193e115ed1d35e8955ed4677aa9a808a
erik <erik@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2008-12-03 03:34:42 +0000

    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@1715 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- classes/util/AM2300Control.m
+++ classes/util/AM2300Control.m
@@ -2,6 +2,8 @@
 % it takes TTL command signals (0/+5V voltages) into its serial port, and outputs a specific current over its banana plugs output.
 % because it is a current source, the output current is supposed to be relatively independent of the resistance over which it is pushing the current.
 % 
+% please read the manual to make sure you know what you're doing...  below are just my rough notes from memory
+%
 % first check the battery level on the stimulus isolator: set power to off, push battery/test button. light should come on.
 % you don't want to be connected to AC, so charge up if necessary.
 % rechargable batteries can be ruined by certain charging practices, so see the manual for whether you need to recharge fully, etc.
@@ -10,9 +12,11 @@
 % 	hook up the stim isolater (still not plugging into control cable) so that it will push the current over the resistor. 
 % 	put the multimeter in series with the resistor (be sure you're using the small current input on the multimeter, NOT the volt
 % 	on front panel of stimulus isolator, set baseline to 0, range to 10ua, pulse to .1, so that's 1ua
+%   i think you can manually trigger the output by setting the switch to "on" and holding the "test" button, but i can't recall for sure.
 % 
 % hook up serial and parallel breakout cards to map pins [2 3 4] parallel to [4 8 9] serial (on the stimulus isolator these mean "gate", "sign", "pulse baseline")
 % also hook up pins [any 18-25] parallel to [3 and 7] serial (these are the TTL ground lines)
+% the grey serial-parallel cable that is with the stim isolator probably has these same mappings -- but need to check to make sure!
 %
 % don't plug the cable into the stimulus isolator until you have made sure to initialize the pins to zero on the parallel port (see below).
 % 

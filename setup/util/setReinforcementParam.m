7055e07f21bc0ee173c636fdd192acbda9e16266
erik <erik@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2008-12-11 23:43:48 +0000

    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@1743 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- setup/util/setReinforcementParam.m
+++ setup/util/setReinforcementParam.m
@@ -1,17 +1,5 @@
 function setReinforcementParam(param,ids,val,stepNum,comment,auth)
 
-if ~exist('auth','var')
-    auth='unspecified'; %bad idea?
-end
-
-if ~exist('comment','var')
-    comment='';
-end
-
-if ~exist('stepNum','var') || isempty(stepNum)
-    stepNum='all';
-end
-
 r=init;
 subs=getSubjectsFromIDs(r,ids);
 for i=1:length(subs)
--- setup/util/setRewardULorMS.m
+++ setup/util/setRewardULorMS.m
@@ -1,2 +1,14 @@
 function setRewardULorMS(ids,val,stepNum,comment,auth)
+if ~exist('auth','var')
+    auth='unspecified'; %bad idea?
+end
+
+if ~exist('comment','var')
+    comment='';
+end
+
+if ~exist('stepNum','var') || isempty(stepNum)
+    stepNum='all';
+end
+
 setReinforcementParam('rewardULorMS',ids,val,stepNum,comment,auth);

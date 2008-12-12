16e58683b3bd793a8b8283180b1b1db2fd6b6e6f
rlab <rlab@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2008-12-12 17:03:35 +0000

    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@1750 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- classes/@subject/changeProtocolStep.m
+++ classes/@subject/changeProtocolStep.m
@@ -1,27 +1,24 @@
-function [subject r]=changeProtocolStep(subject,ts,r,comment,auth,stepNumToChange)
+function [subject r]=changeProtocolStep(subject,ts,r,comment,auth,stepNum)
 
-if ~exist('stepNumToChange','var')
-    stepNumToChange=subject.trainingStepNum;
+if ~exist('stepNum','var')||isempty(stepNum)
+    stepNum=subject.trainingStepNum;
 end
+
 if isa(ts,'trainingStep') && isa(r,'ratrix') && ~isempty(getSubjectFromID(r,subject.id))
-    if stepNumToChange>0
+    if ~isempty(subject.protocol) && stepNum>0 && stepNum<=getNumTrainingSteps(subject.protocol)
+       
+        
         if authorCheck(r,auth)
+                       
+            newProtocol = changeStep(subject.protocol, ts, stepNum);
             
-            for i=1:getNumTrainingSteps(subject.protocol)
-                if i~=stepNumToChange
-                    steps{i}=getTrainingStep(subject.protocol,i);
-                else
-                    steps{i}=ts;
-                end
-            end
-            
-            [subject r]=setProtocolAndStep(subject,protocol(getName(subject.protocol),steps),0,1,0,subject.trainingStepNum,r,comment,auth);
+            [subject r]=setProtocolAndStep(subject,newProtocol,0,1,0,subject.trainingStepNum,r,comment,auth);
             
         else
             error('author failed authentication')
         end
     else
-        error('subject not on a training step')
+        error('subject does not have a protocol, or stepNum is not a valid index of trainingSteps in the protocol')
     end
 else
     error('need trainingStep and ratrix that contains this subject')

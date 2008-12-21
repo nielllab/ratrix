def56aae71ba415e68d437de5316631cbca59e5b
erik <erik@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2008-12-21 06:08:24 +0000

    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@1797 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- classes/protocols/@soundManager/private/testsound.m
+++ classes/protocols/@soundManager/private/testsound.m
@@ -1,7 +1,38 @@
 function testsound
+clear mex
 clear psychportaudio
-initializepsychsound(1)
-x=psychportaudio('open',[],[],4)
-psychportaudio('fillbuffer',x,rand(2,100))
-psychportaudio('start',x);y=getsecs;psychportaudio('stop',x);getsecs-y
+
+p=maxpriority('getsecs');
+priority(p);
+initializepsychsound(1);
+x=psychportaudio('open',[],[],4);
+psychportaudio('fillbuffer',x,rand(2,40000)-.5);
+getsecs;
+psychportaudio('start',x);
+psychportaudio('stop',x);
+
+for i=1:10
+    y=getsecs;
+    psychportaudio('start',x);
+    s=getsecs-y;
+    pause(.5);
+    y=getsecs;
+    psychportaudio('stop',x,2);
+    t=getsecs-y;
+    fprintf('%g\t%g\n',s,t)
+end
+
 psychportaudio('close')
+
+x=audioplayer(rand(2,40000)-.5,44100);
+for i=1:10
+    y=getsecs;
+    play(x);
+    s=getsecs-y;
+    pause(.5);
+    y=getsecs;
+    stop(x);
+    t=getsecs-y;
+    fprintf('%g\t%g\n',s,t)
+end
+priority(0);

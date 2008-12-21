943ad683091f15e981e01a55e2ac08c73288f646
erik <erik@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2008-12-21 12:30:01 +0000

    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@1803 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- classes/@station/setResolution.m
+++ classes/@station/setResolution.m
@@ -36,7 +36,7 @@ if oldRes.width~=res.width || oldRes.height~=res.height || oldRes.hz ~=res.hz ||
     Screen('Resolution', s.screenNum, res.width, res.height, res.hz, res.pixelSize);
     s=startPTB(s);
     
-    newRes=Screen('Resolution', s.screenNum);
+    newRes=Screen('Resolution', s.screenNum)
     if ~all([newRes.width==res.width newRes.height==res.height newRes.pixelSize==res.pixelSize newRes.hz==res.hz])
         error('failed to get desired res') %needs to be warning to work with remotedesktop
     end

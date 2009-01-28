e152666e8df989e87f39cebb46d37c7879d359e5
fan <fan@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2009-01-28 21:03:19 +0000

    catch daqdevice objects that error during structize
    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@1888 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- classes/util/matlaby/structize.m
+++ classes/util/matlaby/structize.m
@@ -2,25 +2,31 @@ function s=structize(s)
 if iscell(s)
     s=cellfun(@structize,s,'UniformOutput',false);
 elseif isobject(s) || isstruct(s)
-    if isscalar(s) || all(size(s)==0) %size(object with no data members) is [0 0]
-        if isobject(s)
-            t=class(s);
-            s=struct(s);
-            f=fields(s);
-            if ~ismember('originalType',f)
-                if isempty(s)
-                    s=struct;
+    try
+        if isscalar(s) || all(size(s)==0) %size(object with no data members) is [0 0]
+            if isobject(s)
+                t=class(s);
+                s=struct(s);
+                f=fields(s);
+                if ~ismember('originalType',f)
+                    if isempty(s)
+                        s=struct;
+                    end
+                    s.originalType=t;
+                else
+                    error('already a originalType field')
                 end
-                s.originalType=t;
-            else
-                error('already a originalType field')
             end
+            if ~all(size(s)==0) %structfun can't handle s=struct([]) which is [0 0]
+                s=structfun(@structize,s,'UniformOutput',false); %requires scalar s
+            end
+        else
+            s=arrayfun(@structize,s,'UniformOutput',true);
         end
-        if ~all(size(s)==0) %structfun can't handle s=struct([]) which is [0 0] 
-            s=structfun(@structize,s,'UniformOutput',false); %requires scalar s
-        end
-    else
-        s=arrayfun(@structize,s,'UniformOutput',true);
+    catch
+        % 1/28/09 hopefully we are catching daqdevice case (for
+        % syringePump stations) and not much else here...
+        s=class(s); % just record the class of the object we are "throwing away"
     end
 else
     %pass through non-object/struct/cell

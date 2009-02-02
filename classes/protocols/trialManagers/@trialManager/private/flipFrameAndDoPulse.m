9f9f1636b2c8ffa3a0d5c7eb5b820ea231a96d76
erik <erik@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2009-02-02 10:22:05 +0000

    timestamp cleanup
    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@1918 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- classes/protocols/trialManagers/@trialManager/private/flipFrameAndDoPulse.m
+++ classes/protocols/trialManagers/@trialManager/private/flipFrameAndDoPulse.m
@@ -1,5 +1,5 @@
-function [lastI vbl ft missed timestamps] = ...
-    flipFrameAndDoPulse(tm, window, dontclear, i, vbl, framesPerUpdate, ifi, paused, doFramePulse,station,timestamps)
+function [lastI timestamps] = ...
+    flipFrameAndDoPulse(tm, window, dontclear, i, framesPerUpdate, ifi, paused, doFramePulse,station,timestamps)
 
 timeStamps.enteredFlipFrameAndDoPulse=GetSecs;
 
@@ -10,7 +10,7 @@ if window>=0
 end
 timestamps.drawingFinished=GetSecs;
 
-timestamps.when=vbl+(framesPerUpdate-0.8)*ifi; %this 0.8 is critical -- get frame drops if it is 0.2.  mario uses 0.5.  in theory any number 0<x<1 should give identical results.
+timestamps.when=timestamps.vbl+(framesPerUpdate-0.8)*ifi; %this 0.8 is critical -- get frame drops if it is 0.2.  mario uses 0.5.  in theory any number 0<x<1 should give identical results.
 
 if ~paused && doFramePulse
     framePulse(station);
@@ -22,19 +22,19 @@ timestamps.prePulses=GetSecs;
 
 %wait for next frame, flip buffer
 if window>=0
-    [vbl sos ft missed]=Screen('Flip',window,timestamps.when,dontclear); %vbl=vertical blanking time, when flip starts executing
-     %http://psychtoolbox.org/wikka.php?wakka=FaqFlipTimestamps
-     %vbl=vertical blanking time, when bufferswap occurs (corrected by beampos logic if available/reliable)
-     %sos=stimulus onset time -- vbl + a computed constant corresponding to the duration of the vertical blanking (a delay in when, after vbl, that the swap actually happens, depends on a lot of guts)
-     %ft=timestamp from the end of flip's execution --  using this for 'apparent misses' is probably wrong since i think this might be significantly later than the swap and more jitter prone than vbl!
+    [timestamps.vbl sos timestamps.ft timestamps.missed]=Screen('Flip',window,timestamps.when,dontclear); %vbl=vertical blanking time, when flip starts executing
+    %http://psychtoolbox.org/wikka.php?wakka=FaqFlipTimestamps
+    %vbl=vertical blanking time, when bufferswap occurs (corrected by beampos logic if available/reliable)
+    %sos=stimulus onset time -- vbl + a computed constant corresponding to the duration of the vertical blanking (a delay in when, after vbl, that the swap actually happens, depends on a lot of guts)
+    %ft=timestamp from the end of flip's execution --  using this for 'apparent misses' is probably wrong since i think this might be significantly later than the swap and more jitter prone than vbl!
 else
     waitTime=GetSecs()-timestamps.when;
     if waitTime>0
         WaitSecs(waitTime);
     end
-    ft=timestamps.when;
-    vbl=ft;
-    missed=0;
+    timestamps.ft=timestamps.when;
+    timestamps.vbl=ft;
+    timestamps.missed=0;
 end
 
 if ~paused
@@ -47,9 +47,9 @@ timestamps.postFlipPulse=GetSecs;
 
 %logwrite('just flipped');
 
-if ft-vbl>.15*ifi
+if timestamps.ft-timestamps.vbl>.15*ifi
     %this occurs when my osx laptop runs on battery power
-    fprintf('long delay inside flip after the swap-- ft-vbl:%.15g%% of ifi, now-vbl:%.15g\n',(ft-vbl)/ifi,GetSecs-vbl)
+    fprintf('long delay inside flip after the swap-- ft-vbl:%.15g%% of ifi, now-vbl:%.15g\n',(timestamps.ft-timestamps.vbl)/ifi,GetSecs-timestamps.vbl)
 end
-    
+
 end % end function
--- classes/protocols/trialManagers/@trialManager/private/saveMissedFrameData.m
+++ classes/protocols/trialManagers/@trialManager/private/saveMissedFrameData.m
@@ -1,13 +1,12 @@
-function [responseDetails lastFrameTime timestamps] = ...
-    saveMissedFrameData(tm, responseDetails, missed, frameNum, timingCheckPct, lastFrameTime, ifi,...
-    timestamps,vbl,ft)
+function [responseDetails timestamps] = ...
+    saveMissedFrameData(tm, responseDetails, frameNum, timingCheckPct, ifi, timestamps)
 
 %save facts about missed frames
 
 type='';
-thisIFI=vbl-lastFrameTime;
+thisIFI=timestamps.vbl-timestamps.lastFrameTime;
 
-if missed>0
+if timestamps.missed>0
     type='caught';
     responseDetails.numMisses=responseDetails.numMisses+1;
     
@@ -16,7 +15,7 @@ if missed>0
         responseDetails.misses(responseDetails.numMisses)=frameNum;
         responseDetails.afterMissTimes(responseDetails.numMisses)=GetSecs();
         responseDetails.missIFIs(responseDetails.numMisses)=thisIFI;
-        %responseDetails.missTimestamps(responseDetails.numMisses)=timestamps; %need to figure out: Error: Subscripted assignment between dissimilar structures
+        responseDetails.missTimestamps(responseDetails.numMisses)=timestamps; %need to figure out: Error: Subscripted assignment between dissimilar structures
     else
         responseDetails.numUnsavedMisses=responseDetails.numUnsavedMisses+1;
     end
@@ -32,7 +31,7 @@ else
             responseDetails.apparentMisses(responseDetails.numApparentMisses)=frameNum;
             responseDetails.afterApparentMissTimes(responseDetails.numApparentMisses)=GetSecs();
             responseDetails.apparentMissIFIs(responseDetails.numApparentMisses)=thisIFI;
-            %responseDetails.apparentMissTimestamps(responseDetails.numApparentMisses)=timestamps; %need to figure out: Error: Subscripted assignment between dissimilar structures
+            responseDetails.apparentMissTimestamps(responseDetails.numApparentMisses)=timestamps; %need to figure out: Error: Subscripted assignment between dissimilar structures
         else
             responseDetails.numUnsavedApparentMisses=responseDetails.numUnsavedApparentMisses+1;
         end
@@ -42,31 +41,29 @@ end
 
 if ~strcmp(type,'')
     fprintf('missed frameNum: %d, ifi: %g (%g%% late): %s\n',frameNum,thisIFI,100*((thisIFI/ifi)-1),type)
-    fprintf('\tlast misses recorded:\t\t%g\n',  1000*(timestamps.missesRecorded-timestamps.prevPostFlipPulse))
-    fprintf('\tlast eyetracker done:\t\t%g\n',  1000*(timestamps.eyeTrackerDone-timestamps.missesRecorded))
-    fprintf('\tlast kbCheck done:\t\t%g\n',     1000*(timestamps.kbCheckDone-timestamps.eyeTrackerDone))
-    fprintf('\tlast handle kbd done:\t\t%g\n',  1000*(timestamps.keyboardDone-timestamps.kbCheckDone))
-    fprintf('\tlast into phase logic:\t\t%g\n', 1000*(timestamps.enteringPhaseLogic-timestamps.keyboardDone))
-    fprintf('\tlast phase logic done:\t\t%g\n', 1000*(timestamps.phaseLogicDone-timestamps.enteringPhaseLogic))
-    fprintf('\tlast reward done:\t\t%g\n',      1000*(timestamps.rewardDone-timestamps.phaseLogicDone))
-    fprintf('\tlast server comm done:\t\t%g\n', 1000*(timestamps.serverCommDone-timestamps.rewardDone))
-    fprintf('\tlast phase records done:\t%g\n', 1000*(timestamps.phaseRecordsDone-timestamps.serverCommDone))
-    fprintf('\tlast loop done:\t\t\t%g\n',      1000*(timestamps.loopEnd-timestamps.phaseRecordsDone))
-    fprintf('\tlast time to cycle:\t\t%g\n',    1000*(timestamps.loopStart-timestamps.loopEnd))
-    fprintf('\tphase update:\t\t\t%g\n',        1000*(timestamps.phaseUpdated-timestamps.loopStart))
-    fprintf('\tframe draw:\t\t\t%g\n',          1000*(timestamps.frameDrawn-timestamps.phaseUpdated))
-    fprintf('\tframe drop corner drawn:\t%g\n', 1000*(timestamps.frameDropCornerDrawn-timestamps.frameDrawn))
-    fprintf('\ttext drawn:\t\t\t%g\n',          1000*(timestamps.textDrawn-timestamps.frameDropCornerDrawn))
-    fprintf('\tdrawing finished:\t\t%g\n',      1000*(timestamps.drawingFinished-timestamps.textDrawn))
-    fprintf('\tprepulses done:\t\t\t%g\n',      1000*(timestamps.prePulses-timestamps.drawingFinished))
-    
-    fprintf('\tvbl done:\t\t\t%g\n',            1000*(vbl-timestamps.prePulses))    
-    fprintf('\tflip returned:\t\t\t%g\n',       1000*(ft-vbl))    
-    
-    fprintf('\tpost flip pulse:\t\t%g\n',       1000*(timestamps.postFlipPulse-ft)) 
+    fprintf('\tlast misses recorded:\t\t%g\n',  1000*(timestamps.missesRecorded       - timestamps.prevPostFlipPulse))
+    fprintf('\tlast eyetracker done:\t\t%g\n',  1000*(timestamps.eyeTrackerDone       - timestamps.missesRecorded))
+    fprintf('\tlast kbCheck done:\t\t%g\n',     1000*(timestamps.kbCheckDone          - timestamps.eyeTrackerDone))
+    fprintf('\tlast handle kbd done:\t\t%g\n',  1000*(timestamps.keyboardDone         - timestamps.kbCheckDone))
+    fprintf('\tlast into phase logic:\t\t%g\n', 1000*(timestamps.enteringPhaseLogic   - timestamps.keyboardDone))
+    fprintf('\tlast phase logic done:\t\t%g\n', 1000*(timestamps.phaseLogicDone       - timestamps.enteringPhaseLogic))
+    fprintf('\tlast reward done:\t\t%g\n',      1000*(timestamps.rewardDone           - timestamps.phaseLogicDone))
+    fprintf('\tlast server comm done:\t\t%g\n', 1000*(timestamps.serverCommDone       - timestamps.rewardDone))
+    fprintf('\tlast phase records done:\t%g\n', 1000*(timestamps.phaseRecordsDone     - timestamps.serverCommDone))
+    fprintf('\tlast loop done:\t\t\t%g\n',      1000*(timestamps.loopEnd              - timestamps.phaseRecordsDone))
+    fprintf('\tlast time to cycle:\t\t%g\n',    1000*(timestamps.loopStart            - timestamps.loopEnd))
+    fprintf('\tphase update:\t\t\t%g\n',        1000*(timestamps.phaseUpdated         - timestamps.loopStart))
+    fprintf('\tframe draw:\t\t\t%g\n',          1000*(timestamps.frameDrawn           - timestamps.phaseUpdated))
+    fprintf('\tframe drop corner drawn:\t%g\n', 1000*(timestamps.frameDropCornerDrawn - timestamps.frameDrawn))
+    fprintf('\ttext drawn:\t\t\t%g\n',          1000*(timestamps.textDrawn            - timestamps.frameDropCornerDrawn))
+    fprintf('\tdrawing finished:\t\t%g\n',      1000*(timestamps.drawingFinished      - timestamps.textDrawn))
+    fprintf('\tprepulses done:\t\t\t%g\n',      1000*(timestamps.prePulses            - timestamps.drawingFinished))
+    fprintf('\tvbl done:\t\t\t%g\n',            1000*(timestamps.vbl                  - timestamps.prePulses))    
+    fprintf('\tflip returned:\t\t\t%g\n',       1000*(timestamps.ft                   - timestamps.vbl))    
+    fprintf('\tpost flip pulse:\t\t%g\n',       1000*(timestamps.postFlipPulse        - timestamps.ft)) 
 end
 
-lastFrameTime=vbl;
+timestamps.lastFrameTime=timestamps.vbl;
 timestamps.prevPostFlipPulse=timestamps.postFlipPulse;
 
 end % end function

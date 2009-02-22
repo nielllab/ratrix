5389a8ed158ae2d6ac091dbc4f36758ec95e72a6
erik <erik@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2009-02-22 21:09:55 +0000

    more -nojvm
    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@1963 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- classes/pumpSystem/util/closeAllSerials.m
+++ classes/pumpSystem/util/closeAllSerials.m
@@ -1,4 +1,8 @@
 function closeAllSerials
+if ~usejava('jvm')
+    warning('closeAllSerials can''t call instrfind with no jvm')
+    return
+end
 alreadies=instrfind;
 if ~isempty(alreadies)
     fprintf('closing and deleting %d serial ports\n',length(alreadies));

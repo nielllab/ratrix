f43ba54c283f5a95939195b7aa4b9df513a0bd64
fan <fan@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2009-02-25 23:54:09 +0000

    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@1984 d291aab3-510b-4049-a6d1-fe65b8d52d70

---
+++ classes/protocols/@stimSpec/setScaleFactor.m
@@ -0,0 +1,7 @@
+function spec=setScaleFactor(spec,scaleFactor)
+% stimSpec.scaleFactor set function
+% sets the scaleFactor associated with the given stimSpec object
+
+spec.scaleFactor = scaleFactor;
+
+end

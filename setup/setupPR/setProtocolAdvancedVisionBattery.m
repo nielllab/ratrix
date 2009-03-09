3f3fbc1d6e8f8db5af9a71be8f84050d85cd6c9a
rlab <rlab@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2009-03-09 21:06:58 +0000

    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@2028 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- setup/setupPR/setProtocolAdvancedVisionBattery.m
+++ setup/setupPR/setProtocolAdvancedVisionBattery.m
@@ -9,17 +9,15 @@ function r = setProtocolAdvVisTest(r,subjIDs)
 %   particular experiment, culminating in a protocol p that is applied to
 %   the subjID's in the ratrix object r
 % revised 3/9/09 for new tag
- 
 %%%%%QUESTIONS 3/9 need to request support in each relevant calcstim 
 % do stimuli toggle? 
 % response time limit 
 % max stim duration
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% define rat cohorts 
-% all rats that received LGN lesions in cohort 2 or 3
-ValidRats={'307','309','297','298','299','302'};  
-demostep=3; % affects demo1 subject only
+svnRevision={'svn://132.239.158.177/projects/ratrix/trunk'};
+ValidRats={'307','309','297','298','299','302'};  % all rats that received LGN lesions in cohort 2 or 3 
+demostep=3; % for testing: affects demo1 subject only
 
 % params used by both freedrinks and gotostim trial managers
 maxWidth                =1024; % of the screen
@@ -33,7 +31,6 @@ fractionSoundOn        = 1;
 fractionPenaltySoundOn = 0;
 rewardScalar =1;
 msPuff=0;
-
 %params of freedrinks trial manager only
 msPenaltyFD              =0; %no penalty during free drinks
 stoch_freeDrinkLikelihood=0.006; %ts1 stochastic water emitted without licks
@@ -51,7 +48,6 @@ radius                  =.10;
 thresh                  =.00005; %what is this?
 yPosPct                 =.65;
 
-svnRevision={'svn://132.239.158.177/projects/ratrix/trunk'};
 
 %path containing ALL the stimuli
 imdir='\\Reinagel-lab.ad.ucsd.edu\rlab\Rodent-Data\preinagel\PRimageset' 
@@ -73,11 +69,10 @@ ypos_nAFC=0; %location of image stimuli is near ports
 graduationCriterion=performanceCriterion([.85 0.80], int16([200 300])); %85% correct for 200 consecutive trials
 
 %create sound manager object used by stochastic free drinks only
-% no hissing sound when water clogs ports
 %[correctSound, keepGoingSound,trySomethingElseSound, wrongSound]
 smSTOCH=soundManager({soundClip('correctSound','allOctaves',[400],20000), ...
     soundClip('keepGoingSound','allOctaves',[300],20000), ...
-     soundClip('trySomethingElseSound','gaussianWhiteNoise'), ...
+     soundClip('trySomethingElseSound','allOctaves',[],20000), ... % no white noise hiss
     soundClip('wrongSound','allOctaves',[50],20000)}); % tone
 
 %create sound manager object used by all the other trial managers
@@ -90,7 +85,7 @@ sm =soundManager({soundClip('correctSound','allOctaves',[400],20000), ...
 scheduler=minutesPerSession(120,3); % sch=noTimeOff();
 
 % new in trunk 3/9/09
-doAllRequests='first'; % only reward first requesttriallick
+doAllRequests='first'; % only reward first request trial lick
 %create FD reinforcement manager object
 FDreinfmanager = constantReinforcement(rewardSizeULorMS,...
     requestRewardSizeULorMS,doAllRequests,... % NEW parameters
@@ -163,12 +158,58 @@ imagelevel2invar = images(imdir,ypos_nAFC,background_nAFC,...
 %define the associated training step
 objrec2invar = trainingStep(gotostim, imagelevel2invar, graduationCriterion, scheduler,svnRevision); 
 
+%%%%%%%%%%%%%%%%%%%% motion training steps %%%%%%%%%%%%%%%%%%%%%%%
+% %%% NOT TESTED NOT TESTED NOT TESTED
+% 
+% % params used by all coherent dots tasks
+% %motionstimulus = coherentDots;
+% %trialManagerClass='nAFC';
+% %frameRate=100;
+% %responsePorts=[1,3];
+% %totalPorts=3;
+% screen_width=100;
+% screen_height=100;
+% num_dots=100;
+% movie_duration=10; % seconds??
+% screen_zoom=[6 6];
+% maxWidth=1024;
+% maxHeight=768;
+% 
+% %%% motion1 = coherent dots 85% coherence only 
+% coherence=[.85];
+% speed=[1];
+% contrast=[1];
+% dot_size=[3];
+% discrimStim1 = coherentDots(screen_width,screen_height,...
+%     num_dots,coherence,speed,contrast,dot_size,movie_duration,screen_zoom,maxWidth,maxHeight);
+%  % discrimStim1= coherentDots(maxWidth,maxHeight, 150, 100, 100, coherence, 1, 3, 10, 85, [6 6]);
+% % use the same trial manager as other 2AFC tasks
+% motion1 = trainingStep(gotostim, discrimStim1, graduationCriterion, scheduler,svnRevision);
+%  
+
+% %%% motion2 = coherent dots, harder
+% %motion2 = makeDotTrainingStep([0.5 0.99]); % 70% threshold is ~0.6?
+% coherence=[0.5 0.99];
+% discrimStim2= coherentDots(stimulus,trialManagerClass,frameRate,...
+%     responsePorts,totalPorts,maxWidth,maxHeight,[],...
+%     150, 100, 100, coherence, 1, 3, 10, 85, [6 6]);
+% motion2 = trainingStep(tmTEST, discrimStim2, pc,sch); %changed from tmEARLY 3/6
+% 
+% %%% motion3  = coherent dots psychometric curve  
+% % many are below threshold!
+% discrimStimPsych = coherentDots(stimulus,trialManagerClass,frameRate,...
+%     responsePorts,totalPorts,maxWidth,maxHeight,[],...
+%     150, 100, 100, [0.2 0.9], 1, 3, 10, 85, [6 6]);
+% motion3 = trainingStep(tmTEST, discrimStimPsych, pc,sch); %changed from tmQUIET 3/6
+
+
 %%%TO ADD%%
 %grating psychophysics
 %motion psychophysics
 %invariance to flipped images, more distractors, etc
 
 %%%PROTOCOLS%%%%%
+ 
 pAdvTest=   protocol('advanced vision tests', {objrec1 objrec2 objrec2invar});  
 
 %%%%%%%%%%%%

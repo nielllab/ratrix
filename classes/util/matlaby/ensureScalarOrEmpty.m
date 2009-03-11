1bbbc23d57ea085432111a53173674f32024b060
fan <fan@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2009-03-11 21:49:58 +0000

    fix to trialDetails compiling
    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@2044 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- classes/util/matlaby/ensureScalarOrEmpty.m
+++ classes/util/matlaby/ensureScalarOrEmpty.m
@@ -1,7 +1,7 @@
 function arrayOut=ensureScalarOrEmpty(cellIn)
 % changes empty ([]) to zero (0)
 if isvector(cellIn)
-    cellIn{cellfun(@isempty,cellIn)}=0;
+    cellIn(cellfun(@isempty,cellIn))={0};
     if all(cellfun(@isscalar,cellIn))
         arrayOut=[cellIn{:}];
     else

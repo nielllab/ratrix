3b893b15d859e7f16a52f848c4386ba2c8f850bc
erik <erik@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2009-03-12 02:40:32 +0000

    edf thinks eyelink.stop looks wrong
    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@2046 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- classes/protocols/eyeTrackers/@eyeLinkTracker/stop.m
+++ classes/protocols/eyeTrackers/@eyeLinkTracker/stop.m
@@ -16,6 +16,22 @@ status = Eyelink('ReceiveFile',lastFile, fullfile(getEyeDataPath(et),fileName),0
 disp('done transfer');
 %status = Eyelink('ReceiveFile',['filename'], ['dest'], ['dest_is_path'])
 %(2.95 receive_data_file in 'EyeLink API Specification.pdf')
+
+% edf thinks this looks wrong -- my original code in "fixationSoundDemo.m" was:
+%    status=Eyelink('ReceiveFile',edfFile,pwd,1);
+%    if status~=0
+%        fprintf('problem: ReceiveFile status: %d\n', status);
+%    end
+%
+% the help says you need to set the third arg to nonzero, so that your path is used.
+%
+% also looks like i also assumed that status should nominally be 0, but the help says:
+% returns: file size if OK, 0 if file transfer was cancelled, negative =  error code
+%
+% so i think you should be hitting your error every time?
+%
+% btw, what does 2.95 refer to?
+
 if ~status==0
     status=status
     fileName=fileName

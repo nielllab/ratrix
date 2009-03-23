f5d93e8b88ea634b64b01df8411a10d02fcd3d0e
philip <philip@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2009-03-23 23:32:33 +0000

    improved some
    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@2115 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- classes/protocols/@RFestimator/getCenter.m
+++ classes/protocols/@RFestimator/getCenter.m
@@ -3,31 +3,49 @@ function ctr = getCenter(RFe,subjectID)
 % The result is returned as a 2-element array [x y] in normalized units as fraction of screen
 
 
-
+%sca
+%keyboard
 RFData=getNeuralAnalysis(fullfile(getDataSource(RFe),subjectID),RFe.dateRange,RFe.centerParams{1});
+%load('\\132.239.158.179\datanet_storage\demo1\analysis\physAnalysis_191-20090205T151316.mat')
+load('\\132.239.158.179\datanet_storage\demo1\analysis\334-20090206T164500\physAnalysis_334-20090206T164500.mat')
 
 
+% find brightest point, to select time frame of interest
+ind=find(max(analysisdata.cumulativeSTA(:))==analysisdata.cumulativeSTA(:));
+[x y t]=ind2sub(size(analysisdata.cumulativeSTA),ind);
+STA2d=analysisdata.cumulativeSTA(:,:,t);
+        
 switch RFe.centerParams{2}
     case 'fitGaussian'
         alpha=RFe.centerParams{3}{1}
-        [STAenvelope STAparams] =fitGaussianEnvelopeToImage(RFData.cumulativeSTA,alpha,[],false,false);
+        
+        [STAenvelope STAparams] =fitGaussianEnvelopeToImage(STA2d,alpha,[],false,false);
     case 'fitGaussianSigEnvelope'
 
         alpha=RFe.centerParams{3}{1}
         medianFilter=RFe.centerParams{3}{2}
         %fit a guassian to the binary significance image -- conservative
-        sigSpots=getSignificantSTASpots(RFData.cumulativeSTA,RFData.cumulativeSpikeCount,stimdata.stimulusDetails.meanLuminance,stimdata.stimulusDetails.std,medianFilter,alpha);
+        sigSpots=getSignificantSTASpots(STA2d,RFData.cumulativeSpikeCount,stimdata.stimulusDetails.meanLuminance,stimdata.stimulusDetails.std,medianFilter,alpha);
         if ~length(union(unique(sigSpots),[0 1]))==2
             error('more than one RF spot!')
         end
         [sigEnvelope sigConservativeParams] =fitGaussianEnvelopeToImage(sigSpots,alpha,[],false,false);
 
         %use the conservative field to narrow a better seach of the STA
-        [STAenvelope STAparams] =fitGaussianEnvelopeToImage(RFData.cumulativeSTA,alpha,sigEnvelope,false,false);
+        [STAenvelope STAparams] =fitGaussianEnvelopeToImage(STA2d,alpha,sigEnvelope,false,false);
     otherwise
         error('unsupported method');
 end
+ % note param convention and matrix convention: (x(1) y(1))/ [x is size 2, y is size 1]
+ctr=STAparams(1:2)./[size(STA2d,2) size(STA2d,1)];
 
-ctr=STAparams(1:2);
+if any(ctr>1) || any(ctr<0)
+    warning('center is estimated to be off screen')
+    beep; beep
+    %force on screen
+    ctr(ctr>1)=1;
+    ctr(ctr<0)=0;
+end
+
 
 end % end function

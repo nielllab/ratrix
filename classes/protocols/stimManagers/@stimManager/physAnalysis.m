7642a7763c98b0107509cae4164bb1570e4ba87d
philip <philip@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2009-03-24 02:58:31 +0000

    superclass method now has correct nargin
    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@2119 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- classes/protocols/stimManagers/@stimManager/physAnalysis.m
+++ classes/protocols/stimManagers/@stimManager/physAnalysis.m
@@ -1,4 +1,4 @@
-function [analysisdata] = physAnalysis(stimManager,spikeData,stimData,plotParameters,parameters,analysisdata)
+function [analysisdata] = physAnalysis(stimManager,spikeData,stimData,plotParameters,parameters,analysisdata,eyeData)
 % default function returns unchanged analysisdata
 warning('using default physAnalysis that does nothing');
 end % end function

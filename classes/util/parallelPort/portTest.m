d4bf3a52e44c897fb099875210ad3d0e1e0a0420
erik <erik@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2009-03-29 05:04:39 +0000

    made a valve/sensor checker
    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@2151 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- classes/util/parallelPort/portTest.m
+++ classes/util/parallelPort/portTest.m
@@ -1,55 +1,81 @@
-method='pci';
+function portTest
 
-switch method
+%DO NOT CHANGE THIS -- SWAPPERS USE IT TO CHECK COMPONENTS!!!
 
-    case 'pcmcia'
-        portAddress='FFF8';
-    case 'pci'
-        portAddress='B888';
-    otherwise
-        error('method should be pci or pcmcia')
+addr='0378';
+valves=[6 7 8];
+sensors=[4 2 3];
+closed=char('0'*ones(1,8));
+last=sensors;
+while true
+    out=dec2bin(lptread(hex2dec(addr)+1),8)=='0';
+    this=out(sensors); 
+    if ~all(this==last)
+        clc
+        last=this %type out the state of the left, center, right sensors (zero=open, one=blocked)
+    end
+    t=closed;
+    t(valves(out(sensors)))='1';
+    lptwrite(hex2dec(addr),bin2dec(t));
 end
 
-leftV = '00000001';
-centerV = '00000010';
-rightV = '00000100';
-closeV = '00000000';
-leftP = 2;
-centerP = 3;
-rightP = 4;
 
-poking = 0;
-while 1
-    pause(.01)
-    %status=dec2bin(lpt1.read);
 
-    %y=lptread(1+hex2dec(portAddress))
 
-    status=dec2bin(lptread(1+hex2dec(portAddress)));
-    %disp(sprintf('%c %c %c',status(2),status(3),status(4)));
+if false
+    method='pci';
 
-    left=status(leftP)=='1';
-    center=status(centerP)=='1';
-    right=status(rightP)=='1';
-    [left center right]
+    switch method
 
-    if poking
-        if ~(left||center||right)
-            poking=0;
-            lptwrite(hex2dec(portAddress),  bin2dec(closeV))
-        end
-    else
-        if left||center||right
-            poking=1;
-            if left
-                valve = leftV;
-            elseif center
-                valve = centerV;
-            elseif right
-                valve = rightV;
+        case 'pcmcia'
+            portAddress='FFF8';
+        case 'pci'
+            portAddress='B888';
+        otherwise
+            error('method should be pci or pcmcia')
+    end
+
+    leftV = '00000001';
+    centerV = '00000010';
+    rightV = '00000100';
+    closeV = '00000000';
+    leftP = 2;
+    centerP = 3;
+    rightP = 4;
+
+    poking = 0;
+    while 1
+        pause(.01)
+        %status=dec2bin(lpt1.read);
+
+        %y=lptread(1+hex2dec(portAddress))
+
+        status=dec2bin(lptread(1+hex2dec(portAddress)));
+        %disp(sprintf('%c %c %c',status(2),status(3),status(4)));
+
+        left=status(leftP)=='1';
+        center=status(centerP)=='1';
+        right=status(rightP)=='1';
+        [left center right]
+
+        if poking
+            if ~(left||center||right)
+                poking=0;
+                lptwrite(hex2dec(portAddress),  bin2dec(closeV))
             end
+        else
+            if left||center||right
+                poking=1;
+                if left
+                    valve = leftV;
+                elseif center
+                    valve = centerV;
+                elseif right
+                    valve = rightV;
+                end
 
-            lptwrite(hex2dec(portAddress), bin2dec(valve))
+                lptwrite(hex2dec(portAddress), bin2dec(valve))
+            end
         end
     end
 end

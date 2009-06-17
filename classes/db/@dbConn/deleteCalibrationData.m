de6b6cdf41e2e27b0cb517b453cc3e23c38d2f02
fan <fan@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2009-06-17 19:31:35 +0000

    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@2513 d291aab3-510b-4049-a6d1-fe65b8d52d70

---
+++ classes/db/@dbConn/deleteCalibrationData.m
@@ -0,0 +1,40 @@
+function out=deleteCalibrationData(conn,mac,timeRange)
+% this function deletes calibrationData entries based on a mac and timeRange
+
+out=0;
+str=sprintf('select data, to_char(timestamp,''mm-dd-yyyy hh24:mi''),cmd_line from MONITOR_CALIBRATION where station_mac=''%s''',mac);
+results=query(conn,str);
+list=results(:,[2 3]);
+
+for i=1:size(results,1)
+    results{i,2}=datenum(results{i,2},'mm-dd-yyyy HH:MM');
+end
+which=find(cell2mat(results(:,2))>=timeRange(1)&cell2mat(results(:,2))<=timeRange(2));
+
+if ~isempty(which)
+    dispStr=sprintf('This will DELETE %d calibration data entries! Are you sure you want to delete (Y/N)?',length(which));
+    validInput=false;
+    while ~validInput
+        s=input(dispStr,'s');
+        if strcmpi(s,'Y')
+            validInput=true;
+            % do delete
+            for i=1:length(which)
+                str=sprintf('delete from MONITOR_CALIBRATION where cmd_line=''%s'' and timestamp=to_date(''%s'',''mm-dd-yyyy hh24:mi'')',...
+                    list{which(i),2},list{which(i),1})
+                exec(conn,str);
+                out=out+1;
+            end
+        elseif strcmpi(s,'N')
+            validInput=true;
+            % do not delete
+            disp('No entries deleted.');
+        else
+            disp('Invalid input - please try again');
+        end
+    end        
+elseif isempty(which)
+    error('failed to find any entries for the given mac address and timeRange');
+end
+
+end % end function
--- classes/db/@dbConn/getCalibrationData.m
+++ classes/db/@dbConn/getCalibrationData.m
@@ -3,20 +3,34 @@ function out=getCalibrationData(conn,mac,timeRange)
 % timeRange is specified as a a 2x1 vector of datenums
 
 out={};
-str=sprintf('select data, to_char(timestamp,''mm-dd-yyyy hh24:mi'') from MONITOR_CALIBRATION where station_mac=''%s''',mac);
+str=sprintf('select data, to_char(timestamp,''mm-dd-yyyy hh24:mi''),cmd_line from MONITOR_CALIBRATION where station_mac=''%s''',mac);
 results=query(conn,str);
+list=results(:,[2 3]);
 
 for i=1:size(results,1)
     results{i,2}=datenum(results{i,2},'mm-dd-yyyy HH:MM');
 end
 which=find(cell2mat(results(:,2))>=timeRange(1)&cell2mat(results(:,2))<=timeRange(2));
+ind=1;
 if length(which)>1
-    which
-    warning('found multiple entries for the given mac address - using first entry!');
+    for i=1:length(which)
+        dispStr=sprintf('%d\t%s\t%s',which(i),list{which(i),1},list{which(i),2});
+        disp(dispStr);
+    end
+    validInput=false;
+    while ~validInput
+        dispStr=sprintf('select an entry (%d-%d):',which(1),which(end));
+        ind=input(dispStr);
+        if isnumeric(ind) && isscalar(ind) && length(find(which==ind))==1
+            % pass
+            validInput=true;
+            ind=find(which==ind);
+        end
+    end
 elseif isempty(which)
     error('failed to find any entries for the given mac address and timeRange');
 end
-c=results{which(1),1};
+c=results{which(ind),1};
 stream=c.getBinaryStream;
 nextbyte=stream.read();
 while nextbyte~=-1

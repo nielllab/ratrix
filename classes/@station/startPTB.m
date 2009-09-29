edef1fbadf144ec46e661dfb9b114b2642940395
erik <erik@d291aab3-510b-4049-a6d1-fe65b8d52d70>
2009-09-29 22:51:52 +0000

    downgrading imaging pipeline concerns to warning
    git-svn-id: svn://132.239.158.177/projects/ratrix/trunk@2582 d291aab3-510b-4049-a6d1-fe65b8d52d70

--- classes/@station/startPTB.m
+++ classes/@station/startPTB.m
@@ -44,7 +44,8 @@ try
     if isempty(imagingTasks)
         s.window = Screen('OpenWindow',s.screenNum,0);%,[],32,2);  %%color, rect, depth, buffers (none can be changed in basic version)
     else
-        error('edf says: have you checked that the stopPTB will remove these tasks?  i see no evidence that you clean up after yourself if a later trial doesn''t want these things.  a clear Screen may help, but i want proof.  it''s not stated in ''help psychimaging'' -- worst case, ask mario.  also, i don''t see that you''ve been careful to make sure the pipeline details are recorded in the trial record.')
+        warning('edf says: have you checked that the stopPTB will remove these tasks?  i see no evidence that you clean up after yourself if a later trial doesn''t want these things.  a clear Screen may help, but i want proof.  it''s not stated in ''help psychimaging'' -- worst case, ask mario.  also, i don''t see that you''ve been careful to make sure the pipeline details are recorded in the trial record.')
+        %well, i guess it's relatively convincing that you get a unique window pointer out of it, so i'm downgrading to a warning...
         
         PsychImaging('PrepareConfiguration');
         % PsychImaging('AddTask', 'General', 'FloatingPoint32BitIfPossible'); %enable general support of such tasks

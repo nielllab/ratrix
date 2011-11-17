e1c96299afa38cba92215492a40192a930b58896
erik <erik.flister@gmail.com>
2011-11-16 22:28:56 -0800

    sound error on k-t step setting

--- classes/protocols/@soundManager/private/stopPlayer.m
+++ classes/protocols/@soundManager/private/stopPlayer.m
@@ -1,5 +1,9 @@
 function sm=stopPlayer(sm)
-
-        PsychPortAudio('Stop', sm.player,2,0);
+try
+    PsychPortAudio('Stop', sm.player,2,0);
+catch e
+    sca
+    keyboard
+end
 sm.playing=[];
 sm.looping=false;

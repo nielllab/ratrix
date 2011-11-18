7022efe27770d7946a65726737485a4c26bb2f6c
erik <erik.flister@gmail.com>
2011-11-17 20:51:36 -0800

    fixed k-t'ing back to a step with a numTrialsDoneCriterion -- graduation would reoccur after the first trial because trials in that step weren't required to be contiguous!
    added debugging info for soundmgr error on k-t'ing

--- classes/protocols/@soundManager/private/stopPlayer.m
+++ classes/protocols/@soundManager/private/stopPlayer.m
@@ -2,8 +2,31 @@ function sm=stopPlayer(sm)
 try
     PsychPortAudio('Stop', sm.player,2,0);
 catch e
-    sca
-    keyboard
+    e
+    
+    %TODO: debug me
+    sm.player
+	sm.playing
+    sm.looping    
+    
+    warning('occurs on k+t -- trying to stop sound but already killed player')
+    
+%  In @soundManager\private\stopPlayer at 6
+%  In @soundManager\private\doSound at 22
+%  In soundManager.playSound at 5
+%  In @trialManager\private\handlePhasedTrialLogic at 126
+%  In @trialManager\private\runRealTimeLoop at 734
+%  In @trialManager\private\stimOGL at 79
+%  In trialManager.doTrial at 267
+%  In trainingStep.doTrial at 83
+%  In subject.doTrial at 9
+%  In station.doTrials at 114
+%  In standAloneRun at 162    
+    
+    try
+        s=PsychPortAudio('GetStatus',sm.player)
+    end
+
 end
 sm.playing=[];
 sm.looping=false;
--- classes/protocols/criteria/@numTrialsDoneCriterion/checkCriterion.m
+++ classes/protocols/criteria/@numTrialsDoneCriterion/checkCriterion.m
@@ -2,6 +2,7 @@ function [graduate details] = checkCriterion(c,subject,trainingStep,trialRecords
 % this criterion will graduate if we have done a certain number of trials in this trainingStep
 
 thisStep=[trialRecords.trainingStepNum]==trialRecords(end).trainingStepNum;
+thisStep(1:find(~thisStep,1,'last')) = false;
 trialsUsed=trialRecords(thisStep);
 
 details=[];

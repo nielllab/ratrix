c82d65c5062851add2b93c74033feccdac7b3a50
eflister <erik.flister@gmail.com>
2011-11-28 11:22:39 -0800

    protocol updates

--- setup/setProtocolMouse.m
+++ setup/setProtocolMouse.m
@@ -22,7 +22,7 @@ msAirpuff                 =msPenalty;
 constantRewards=constantReinforcement(rewardSizeULorMS,requestRewardSizeULorMS,requestMode,msPenalty,fractionOpenTimeSoundIsOn,fractionPenaltySoundIsOn,scalar,msAirpuff);
 
 allowRepeats=false;
-freeDrinkLikelihood=0.008;
+freeDrinkLikelihood=0.004;
 fd = freeDrinks(sm,freeDrinkLikelihood,allowRepeats,constantRewards);
 
 freeDrinkLikelihood=0;
@@ -64,15 +64,32 @@ numTriggers = 20;
 ts1 = trainingStep(fd,  dots, rateCriterion(trialsPerMinute,minutes), noTimeOff(), svnRev,svnCheckMode);  %stochastic free drinks
 ts2 = trainingStep(fd2, dots, numTrialsDoneCriterion(numTriggers)   , noTimeOff(), svnRev,svnCheckMode);  %free drinks
 
-trialsPerMinute = 7;
-minutes = 2;
+trialsPerMinute = 6;
+minutes = 1;
 ts3 = trainingStep(nafcTM, dots, rateCriterion(trialsPerMinute,minutes), noTimeOff(), svnRev,svnCheckMode);  %coherent dots
-ts4 = trainingStep(nrTM  , dots, repeatIndefinitely()                  , noTimeOff(), svnRev,svnCheckMode);  %coherent dots
+ts4 = trainingStep(nrTM  , dots,  numTrialsDoneCriterion(400)          , noTimeOff(), svnRev,svnCheckMode);  %coherent dots
 
-p=protocol('mouse dots',{ts1, ts2, ts3, ts4});
-stepNum=uint8(3);
+msPenalty = 3000;
+longPenalty=constantReinforcement(rewardSizeULorMS,requestRewardSizeULorMS,requestMode,msPenalty,fractionOpenTimeSoundIsOn,fractionPenaltySoundIsOn,scalar,msAirpuff);
+lpTM=nAFC(sm,percentCorrectionTrials,longPenalty,eyeController,{'off'},dropFrames,'ptb','center'); %this percentCorrectionTrials should currently do nothing (need to fix)
+ts5 = trainingStep(lpTM  , dots, repeatIndefinitely()                  , noTimeOff(), svnRev,svnCheckMode);  %coherent dots
+
+p=protocol('mouse dots',{ts1, ts2, ts3, ts4, ts5});
+
+%not the right way to do this
+if strcmp(subjIDs,'all')
+    subjIDs=getSubjectIDs(r);
+end
 
 for i=1:length(subjIDs),
     subj=getSubjectFromID(r,subjIDs{i});
+    switch subjIDs{i}
+        case {'e1rt','e2rt','e1lt','e2lt'}
+            stepNum=uint8(5);
+        case {'n5rt','n5rn','n5lt','n8lt'}
+            stepNum=uint8(1);            
+        otherwise
+            break %this won't work cuz standAloneRun will try to run the subject after this
+    end
     [subj r]=setProtocolAndStep(subj,p,true,false,true,stepNum,r,'call to setProtocolMouse','edf');
 end

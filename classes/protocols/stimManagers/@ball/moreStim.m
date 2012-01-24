cc376654e356214d6ab0601cc004b6a446f20236
erik <erik.flister@gmail.com>
2012-01-23 21:14:20 -0800

    continuous line for ball

--- classes/protocols/stimManagers/@ball/moreStim.m
+++ classes/protocols/stimManagers/@ball/moreStim.m
@@ -27,8 +27,30 @@ relPos=dynamicDetails.track(:,1:i)-repmat(dynamicDetails.track(:,i)-s.initialPos
 out=true(size(stim));
 
 relPos=round(relPos./repmat(getScaleFactor(s)',1,i));
-relPos=relPos(:,all(relPos>0 & relPos<=repmat(fliplr(size(out))',1,size(relPos,2))));
-out(sub2ind(size(out),relPos(2,:),relPos(1,:)))=false;
+
+    function out=count(x,y)
+        out = linspace(x,y,1+abs(diff([x y]))); % x:sign(diff([x y])):y %fails when x==y
+    end
+
+if true
+    d = diff(relPos,[],2);
+    for j=2:i
+        corners=relPos(:,j-[1 0]);
+        if any(d(:,j-1)) && any(corners(:)>0 & corners(:)<=repmat(size(out)',2,1))
+            inds = count(corners(1,1),corners(1,end));
+            for k=1:length(inds)
+                if inds(k)>0 && inds(k)<=size(out,2)
+                    ys = round(corners(2,1)+d(2,j-1)*(k-[1 0])/length(inds));
+                    ys = count(ys(1),ys(end));
+                    out(ys(ys>0 & ys<=size(out,1)),inds(k))=false;
+                end
+            end
+        end
+    end
+else
+    relPos=relPos(:,all(relPos>0 & relPos<=repmat(fliplr(size(out))',1,size(relPos,2))));
+    out(sub2ind(size(out),relPos(2,:),relPos(1,:)))=false;
+end
 
 doFramePulse = true;
 indexPulse = true;

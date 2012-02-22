593dce8c39f3434025155131a4c5c5118b0effcb
erik <erik.flister@gmail.com>
2012-02-22 14:45:00 -0800

    teensy

--- classes/util/infrastructure/calibrateSingle.m
+++ classes/util/infrastructure/calibrateSingle.m
@@ -4,10 +4,14 @@ duration = .03;
 gap = .25;
 accuracy = .1;
 
+[pathstr, name, ext] = fileparts(mfilename('fullpath'));
+addpath(fullfile(fileparts(fileparts(fileparts(pathstr))),'bootstrap'));
+setupEnvironment;
+
 start = input('\nenter the water level (ml)\n');
 calibrateLocal( n, [0 duration 0], gap, accuracy );
 stop = input('\nenter the water level (ml)\n');
 
 rate = 10*(start - stop)/(duration*n); % ul/10ms
-fprintf('volume per 10ms on left side was %gul (that means 2ml in 150 rewards needs %dms each)\n',rate,round((2*10^4)/(150*rate)));
+fprintf('volume per 10ms was %gul\nthat means 2ml in 150 rewards needs %dms each\n',rate,round((2*10^4)/(150*rate)));
 end

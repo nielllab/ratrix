e7716e16e1aa6f5bd8e0c99f9217db543a8ce103
erik <erik.flister@gmail.com>
2012-03-15 14:49:28 -0700

    init ratrixData->mouseData

--- setup/util/init.m
+++ setup/util/init.m
@@ -2,5 +2,5 @@ function r=init
 [pathstr, name, ext] = fileparts(mfilename('fullpath'));
 addpath(fullfile(fileparts(fileparts(pathstr)),'bootstrap'));
 setupEnvironment
-dataPath=fullfile(fileparts(fileparts(getRatrixPath)),'ratrixData',filesep);
+dataPath=fullfile(fileparts(fileparts(getRatrixPath)),'mouseData',filesep);
 r=ratrix(fullfile(dataPath, 'ServerData'),0); %load from file

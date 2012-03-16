1d5caac0fe57ade77df241680f7a72449c7ec11f
erik <erik.flister@gmail.com>
2012-03-15 20:27:00 -0700

    fix ball analysis

--- analysis/liza/plotCrossingTime.m
+++ analysis/liza/plotCrossingTime.m
@@ -95,41 +95,100 @@ if ~all((res == 2) == (loc == 1))
     warning('manual kills didn''t line up with empty corrects')
 end
 
-%these trials each have two phases -- a discrimination phase followed by a reinforcement phase
+%these trials each have 2-3 phases -- a possible "slow" phase, a discrimination phase, and a reinforcement phase
 %we take the actual measured reward duration from the reinforcement phase (currently quantized to frame boundaries)
 %from the discrimination phase, we take the x,y,t track location measurements
 %entries in 'times' are in seconds, corresponding to x,y entries in 'track'
 %we also pull out some other info just to do some consistency checking later
 [actualRewards nFrames times targ track results] = cellfun(@(x)g(x),{records.phaseRecords},'UniformOutput',false);
     function [a n t targ track r]=g(x)
-        if length(x)~=2
-            error('expected 2 phases')
+        if ~any(length(x)==[2 3])
+            error('expected 2 or 3 phases')
         end
         
-        a = x(2).actualRewardDurationMSorUL;
-        n = x(1).dynamicDetails.nFrames;
-        t = x(1).dynamicDetails.times;
-        if ~all(diff(t(~isnan(t)))>0)
-            error('track timestamps aren''t increasing')
+        phasesDone = false(1,3);
+        for i=1:length(x)
+            if ~all(phasesDone(2:i-1))
+                error('phases out of order')
+            end
+            
+            if isempty(x(i).phaseType) %k-q'd before got to this phase?
+                if any(phasesDone)
+                    switch find(phasesDone,1,'last')
+                        case 1
+                            x(i).phaseType='discrim';
+                        case 2
+                            x(i).phaseType='reinforced';
+                        otherwise
+                            error('huh')
+                    end
+                else
+                    error('huh')
+                end
+            end
+            
+            switch x(i).phaseType
+                case 'pre-request'
+                    setPhase(1);
+                    
+                    % TODO: here's where you should add analysis of the "slow" track
+                    % load in the track from this phase following example from the discrim phase below
+                case 'discrim'
+                    setPhase(2);
+                    
+                    n     = doField(x(i).dynamicDetails,'nFrames');
+                    t     = doField(x(i).dynamicDetails,'times');
+                    targ  = doField(x(i).dynamicDetails,'target');
+                    track = doField(x(i).dynamicDetails,'track');
+                    r     = doField(x(i).dynamicDetails,'result');
+                    
+                    if ~all(diff(t(~isnan(t)))>0)
+                        error('track timestamps aren''t increasing')
+                    end
+                    
+                    if ~isempty(r)
+                        if any(cellfun(@(x)any(isnan(x)),{t track(:)}))
+                            error('should only happen when there is no result (manual kills)')
+                        end
+                    else
+                        r = '';
+                        test=repmat(isnan(t),2,1) == isnan(track);
+                        if ~all(test(:))
+                            error('test and track didn''t have matching nans')
+                        end
+                        if length(find(diff(isnan(t)))) > 1
+                            error('should be at most one transition from non-nans to nans')
+                        end
+                        t=t(~isnan(t));
+                        track=track(:,~isnan(t));
+                    end
+                case 'reinforced'
+                    setPhase(3);
+                    
+                    a = x(i).actualRewardDurationMSorUL;
+                otherwise
+                    x(i).phaseType
+                    error('huh')
+            end
         end
-        targ = x(1).dynamicDetails.target;
-        track = x(1).dynamicDetails.track;
-        if isfield(x(1).dynamicDetails,'result')
-            r = x(1).dynamicDetails.result;
-            if any(cellfun(@(x)any(isnan(x)),{t track(:)}))
-                error('should only happen when there is no result (manual kills)')
+        if ~all(phasesDone(2:end))
+            error('missing phases')
+        end
+        
+        function setPhase(sp)
+            if ~phasesDone(sp)
+                phasesDone(sp)=true;
+            else
+                error('duplicate phase!')
             end
-        else
-            r = '';
-            test=repmat(isnan(t),2,1) == isnan(track);
-            if ~all(test(:))
-                error('test and track didn''t have matching nans')
+        end
+        
+        function out = doField(x,f)
+            if isfield(x,f)
+                out = x.(f);
+            else
+                out = [];
             end
-            if length(find(diff(isnan(t)))) > 1
-                error('should be at most one transition from non-nans to nans')
-            end
-            t=t(~isnan(t));
-            track=track(:,~isnan(t));
         end
     end
 
@@ -139,6 +198,12 @@ end
 
 %no entry was made if there was no motion (and there were missed frames), so the raw number of entries slightly distorts actual duration
 len = cellfun(@length,times);
+times = cellfun(@fixTimes,times,'UniformOutput',false);
+    function x=fixTimes(x)
+        if isempty(x)
+            x=[0 0]; %diff(0) is [];
+        end
+    end
 dur = cellfun(@(x)1000*diff(x([1 end])),times);
 
 % i'm seeing a lot of trials (12%) that have only one timestamp
@@ -148,16 +213,23 @@ if false %this shows that they position really is past the wall on the first tim
     plot(x(1,:),x(2,:),'.')
 end
 
+records = arrayfun(@fixStimDetails,records);
+    function x=fixStimDetails(x)
+        if ~isfield(x.stimDetails,'correctionTrial')
+            x.stimDetails.correctionTrial = [];
+        end
+    end
+
 %verify the following info matches consistently
 s = [records.stimDetails];
 timeout = [s.nFrames];
 targetLocation = [s.target];
 
-if ~all(cell2mat(nFrames)==timeout) % this was the limit on the trial length -- the # of position changes
+if ~all(cellfun(@(x,y)isempty(x) || x==y,nFrames,mat2cell(timeout,1,ones(1,length(timeout))))) % this was the limit on the trial length -- the # of position changes
     error('nFrames didn''t match timeout')
 end
 
-if ~all(cell2mat(targ)==targetLocation)
+if ~all(cellfun(@(x,y)isempty(x) || x==y,targ,mat2cell(targetLocation,1,ones(1,length(targetLocation)))))
     error('targetLocation and targ didn''t match')
 end
 
@@ -165,11 +237,23 @@ if ~all(cellfun(@isempty,results) == (res == 2))
     error('empty results didn''t line up with manual kills')
 end
 
-[~,loc]=ismember(results,unique(results));
-
-if ~all(((loc==3) == (res==0)) & ((loc==2) == (res==1)) & ((loc==1) == (res==2)))
-    error('correct/timedout results didn''t line up with correctness')
+if ~all(cellfun(@checkCorrect,results,mat2cell(res,1,ones(1,length(res)))))
+    error('correct/incorrect/timedout results didn''t line up with correctness')
 end
+
+    function out=checkCorrect(x,y)
+        out = false;
+        switch x
+            case ''
+                out = y==2;
+            case 'correct'
+                out = y==1;
+            case {'incorrect','timedout'}
+                out = y==0;
+            otherwise
+                error('unexpected result')
+        end
+    end
 
 %this is set whenever you hit k-ctrl-# to manually open valve
 manualRewards = [records.containedForcedRewards];
@@ -308,7 +392,9 @@ end
 eps=min(dur(dur>0))/10;
 p(trialNums,dur,'g.')
 hold on
-p(trialNums(dur==0),eps,'g+'); % semilogy on 0 fails (would rather draw these off axis, but how do this + survive figure resizing?)
+if any(dur==0)
+    p(trialNums(dur==0),eps,'g+'); % semilogy on 0 fails (would rather draw these off axis, but how do this + survive figure resizing?)
+end
 xd=trialNums(~isnan(dur));
 pTiles(pTiles==0)=eps;
 rangePlot(xd,pTiles([1 end],:));

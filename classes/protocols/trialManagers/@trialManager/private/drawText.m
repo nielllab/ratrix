6a906aa656142e3d5b1f61fee8ac45d888221aa9
erik <erik.flister@gmail.com>
2012-04-11 15:45:44 -0700

    text ports

--- classes/protocols/trialManagers/@trialManager/private/drawText.m
+++ classes/protocols/trialManagers/@trialManager/private/drawText.m
@@ -1,5 +1,5 @@
 function xTextPos = drawText(tm, window, labelFrames, subID, xOrigTextPos, yTextPos, normBoundsRect, stimID, protocolStr, ...
-    textLabel, trialLabel, i, frameNum, manual, didManual, didAPause, ptbVersion, ratrixVersion, numDrops, numApparentDrops, phaseInd, phaseType,textType)
+    textLabel, trialLabel, i, frameNum, manual, didManual, didAPause, ptbVersion, ratrixVersion, numDrops, numApparentDrops, phaseInd, phaseType,textType,ports)
 
 %DrawFormattedText() won't be any faster cuz it loops over calls to Screen('DrawText'), tho it would clean this code up a bit.
 
@@ -10,9 +10,8 @@ switch textType
         if labelFrames
             [xTextPos] = Screen('DrawText',window,['ID:' subID ],xOrigTextPos,yTextPos,brightness*ones(1,3));
             xTextPos=xTextPos+50;
-            [garbage,yTextPos] = Screen('DrawText',window,['trlMgr:' class(tm) ' stmMgr:' stimID  ' prtcl:' protocolStr ],xTextPos,yTextPos,brightness*ones(1,3));
+            doStr(['trlMgr:' class(tm) ' stmMgr:' stimID  ' prtcl:' protocolStr ]);
         end
-        yTextPos=yTextPos+1.5*normBoundsRect(4);
         
         if labelFrames
             if iscell(textLabel)  % this is a reoccuring cost per frame... could be before the loop... pmm
@@ -25,40 +24,39 @@ switch textType
             else
                 phaseTypeDisplay=phaseType;
             end
-            [garbage,yTextPos] = Screen('DrawText',window,sprintf('prty:%g %s stmInd:%d frm:%d drps:%d(%d), phsInd:%d strtgy:%s',Priority(),trialLabel,i,frameNum,numDrops,numApparentDrops,phaseInd,phaseTypeDisplay),xTextPos,yTextPos,brightness*ones(1,3));
-            yTextPos=yTextPos+1.5*normBoundsRect(4);
+            doStr(sprintf('prty:%g %s stmInd:%d frm:%d drps:%d(%d), phsInd:%d strtgy:%s',Priority(),trialLabel,i,frameNum,numDrops,numApparentDrops,phaseInd,phaseTypeDisplay));
             
-            [garbage,yTextPos] = Screen('DrawText',window,sprintf('stim:%s',txtLabel),xTextPos,yTextPos,brightness*ones(1,3));
-            yTextPos=yTextPos+1.5*normBoundsRect(4);            
+            doStr(sprintf('stim:%s',txtLabel));
             
-            [garbage,yTextPos] = Screen('DrawText',window,sprintf('ptb:%s',ptbVersion),xTextPos,yTextPos,brightness*ones(1,3));
-            yTextPos=yTextPos+1.5*normBoundsRect(4);
+            doStr(sprintf('ptb:%s',ptbVersion));
             
-            [garbage,yTextPos] = Screen('DrawText',window,sprintf('ratrix:%s',ratrixVersion),xTextPos,yTextPos,brightness*ones(1,3));
-            yTextPos=yTextPos+1.5*normBoundsRect(4);
+            doStr(sprintf('ratrix:%s',ratrixVersion));
+            
+            doStr(sprintf('ports:%s',int2str(ports))); %slow?
         end
     case 'light'
-        [garbage,yTextPos] = Screen('DrawText',window,sprintf('%s stimInd:%d frame:%d drops:%d(%d)',trialLabel,i,frameNum,numDrops,numApparentDrops),xTextPos,yTextPos,brightness*ones(1,3));
-        yTextPos=yTextPos+1.5*normBoundsRect(4);
+        doStr(sprintf('%s stimInd:%d frame:%d drops:%d(%d)',trialLabel,i,frameNum,numDrops,numApparentDrops));
     otherwise
         error('unsupported')
 end
+
+    function doStr(str)
+        [~,yTextPos] = Screen('DrawText',window,str,xTextPos,yTextPos,brightness*ones(1,3));
+        yTextPos = yTextPos+1.5*normBoundsRect(4);
+    end
 
 if manual
     manTxt='on';
 else
     manTxt='off';
 end
+
 if didManual
-    [garbage,yTextPos] = Screen('DrawText',window,sprintf('trial record will indicate manual poking on this trial (k+m to toggle for next trial: %s)',manTxt),xTextPos,yTextPos,brightness*ones(1,3));
-    yTextPos=yTextPos+1.5*normBoundsRect(4);
+    doStr(sprintf('trial record will indicate manual poking on this trial (k+m to toggle for next trial: %s)',manTxt));
 end
 
 if didAPause
-    %[garbage,yTextPos] = ...
-    Screen('DrawText',window,'trial record will indicate a pause occurred on this trial',xTextPos,yTextPos,brightness*ones(1,3));
-    %yTextPos=yTextPos+1.5*normBoundsRect(4);
+    doStr('trial record will indicate a pause occurred on this trial');
 end
 
-
-end % end function
+end

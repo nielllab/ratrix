71142199e8d8d24a7e567ec15d55e81391114deb
eflister <erik.flister@gmail.com>
2012-08-02 15:55:18 -0700

    tiny

--- setup/util/setupMice.m
+++ setup/util/setupMice.m
@@ -1,10 +1,27 @@
 function setupMice
-names = {'3350'};
+%names = {'3350'};
 %names = {'test','j10lt','j10ln','j8lt','j8ln'};
 %names = {'test','j10rt','j8rt','j7rt','j6rt'};
 
-ratrixPath = 'C:\Users\nlab\Desktop\wehrData';
-for i=1:length(names)
-    standAloneRun(ratrixPath,'setProtocolWehr',names{i},[],[],true)
+%names = {'c2rt','c1rt'};
+%names = {'c2rn','c1rn'};
+names = {'c1rt'};
+
+lab = 'wehr';
+
+switch lab
+    case 'wehr'
+        ratrixPath = 'C:\Users\nlab\Desktop\wehrData';
+        p = 'setProtocolWehr';
+    case 'niell'
+        ratrixPath = 'C:\Users\nlab\Desktop\mouseData0512';
+        p = 'setProtocolCMN';
+    otherwise
+        error('huh?')
 end
+
+for i=1:length(names)
+    standAloneRun(ratrixPath,p,names{i},[],[],true)
+end
+
 end

841d4747ed630c9e7e7a3cff81b57d16fa035de6
erik <erik.flister@gmail.com>
2012-11-06 13:59:21 -0800

    updated to include wehr ratrix comp 2 in makedefaultstation

--- classes/util/infrastructure/makeDefaultStation.m
+++ classes/util/infrastructure/makeDefaultStation.m
@@ -130,7 +130,7 @@ if a
     switch b
         case {'F46D04EFE0FF','5404A6EF6720','14DAE971D50E'}
             stationSpec.portSpec.valveSpec=int8(3); %mini-3-way-lickometer and ball only use center valve
-        case {'08002700D40D','C860005FBB51', 'C860005FBCBE'} %aldis' station 
+        case {'08002700D40D','C860005FBB51', 'C860005FBCBE'} % aldis' station 
             stationSpec.laserPin = uint8(9);
             stationSpec.framePin = [];%remove
     end

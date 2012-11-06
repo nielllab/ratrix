20dc25210113cf17825879cb58a743690a2f17ad
erik <erik.flister@gmail.com>
2012-11-05 17:34:55 -0800

    added new case in makedefaultstation for wehr ratrix2 computer

--- classes/util/infrastructure/makeDefaultStation.m
+++ classes/util/infrastructure/makeDefaultStation.m
@@ -130,9 +130,9 @@ if a
     switch b
         case {'F46D04EFE0FF','5404A6EF6720','14DAE971D50E'}
             stationSpec.portSpec.valveSpec=int8(3); %mini-3-way-lickometer and ball only use center valve
-        case {'08002700D40D','C860005FBB51'} %aldis' station 
+        case {'08002700D40D','C860005FBB51', 'C860005FBCBE'} %aldis' station 
             stationSpec.laserPin = uint8(9);
-            %stationSpec.framePin = [];%remove
+            stationSpec.framePin = [];%remove
     end
 end
 

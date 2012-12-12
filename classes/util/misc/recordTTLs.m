6dc2bf07597d7b4b5523583f0439e00c7eaba11e
nlab <nlab@nlab.com>
2012-12-12 10:53:58 -0800

    more scanimage ttl recording

--- classes/util/misc/recordTTLs.m
+++ classes/util/misc/recordTTLs.m
@@ -2,13 +2,14 @@ function recordTTLs(eventName, eventData)
 %ScanImage 3.8 user function
 %adapted from hantman_stimPulse
 
-dbstop if error %ignored?
+dbstop if error %ignored
 
 persistent hDin hSys ttlChans
 
 global state
 
 numLines = 2;
+taskName = 'TTLrec';
 
 switch eventName
     
@@ -19,7 +20,6 @@ switch eventName
         end
         
         if isempty(hDin) || ~isvalid(hDin)
-            taskName = 'TTLrec';
             if hSys.taskMap.isKey(taskName) %This shouldn't happen in usual operation
                 delete(hSys.taskMap(taskName));
             end
@@ -27,17 +27,25 @@ switch eventName
             
             % Dev1 PCI-6110 (s series, PMT inputs, mirror + shutter outputs) has 8 DIO (software timed)
             % Dev2 PCIe-6321 (x series, pockels output) has 24 DIO (8 hardware-timed up to 1 MHz -- port0 hardware timed, ports1-2 PFI-able)
+            
+            % is it possible to give lines their own helpful names?
             ttlChans = hDin.createDIChan('Dev2',['port0/line0:' num2str(numLines-1)],'ttlFramePhase','DAQmx_Val_ChanForAllLines');
             
             hz = 10000; %frame pulses definitely > 100us
-            mins = .5; %?
+            mins = 3 * 60; %how choose this buffer size?  it is system RAM and in circular buffer that gets overwritten.  have to make it 3 hrs long?
             
             % how make sure we get hardware timed?  and same clock as PMT scans?
+            source = state.init.hAI.sampClkSrc; % /Dev1/ai/SampleClockTimebase -- errors:
+            % NI DAQmx error (-89136) in call to API function 'DAQmxStartTask':
+            % Specified route cannot be satisfied, because the hardware does not support it.
             
-            source = state.init.hAI.sampClkSrc; % /Dev1/ai/SampleClockTimebase
-            hDin.cfgSampClkTiming(hz, 'DAQmx_Val_ContSamps', round(hz * 60 * mins)); % , source);
+            source = '/Dev2/RTSI7'; % doesn't error, but when we go to read data, acts like it never started
+            source = [];
+            hDin.cfgSampClkTiming(hz, 'DAQmx_Val_ContSamps', round(hz * 60 * mins), source);
             
-            hDin.cfgDigEdgeStartTrig(state.init.triggerInputTerminal); %PFI0
+            hDin.set('sampClkTimebaseRate', 20e6, 'sampClkTimebaseSrc', 'RTSI7');
+            
+            hDin.cfgDigEdgeStartTrig(state.init.triggerInputTerminal); % PFI0
         end
         
         if ~hDin.isTaskDone()
@@ -49,49 +57,73 @@ switch eventName
         
     case {'acquisitionDone' 'abortAcquisitionEnd'}
         
-        eventName
+        % eventName
         
         try
             uints = 2.^(3:5);
             u = find(numLines <= uints,1,'first');
             bits = uints(u);
             if isempty(u)
-                type = 'logical';
+                type = 'logical'; % will be 1 byte/sample/line -- uints pack them better
             else
                 type = ['uint' num2str(bits)];
             end
             
             % logical doesn't work -- we get uint8's.  unit8 doesn't work, we get uint16's!
             [outputData, sampsPerChanRead] = readDigitalData(hDin, inf, type);
-            % if logical: (will be 1 byte/sample/line -- uints pack them better)
-            % columns = channels
-            % rows = samples * lines
+            % any way to stream these to disk during acquisition?
             
             if strcmp(type,'uint8') && isa(outputData,'uint16')
                 % for now, pass
             elseif ~isa(outputData,type)
                 class(outputData)
-                error('huh?')
+                type
+                error('didn''t get requested type')
+            end
+            
+            if ~isvector(outputData)
+                size(outputData,2)
+                error('we only do one channel for now (it can have many lines)')
             end
             
             switch type
                 case 'logical'
                     if sampsPerChanRead * numLines ~= size(outputData,1)
-                        error('huh?')
+                        size(outputData)
+                        sampsPerChanRead
+                        error('unexpected sampsPerChanRead')
                     end
+                    
+                    % columns = channels
+                    % rows = samples * lines
+                    
+                    bin = reshape(outputData,[sampsPerChanRead/numLines numLines]);
                 otherwise
                     if sampsPerChanRead ~= size(outputData,1)
-                        error('huh?')
+                        size(outputData)
+                        sampsPerChanRead
+                        error('unexpected sampsPerChanRead')
                     end
+                    
+                    bin = fliplr(dec2bin(outputData,bits)=='1');
+                    bin = bin(:,1:numLines);
             end
             
-            figure
-            bin = fliplr(dec2bin(outputData,bits)=='1');
-            bin = bin(:,1:numLines);
-            plot(bin + .1*repmat(1:numLines,size(bin,1),1))
+            figure;
+            fact = .1;
+            plot(bin + fact*repmat(1:numLines,size(bin,1),1));
             % how get corresponding times/frames?
+            % could be in state.acq.frameRate: 1.4796
+            %             state.acq.linesPerFrame: 256
+            %             state.acq.pixelsPerLine: 256
+            %             state.acq.pixelTime: 6.4000e-006
             
-            keyboard
+            ylim([0 fact*(numLines+1)]);
+            set(gca,'YTick',[]);
+            title('TTL in');
+            
+            % keyboard
+            
         catch e
             % after first iteration of LOOP, or on abortAcquisitionEnd, hDin is no good, can't read
             
@@ -101,7 +133,8 @@ switch eventName
             % Reserve, commit or start the task prior to getting the property.
             
             getReport(e)
-            % keyboard
+            keyboard
+            rethrow(e);
         end
         
         %Stop Task, to allow it to be re-used on next acquisition start
@@ -111,5 +144,5 @@ switch eventName
         end
         
     otherwise
-        fprintf('%s got %s, but didn''t handle it\n',mfilename,eventName)
+        fprintf('%s got %s, but didn''t handle it\n',mfilename,eventName);
 end

8e17cc68ddb620937f5dfddf2841f094a3fab984
erik <erik.flister@gmail.com>
2012-12-12 17:31:12 -0800

    stuff for gcam13 ball mice

---
+++ setup/setProtocolGTS.m
@@ -0,0 +1,85 @@
+function r = setProtocolMouse(r,subjIDs)
+
+if ~isa(r,'ratrix')
+    error('need a ratrix')
+end
+
+if ~all(ismember(subjIDs,getSubjectIDs(r)))
+    error('not all those subject IDs are in that ratrix')
+end
+
+sm=makeStandardSoundManager();
+
+rewardSizeULorMS          =50;
+requestRewardSizeULorMS   =0;
+requestMode               ='first';
+msPenalty                 =3500;
+fractionOpenTimeSoundIsOn =1;
+fractionPenaltySoundIsOn  =1;
+scalar                    =1;
+msAirpuff                 =msPenalty;
+
+% sca
+% keyboard
+
+if ~isscalar(subjIDs)
+    error('expecting exactly one subject')
+end
+switch subjIDs{1}
+    case 'gcam13tt'
+        requestRewardSizeULorMS = 0;
+   case 'gcam13ln'
+        requestRewardSizeULorMS = 0;
+    otherwise
+        warning('unrecognized mouse, using defaults')
+end
+
+noRequest = constantReinforcement(rewardSizeULorMS,requestRewardSizeULorMS,requestMode,msPenalty,fractionOpenTimeSoundIsOn,fractionPenaltySoundIsOn,scalar,msAirpuff);
+
+percentCorrectionTrials = .5;
+
+maxWidth  = 1920;
+maxHeight = 1080;
+
+[w,h] = rat(maxWidth/maxHeight);
+textureSize = 10*[w,h];
+zoom = [maxWidth maxHeight]./textureSize;
+
+svnRev = {'svn://132.239.158.177/projects/ratrix/trunk'};
+svnCheckMode = 'session';
+
+interTrialLuminance = .5;
+
+stim.gain = 1.4 * ones(2,1);
+stim.targetDistance = 500 * ones(1,2);
+stim.timeoutSecs = 10;
+stim.slow = [40; 80]; % 10 * ones(2,1);
+stim.slowSecs = 1;
+stim.positional = false;
+stim.cue = true;
+stim.soundClue = false;
+
+pixPerCycs             = [100]; %*10^9;
+targetOrientations     = [-1 1]*pi/4;
+distractorOrientations = []; %-targetOrientations;
+mean                   = .5;
+radius                 = .35;
+contrast               = 1;
+thresh                 = .00005;
+yPosPct                = .5;
+stim.stim = orientedGabors(pixPerCycs,targetOrientations,distractorOrientations,mean,radius,contrast,thresh,yPosPct,maxWidth,maxHeight,zoom,interTrialLuminance);
+%stim.stim = orientedGabors(pixPerCycs,targetOrientations,distractorOrientations,mean,radius,[-1 1]  ,thresh,yPosPct,maxWidth,maxHeight,zoom,interTrialLuminance,'none', 'normalizeDiagonal');
+
+%stim.stim = 'flip';
+%stim.stim=nan;
+
+ballSM = trail(stim,maxWidth,maxHeight,zoom,interTrialLuminance);
+ballTM = ball(percentCorrectionTrials,sm,noRequest);
+ts1 = trainingStep(ballTM, ballSM, repeatIndefinitely(), noTimeOff(), svnRev, svnCheckMode); %ball
+
+p=protocol('mouse',{ts1});
+
+stepNum=uint8(1);
+subj=getSubjectFromID(r,subjIDs{1});
+[subj r]=setProtocolAndStep(subj,p,true,false,true,stepNum,r,'LY01 (40,80), R=36','edf');
+end

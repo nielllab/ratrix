43a3cd1c72f238042ea9129166f76328bed41409
joe <joseph.wekselblatt@gmail.com>
2013-07-16 17:56:52 -0700

    Kibrom: I took away the stop rewards for gcam39rt,39tt,40lt,43lt,44lt,45tt,53ln,53rt. I also increased all their water rewards too 120. I also did this for g54A-11rt and tt.

--- setup/setProtocolBallHvV_center_vertical.m
+++ setup/setProtocolBallHvV_center_vertical.m
@@ -42,10 +42,40 @@ switch subjIDs{1}
         requestRewardSizeULorMS = 0;
    case 'gcam30rn'
         requestRewardSizeULorMS = 0;
-        rewardSizeULorMS        = 100;
+        rewardSizeULorMS        = 120;
    case 'gcam35lt'
         requestRewardSizeULorMS = 0;
-        rewardSizeULorMS        = 100;    
+        rewardSizeULorMS        = 120;    
+   case 'gcam39rt'
+        requestRewardSizeULorMS = 0;
+        rewardSizeULorMS        = 120;
+   case 'gcam39tt'
+        requestRewardSizeULorMS = 0;
+        rewardSizeULorMS        = 120; 
+   case 'gcam40lt'
+        requestRewardSizeULorMS = 0;
+        rewardSizeULorMS        = 120;
+   case 'gcam43lt'
+        requestRewardSizeULorMS = 0;
+        rewardSizeULorMS        = 120;
+   case 'gcam44lt'
+        requestRewardSizeULorMS = 0;
+        rewardSizeULorMS        = 120;
+   case 'gcam45tt'
+        requestRewardSizeULorMS = 0;
+        rewardSizeULorMS        = 120;
+   case 'gcam53ln'
+        requestRewardSizeULorMS = 0;
+        rewardSizeULorMS        = 120;
+   case 'gcam53rt'
+        requestRewardSizeULorMS = 0;
+        rewardSizeULorMS        = 120;  
+   case 'g54a11rt'
+        requestRewardSizeULorMS = 0;
+        rewardSizeULorMS        = 120;   
+   case 'g54a11tt'
+        requestRewardSizeULorMS = 0;
+        rewardSizeULorMS        = 120;     
   % case 'g54a11rt'
    %     requestRewardSizeULorMS = 0;
     %    rewardSizeULorMS        = 100;

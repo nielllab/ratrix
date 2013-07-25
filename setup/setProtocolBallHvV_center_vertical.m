76434ceef2e0519f1d7220b0802e4408e30dc9fb
joe <joseph.wekselblatt@gmail.com>
2013-07-24 18:13:25 -0700

    kibrom

--- setup/setProtocolBallHvV_center_vertical.m
+++ setup/setProtocolBallHvV_center_vertical.m
@@ -47,37 +47,37 @@ switch subjIDs{1}
         rewardSizeULorMS        = 120;
    case 'gcam35lt'
         requestRewardSizeULorMS = 0;
-        rewardSizeULorMS        = 120;    
+        rewardSizeULorMS        = 110;    
    case 'gcam39rt'
         requestRewardSizeULorMS = 30;
         rewardSizeULorMS        = 120;
    case 'gcam39tt'
         requestRewardSizeULorMS = 0;
-        rewardSizeULorMS        = 120; 
+        rewardSizeULorMS        = 110; 
    case 'gcam40lt'
         requestRewardSizeULorMS = 30;
         rewardSizeULorMS        = 120;
    case 'gcam43lt'
         requestRewardSizeULorMS = 0;
-        rewardSizeULorMS        = 120;
+        rewardSizeULorMS        = 110;
    case 'gcam44lt'
         requestRewardSizeULorMS = 0;
-        rewardSizeULorMS        = 120;
+        rewardSizeULorMS        = 110;
    case 'gcam45tt'
         requestRewardSizeULorMS = 0;
-        rewardSizeULorMS        = 120;
+        rewardSizeULorMS        = 110;
    case 'gcam53ln'
         requestRewardSizeULorMS = 0;
-        rewardSizeULorMS        = 120;
+        rewardSizeULorMS        = 110;
    case 'gcam53rt'
         requestRewardSizeULorMS = 0;
-        rewardSizeULorMS        = 120;  
+        rewardSizeULorMS        = 110;  
    case 'g54a11rt'
         requestRewardSizeULorMS = 30;
-        rewardSizeULorMS        = 120;   
+        rewardSizeULorMS        = 110;   
    case 'g54a11tt'
         requestRewardSizeULorMS = 0;
-        rewardSizeULorMS        = 120;     
+        rewardSizeULorMS        = 110;     
   % case 'g54a11rt'
    %     requestRewardSizeULorMS = 0;
     %    rewardSizeULorMS        = 100;

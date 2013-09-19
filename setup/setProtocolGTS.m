2c078dfc722cc56ca7a13d6d1d7fe53b37233707
erik <erik.flister@gmail.com>
2013-09-19 10:23:52 -0700

    kibrom

--- setup/setProtocolGTS.m
+++ setup/setProtocolGTS.m
@@ -34,7 +34,7 @@ switch subjIDs{1}
 %         rewardSizeULorMS          =80;
    case 'gcam32tt'
         requestRewardSizeULorMS = 0;
-        rewardSizeULorMS        = 40;
+        rewardSizeULorMS        = 35;
    case 'gcam32ln'
         requestRewardSizeULorMS = 0;
         rewardSizeULorMS        = 45;

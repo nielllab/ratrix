6cafd0bcc6a433accb0ecc92a2e3b9f0d571932a
erik <erik.flister@gmail.com>
2014-01-07 11:35:49 -0800

    Ryan

--- setup/setProtocolGTS.m
+++ setup/setProtocolGTS.m
@@ -50,7 +50,7 @@ switch subjIDs{1}
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 60;
        
-       case 'bfly1a.5lt' 
+   case 'bfly1a.5lt' 
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 130;
 

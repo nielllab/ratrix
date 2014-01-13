07a69a05783b5f3142610c5eac0863544e305b11
erik <erik.flister@gmail.com>
2014-01-13 12:49:22 -0800

    libby

--- setup/setProtocolGTS.m
+++ setup/setProtocolGTS.m
@@ -41,7 +41,7 @@ switch subjIDs{1}
         
    case 'g62b4ln'
        requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 45;
+       rewardSizeULorMS        = 55;
        msPenalty                 =3500; 
 
       

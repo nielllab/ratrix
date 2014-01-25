f80095124082a580c95f76b4c77946a9c54ed063
erik <erik.flister@gmail.com>
2014-01-24 17:33:22 -0800

    libby

--- setup/setProtocolBallHvV_center_vertical.m
+++ setup/setProtocolBallHvV_center_vertical.m
@@ -46,7 +46,7 @@ case 'g62b7lt'           %started HvV_cent 1/21/14
 case 'g62b1lt'     %moved to HvV_center 1/4/14   
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 160;
-       msPenalty                 =3000; 
+       msPenalty                 =3500; 
       
 %    case 'g54a11rt'   %changed to GoToBlack 12/10/13
 %         requestRewardSizeULorMS = 0;

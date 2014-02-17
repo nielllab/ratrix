d8bf1ba75d15b4065af4c76ac6f8605b2b9d5612
erik <erik.flister@gmail.com>
2014-02-17 12:14:54 -0800

    libby

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -22,13 +22,14 @@ msAirpuff                 =msPenalty;
 
 switch subjIDs{1}
    
-     case 'g62b8tt'       %started 2/17/14
-       requestRewardSizeULorMS = 20;
-       rewardSizeULorMS        = 160; 
-       msPenalty               =3500;
 
        
-     case 'g6w5rt'     %started 2/17/14  
+       case 'g6w5rt'     %started 2/17/14  
+       requestRewardSizeULorMS = 20;
+       rewardSizeULorMS        = 160;
+        msPenalty               =3500;
+        
+       case 'g62b8tt'     %started 2/17/14  
        requestRewardSizeULorMS = 20;
        rewardSizeULorMS        = 160;
         msPenalty               =3500;

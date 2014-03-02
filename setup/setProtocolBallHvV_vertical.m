5937b865d3cf9d6c7fbc16ec75a2d23e06ef0127
erik <erik.flister@gmail.com>
2014-03-02 14:22:34 -0800

    Rayn

--- setup/setProtocolBallHvV_vertical.m
+++ setup/setProtocolBallHvV_vertical.m
@@ -30,7 +30,7 @@ end
   case 'g62b1lt'     %moved to HvV_vertical 2/19/14   
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 140;
-       msPenalty                 =4000; 
+       msPenalty                 =4100; 
      
   case 'g62b3rt'          %changed 2/14/14
        requestRewardSizeULorMS = 0;

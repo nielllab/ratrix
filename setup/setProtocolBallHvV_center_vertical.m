39ec9002c27a3517f4d14d3e6a580f67ddaa30f6
erik <erik.flister@gmail.com>
2014-03-11 12:46:19 -0700

    libby

--- setup/setProtocolBallHvV_center_vertical.m
+++ setup/setProtocolBallHvV_center_vertical.m
@@ -45,17 +45,17 @@ switch subjIDs{1}
       case 'g62c.2rt'           %changed 1/30/14 
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 80; 
-       msPenalty               =3500;
+       msPenalty               =4000;
 
     
      case 'g62b8tt'     %started 2/24/14  
        requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 100;
+       rewardSizeULorMS        = 90;
         msPenalty              =4200;
         
     case 'g62b7lt'           %started HvV_cent 1/21/14 
        requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 65; 
+       rewardSizeULorMS        = 55; 
        msPenalty               =4000;    
        
        case 'g6w5rt'     %started 2/17/14  

25b2203b9474dad1052c656162c6747957671f49
Cris <crisniell@gmail.com>
2014-06-19 16:45:18 -0700

    chunk widefield

---
+++ analysis/imaging/analyzeWidefield.m
@@ -0,0 +1,43 @@
+filelistWhatWhere;
+
+%%% batchDfofMovie
+
+outpathname = 'D:\compiled behavior\behavior topos\';
+
+%use = find(strcmp({files.monitor},'vert') &  strcmp({files.notes},'good imaging session') &  strcmp({files.label},'camk2 gc6')&  strcmp({files.task},'HvV_center') &strcmp({files.subj},'g62b7lt'))
+%use = find(strcmp({files.monitor},'vert') &  strcmp({files.notes},'good imaging session') )
+%use = find(strcmp({files.monitor},'land')&     strcmp({files.label},'camk2 gc6'))
+
+alluse = find(strcmp({files.monitor},'vert')&  strcmp({files.notes},'good imaging session')  &    strcmp({files.label},'camk2 gc6') &  strcmp({files.task},'HvV_center'))
+allsubj = unique({files(alluse).subj})
+
+for s = 1:1
+allsubj{s}
+%use = intersect(alluse,find(strcmp({files.subj},allsubj{s})))    
+use = alluse;
+
+%%% calculate gradients and regions
+clear map merge
+x0 =-10; y0=0; sz = 95;
+doTopography;
+
+% %%% overlay behavior on top of topomaps
+doBehavior;
+
+% %%% analyze looming
+% for f = 1:length(use)
+%     loom_resp{f}=fourPhaseOverlay(files(use(f)),pathname,outpathname,'loom');
+% end
+% fourPhaseAvg(loom_resp,allxshift+x0,allyshift+y0,allthetashift,zoom, sz, avgmap);
+
+
+%%% analyze grating
+% for f = 1:length(use)
+%  f
+%  grating_resp{f}=fourPhaseOverlay(files(use(f)),pathname,outpathname,'grating');
+% end
+% fourPhaseAvg(grating_resp,allxshift+x0,allyshift+y0, allthetashift,zoom*0.57, sz, avgmap);
+
+
+end
+

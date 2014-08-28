0d3b3dec59a1efe99b5fe0e7c7e36ccb9332b312
nlab <nlab@gmail.com>
2014-08-28 16:36:28 -0700

    patch

--- analysis/imaging/analyzeGratingPatch.m
+++ analysis/imaging/analyzeGratingPatch.m
@@ -1,30 +1,47 @@
-load(fname,'dfof_bg');
-load(moviefname);
+%load(fname,'dfof_bg');
+load('D:\gratingPatchesCloser');
 imagerate=10;
 
 
 imageT=(1:size(dfof_bg,3))/imagerate;
-img=deconvg6s(dfof_bg,1/imagerate);
-trials=trials(1:end-1);
+img = imresize(double(dfof_bg),0.5);
+
+xpos=xpos(1:300); ypos=ypos(1:300); sf=sf(1:300); tf=tf(1:300);
+trials = length(sf)-1;
+% tic
+% img=deconvg6s(dfof_bg,1/imagerate);
+% toc
+% trials=trials(1:end-1);
 
 acqdurframes = (duration+isi)*imagerate;
 figure
+clear cycavg
 for f=1:acqdurframes
-    cycavg(:,:,f) = median(img(:,:,f:acqdurframes:end),3);
-    subplot(4,3,f)
-    imagesc(squeeze(cycavg(:,:,f)),[0 0.1])
+    cycavg(:,:,f) = mean(img(:,:,f:acqdurframes:end),3);
+    subplot(6,5,f)
+    imagesc(squeeze(cycavg(:,:,f)),[-0.01 0.01])
+    axis off
 end
 
-useframes = 2:6;
-base = -2:0;
+useframes = 6:8;
+base = 1:3;
+trialdata = zeros(size(img,1),size(img,2),trials+1);
+
 for tr=1:trials;
-    t0 = (t-1)*duration*imagerate+t*isi*imagerate+1;
+    t0 = (tr-1)*duration*imagerate+tr*isi*imagerate+1;
     baseframes = base+t0; baseframes=baseframes(baseframes>0);
     trialdata(:,:,tr)=mean(img(:,:,useframes+t0),3) -mean(img(:,:,baseframes),3);
 end
 
+if length(unique(xpos))>1
 [ph amp tuning] = getPixelTuning(trialdata,xpos,'X');
+end
+if length(unique(ypos))>1
 [ph amp tuning] = getPixelTuning(trialdata,ypos,'Y');
-[ph amp tuning] = getPixelTuning(trialdata,sf,'SF');
+end
+if length(unique(sf))>1
+    [ph amp tuning] = getPixelTuning(trialdata,sf,'SF');
+end
+if length(unique(tf))>1
 [ph amp tuning] = getPixelTuning(trialdata,tf,'TF');
-
+end
--- analysis/imaging/widefield functions/getPixelTuning.m
+++ analysis/imaging/widefield functions/getPixelTuning.m
@@ -4,22 +4,42 @@ xrange = unique(xpos);
 for x=1:length(xrange);
     xtuning(:,:,x) = median(trialdata(:,:,find(xpos==xrange(x))),3);
 end
+xtuning(xtuning<0)=0;
 baseline = min(xtuning,[],3);
+if length(xrange)<=2
+    baseline=0;
+end
+
 amp = max(xtuning,[],3);
+xphase=0; total=0;
 for x=1:length(xrange)
     xphase = xphase+(xtuning(:,:,x)-baseline)*x;
     total = total+ (xtuning(:,:,x)-baseline);
 end
-xphase=xphase./total;
+xphase=xphase./(total+0.0001);
 figure
 subplot(2,2,1)
-imagesc(xphase);
-im = mat2im(xphase,jet,[1 length(xrange)]);
-im = im.*amp/prctile(amp(:),95);
-subplot(2,2,2)
+imagesc(xphase,[1.5 length(xrange)-0.5]); colormap hsv
+im = mat2im(xphase,hsv,[1.5 length(xrange)-0.5]);
+amp = amp/prctile(amp(:),80);
+amp(amp>1)=1;
+im = im.*(repmat(amp,[1 1 3]));
+subplot(2,2,2);
 imshow(im);
 subplot(2,2,3)
 imagesc(amp);
 subplot(2,2,4)
 imagesc(total);
 title(label);
+
+phmap=figure;
+imagesc(xphase,[1 length(xrange)]); colormap hsv
+
+for i = 1:10
+    figure(phmap);
+    [y x] = ginput(1);
+    figure
+    plot(squeeze(xtuning(round(x),round(y),:)));
+    xphase(round(x),round(y))
+end
+   keyboard

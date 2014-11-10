58ea8a03e081f73eb9d7220a8253378e45a3b11d
erik <erik.flister@gmail.com>
2014-11-10 13:24:44 -0800

    libby

--- setup/setProtocolGTS.m
+++ setup/setProtocolGTS.m
@@ -32,29 +32,29 @@ switch subjIDs{1}
      
      case 'g62j.8lt' % Started 10/22/14
        requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 90;
+       rewardSizeULorMS        = 80;
        msPenalty               =4100;
 
      case 'g62m.1lt' % Started 10/22/14
        requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 90;
-       msPenalty               =4100;
+       rewardSizeULorMS        = 80;
+       msPenalty               =4000;
        
     case 'g62l1lt' % started GTS Learning paradigm on 10/12/14
        requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 90;
-       msPenalty               =4000;
+       rewardSizeULorMS        = 80;
+       msPenalty               =3900;
        pixPerCycs                = [200];  %only 200 SF for learning guys
        
      case 'g62k.2rt' % Started GTS 8/6/14
        requestRewardSizeULorMS = 00;
-       rewardSizeULorMS        = 35;
-       msPenalty               =4200;
+       rewardSizeULorMS        = 30;
+       msPenalty               =4100;
     
      case 'g62e.12rt' % Started GTS 8/2/14
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 25;
-       msPenalty               =3800;
+       msPenalty               =3700;
     
 %      case 'g62j.4tt' % Started GTS 7/31/14
 %        requestRewardSizeULorMS = 0;
@@ -63,8 +63,8 @@ switch subjIDs{1}
        
      case 'g62j.4rt' % Started GTS 7/27/14
        requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 30;
-       msPenalty               =3700;
+       rewardSizeULorMS        = 25;
+       msPenalty               =3600;
        
 %      case 'g62j.5tt' % Started GTS 7/27/14
 %        requestRewardSizeULorMS = 0;
@@ -80,7 +80,7 @@ switch subjIDs{1}
      case 'g62b.5lt'           %started 1/28/14 
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 40; 
-       msPenalty               =4100;
+       msPenalty               =4000;
 %        
 %      case 'bfly2b.3rt'
 %        requestRewardSizeULorMS = 0;
@@ -91,7 +91,7 @@ switch subjIDs{1}
      case 'g62c6tt' % switched GTS 6/8/14
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 25;
-       msPenalty               =4100;
+       msPenalty               =4000;
 
 
     

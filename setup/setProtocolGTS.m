a1b8bc9739edc6df9b89ed30ec7955046592e878
erik <erik.flister@gmail.com>
2015-03-01 11:21:15 -0800

    libby

--- setup/setProtocolGTS.m
+++ setup/setProtocolGTS.m
@@ -29,32 +29,32 @@ if ~isscalar(subjIDs)
 end
 switch subjIDs{1}
     
-     
-     case 'g62j.8lt' % Started 10/22/14
-       requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 40;
-       msPenalty               =3800;
-
-     case 'g62m.1lt' % Started 10/22/14
-       requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 40;
-       msPenalty               =3800;
-       
-    case 'g62l1lt' % started GTS Learning paradigm on 10/12/14
-       requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 40;
-       msPenalty               =3800;
-       pixPerCycs                = [200];  %only 200 SF for learning guys
-       
-     case 'g62k.2rt' % Started GTS 8/6/14
-       requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 30;
-       msPenalty               =3800;
-    
-     case 'g62e.12rt' % Started GTS 8/2/14
-       requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 25;
-       msPenalty               =3700;
+%      
+%      case 'g62j.8lt' % Started 10/22/14
+%        requestRewardSizeULorMS = 0;
+%        rewardSizeULorMS        = 40;
+%        msPenalty               =3800;
+% 
+%      case 'g62m.1lt' % Started 10/22/14
+%        requestRewardSizeULorMS = 0;
+%        rewardSizeULorMS        = 40;
+%        msPenalty               =3800;
+%        
+%     case 'g62l1lt' % started GTS Learning paradigm on 10/12/14
+%        requestRewardSizeULorMS = 0;
+%        rewardSizeULorMS        = 40;
+%        msPenalty               =3800;
+%        pixPerCycs                = [200];  %only 200 SF for learning guys
+%        
+%      case 'g62k.2rt' % Started GTS 8/6/14
+%        requestRewardSizeULorMS = 0;
+%        rewardSizeULorMS        = 30;
+%        msPenalty               =3800;
+%     
+%      case 'g62e.12rt' % Started GTS 8/2/14
+%        requestRewardSizeULorMS = 0;
+%        rewardSizeULorMS        = 25;
+%        msPenalty               =3700;
     
 %      case 'g62j.4tt' % Started GTS 7/31/14
 %        requestRewardSizeULorMS = 0;
@@ -77,21 +77,21 @@ switch subjIDs{1}
 %        msPenalty               =4200;
 %        pixPerCycs             = [100 150 200]; %*10^9;
     
-     case 'g62b.5lt'           %started 1/28/14 
-       requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 50; 
-       msPenalty               =4000;
+%      case 'g62b.5lt'           %started 1/28/14 
+%        requestRewardSizeULorMS = 0;
+%        rewardSizeULorMS        = 50; 
+%        msPenalty               =4000;
+% %        
+% %      case 'bfly2b.3rt'
+% %        requestRewardSizeULorMS = 0;
+% %        rewardSizeULorMS        = 40;
+% %        msPenalty               =4200; 
+% %        pixPerCycs             = [100 150 200]; %*10^9;
 %        
-%      case 'bfly2b.3rt'
+%      case 'g62c6tt' % switched GTS 6/8/14
 %        requestRewardSizeULorMS = 0;
 %        rewardSizeULorMS        = 40;
-%        msPenalty               =4200; 
-%        pixPerCycs             = [100 150 200]; %*10^9;
-       
-     case 'g62c6tt' % switched GTS 6/8/14
-       requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 40;
-       msPenalty               =4100;
+%        msPenalty               =4100;
 
 
     

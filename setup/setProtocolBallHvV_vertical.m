26e41612b32036d04232530b7c957bba38b1d3ff
erik <erik.flister@gmail.com>
2015-03-06 10:15:48 -0800

    Libby

--- setup/setProtocolBallHvV_vertical.m
+++ setup/setProtocolBallHvV_vertical.m
@@ -19,6 +19,8 @@ fractionPenaltySoundIsOn  =1;
 scalar                    =1;
 msAirpuff                 =msPenalty;
 pixPerCycs                = [100 150 200]; %*10^9;
+percentCorrectionTrials = .5;
+normalizedPosition      = [0.25 0.75];
 
 % sca
 % keyboard
@@ -32,17 +34,7 @@ end
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 100;
        msPenalty               =5200;
-       
-     
-     case 'g62g.6lt' % Started 10/9/14
-       requestRewardSizeULorMS = 00;
-       rewardSizeULorMS        = 120;
-       msPenalty               =5200;
-     
-     case 'g62b7lt'           %Started full 8/14/14
-       requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 40; 
-       msPenalty               =3900;
+
          
      
 %    case 'g62h1tt'     %Started Full 8/15/14
@@ -51,42 +43,7 @@ end
 %         msPenalty              =4300;
 %         pixPerCycs             = [100 125 150]; %*10^9;  
         
-%    case 'g62b8tt'     %started 5/22/14  
-%        requestRewardSizeULorMS = 0;
-%        rewardSizeULorMS        = 25;
-%         msPenalty              =3600;
 
-  case 'g62c.2rt'           %Switched Back 9/15/14
-       requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 120; 
-       msPenalty               =5200;
-       
-%    case 'g62b1lt'     %moved to HvV_vertical 2/19/14   
-%        requestRewardSizeULorMS = 0;
-%        rewardSizeULorMS        = 35;
-%        msPenalty               =3600;
-%      
-%   case 'g62b3rt'          %changed 2/14/14
-%        requestRewardSizeULorMS = 0;
-%        rewardSizeULorMS        = 20; 
-%        msPenalty               = 3600; 
-       
-       
-%   case 'g62h1tt'     %started 5/16/14
-%        requestRewardSizeULorMS = 0;
-%        rewardSizeULorMS        = 45;
-%         msPenalty              =4300;
-
-       
-%   case 'g62b7lt'           %changed 5/16/14
-%        requestRewardSizeULorMS = 0;
-%        rewardSizeULorMS        = 45; 
-%        msPenalty               =4300;    
-
-%    case 'g62g4lt'     %started 5/22/14 
-%        requestRewardSizeULorMS = 0;
-%        rewardSizeULorMS        = 60;
-%         msPenalty              =4300;
            
        
     otherwise
@@ -95,7 +52,6 @@ end
 
 noRequest = constantReinforcement(rewardSizeULorMS,requestRewardSizeULorMS,requestMode,msPenalty,fractionOpenTimeSoundIsOn,fractionPenaltySoundIsOn,scalar,msAirpuff);
 
-percentCorrectionTrials = .5;
 
 maxWidth  = 1920;
 maxHeight = 1080;
@@ -121,10 +77,9 @@ stim.soundClue = false;
 targetOrientations     = 0
 distractorOrientations = []; %-targetOrientations;
 mean                   = .5;
-radius                 = .35;
+radius                 = .25;
 contrast               = 1;
 thresh                 = .00005;
-normalizedPosition      = [0.25 0.75];
 scaleFactor            = 0; %[1 1];
 axis                   = pi/2;
 

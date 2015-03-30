39dd99c22b4bfd86a0f549f5e20e8d0fff320b7b
erik <erik.flister@gmail.com>
2015-03-30 14:48:13 -0700

    Ryan

--- setup/setProtocolBallHvV_vertical.m
+++ setup/setProtocolBallHvV_vertical.m
@@ -34,7 +34,7 @@ end
    case 'g62l8rn'     %Started from Go To Black 3/8/15
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 20;
-       msPenalty              =3500;
+       msPenalty              =3750;
        percentCorrectionTrials = .5;
   
         

8a619362cd10b4766b8f1357f34af969292421d1
erik <erik.flister@gmail.com>
2015-05-03 15:55:22 -0700

    Ryan

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -26,13 +26,13 @@ switch subjIDs{1}
     
      case 'g62a4tt' % Started 4/30/15
        requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 100;
+       rewardSizeULorMS        = 80;
        msPenalty               =2800;
        percentCorrectionTrials = .5;
 
      case 'g62a5nn' % Started 4/30/15
        requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 70;
+       rewardSizeULorMS        = 80;
        msPenalty               =2800;
        percentCorrectionTrials = .5;
 %     

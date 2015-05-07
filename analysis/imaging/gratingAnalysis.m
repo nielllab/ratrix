3cea34d3c5b456ac7c47630ed1d3bf895ddb77fb
Cris <crisniell@gmail.com>
2015-05-07 10:52:20 -0700

    2p analysis

--- analysis/imaging/gratingAnalysis.m
+++ analysis/imaging/gratingAnalysis.m
@@ -10,8 +10,8 @@ evokeRange = (1:dt:4)/dt;
 nstim = min(length(xpos),floor(size(dF,2)*dt/8 -1))
 theta = theta(1:nstim); sf = sf(1:nstim);
 for s = 1:nstim;
- 
-   base(:,s) = mean(dF(:,startTime + (s-1)*(duration+isi)/dt +baseRange),2);
+    
+    base(:,s) = mean(dF(:,startTime + (s-1)*(duration+isi)/dt +baseRange),2);
     evoked(:,s) = mean(dF(:,startTime + isi/dt +(s-1)*(duration+isi)/dt +evokeRange),2);
 end
 
@@ -23,7 +23,7 @@ sfs = unique(sf);
 
 for th = 1:length(angles);
     for sp = 1:length(sfs);
-   %     allresp(:,th,sp,:) = resp(:,theta ==angles(th) & sf==sfs(sp));
+        %     allresp(:,th,sp,:) = resp(:,theta ==angles(th) & sf==sfs(sp));
         orientation(:,th,sp) = median(resp(:,theta ==angles(th) & sf==sfs(sp)),2);
         %     figure
         %     imagesc(squeeze(orientation(:,:,th)),[-0.5 0.5]);
@@ -38,14 +38,14 @@ npts = size(dF,1);
 
 if npts<=225
     figure
-nfigs = ceil(sqrt((npts)));
+    nfigs = ceil(sqrt((npts)));
 end
 
 for i= 1:npts
     if i/1000 == round(i/1000);
         i
     end
-    
+    i
     tftuning=squeeze(mean(orientation(i,:,:),2));
     tfpref(i) =(tftuning(2)-tftuning(1))/(tftuning(2) + tftuning(1));
     if tfpref(i)>0
@@ -54,51 +54,50 @@ for i= 1:npts
         tf_use=1;
     end
     if ~blank
-tuning = squeeze(orientation(i,:,tf_use));
-tuning_std = squeeze(ori_std(i,:,tf_use));
-spont(i)=0; spont_std(i)=0;
+        tuning = squeeze(orientation(i,:,tf_use));
+        tuning_std = squeeze(ori_std(i,:,tf_use));
+        spont(i)=0; spont_std(i)=0;
     else
-      tuning = squeeze(orientation(i,1:end-1,tf_use)) ; tuning_std = squeeze(ori_std(i,1:end-1,tf_use));
-      spont(i) = mean(orientation(i,end,:)); spont_std(i)=sqrt(sum(ori_std(i,end,:).^2))/(size(ori_std,3));
+        tuning = squeeze(orientation(i,1:end-1,tf_use)) ; tuning_std = squeeze(ori_std(i,1:end-1,tf_use));
+        spont(i) = mean(orientation(i,end,:)); spont_std(i)=sqrt(sum(ori_std(i,end,:).^2))/(size(ori_std,3));
     end
     ntrials = length(find(theta==angles(1) & sf==sfs(1)));
     
- 
-   R(i) = max(tuning)-spont(i);
- 
+    
+    R(i) = max(tuning)-spont(i);
+    
     [osicv(i) tuningtheta(i)] = calcOSI(tuning'-spont(i),0);
-   if npts<100*100
-       [thetafit(i) osi(i) A1(i) A2(i) w(i) B(i) nr yfit] = fit_tuningcurve(tuning-spont(i),angles(1:length(tuning)));
-     [osi(i) width(i) amp(i)] = calculate_tuning(A1(i),A2(i),B(i),w(i));
-   else
-       osi=[];
-       width=[];
-       amp=[];
-   end
-
+    if npts<100*100
+        [thetafit(i) osi(i) A1(i) A2(i) w(i) B(i) nr yfit] = fit_tuningcurve(tuning-spont(i),angles(1:length(tuning)));
+        [osi(i) width(i) amp(i)] = calculate_tuning(A1(i),A2(i),B(i),w(i));
+    else
+        osi=[];
+        width=[];
+        amp=[];
+    end
+    
     alltuning(i,:)=tuning;
     if npts<100*100
-    for ori=1:length(tuning);
-        t(ori)=(tuning(ori)-spont(i))/(tuning_std(ori) /sqrt(ntrials));
-        p(ori) = tcdf(t(ori),ntrials-1);
-        p(ori) = 2*min(p(ori),1-p(ori));
-    end
-    minp(i) = min(p);
+        for ori=1:length(tuning);
+            t(ori)=(tuning(ori)-spont(i))/(tuning_std(ori) /sqrt(ntrials));
+            p(ori) = tcdf(t(ori),ntrials-1);
+            p(ori) = 2*min(p(ori),1-p(ori));
+        end
+        minp(i) = min(p);
     else
         minp(i)=NaN;
     end
     
-      if npts<=225
-       subplot(nfigs,nfigs,i)
-     %  subplot(10,8,i-1)
-   errorbar(1:length(tuning),tuning,tuning_std/sqrt(ntrials)); 
-   hold on; plot([1 8],[spont(i) spont(i)],'g');
-    ylim(4*[-1 2]); xlim([0 9])
-    set(gca,'Xtick',[]); set(gca,'Ytick',[]);
-    
- keyboard
-    %title(sprintf('%0.2f %0.2f',minp(i)*length(angles),osi(i)));
-   end
+    if npts<=225
+        subplot(nfigs,nfigs,i)
+        %  subplot(10,8,i-1)
+        errorbar(1:length(tuning),tuning,tuning_std/sqrt(ntrials));
+        hold on; plot([1 8],[spont(i) spont(i)],'g');
+        ylim(4*[-1 2]); xlim([0 9])
+        set(gca,'Xtick',[]); set(gca,'Ytick',[]);
+        
+        %title(sprintf('%0.2f %0.2f',minp(i)*length(angles),osi(i)));
+    end
     
 end
 

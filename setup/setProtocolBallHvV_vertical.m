1957fdef164cc5634644e4f168e378fa8849bdc8
erik <erik.flister@gmail.com>
2015-05-16 11:00:52 -0700

    libby

--- setup/setProtocolBallHvV_vertical.m
+++ setup/setProtocolBallHvV_vertical.m
@@ -34,13 +34,13 @@ end
      case 'g62a5nn' % Started 5/15/15
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 70;
-       msPenalty               =2800;
+       msPenalty               =2900;
        percentCorrectionTrials = .5;
      
      case 'g62a4tt' % Started 5/14/15
        requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 60;
-       msPenalty               =2800;
+       rewardSizeULorMS        = 50;
+       msPenalty               =2900;
        percentCorrectionTrials = .5;
      
      case 'g62l8rn'     %Started from Go To Black 3/8/15

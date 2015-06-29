aebaddb1e1aa60b468024ebdcad43362a6f201f6
erik <erik.flister@gmail.com>
2015-06-29 10:30:38 -0700

    Ryan

--- setup/setProtocolBallHvV_vertical.m
+++ setup/setProtocolBallHvV_vertical.m
@@ -33,19 +33,19 @@ end
     
      case 'g62r3rt' % Started 5/18/15
        requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 70;
+       rewardSizeULorMS        = 60;
        msPenalty               =3000;
        percentCorrectionTrials = .5;
      
      case 'g62a5nn' % Started 5/15/15
        requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 45;
+       rewardSizeULorMS        = 40;
        msPenalty               =3500;
        percentCorrectionTrials = .50;
      
      case 'g62a4tt' % Started 5/14/15
        requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 35;
+       rewardSizeULorMS        = 30;
        msPenalty               =3200;
        percentCorrectionTrials = .50;
      
--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -37,7 +37,7 @@ switch subjIDs{1}
     
      case 'g62a2nn' % Started 6/25/15 (permenant record wont show till 6/26/15)
        requestRewardSizeULorMS = 30;
-       rewardSizeULorMS        = 100;
+       rewardSizeULorMS        = 80;
        msPenalty               =3500;
        percentCorrectionTrials = .5;
        
@@ -47,11 +47,11 @@ switch subjIDs{1}
        msPenalty               =3500;
        percentCorrectionTrials = .5;
     
-    case 'g62a5nn' % Started 6/25/15
-       requestRewardSizeULorMS = 00;
-       rewardSizeULorMS        = 100;
-       msPenalty               =3500;
-       percentCorrectionTrials = .5;
+%     case 'g62a5nn' % Started 6/25/15
+%        requestRewardSizeULorMS = 00;
+%        rewardSizeULorMS        = 100;
+%        msPenalty               =3500;
+%        percentCorrectionTrials = .5;
        
     case 'g62a3tt' % Started 6/25/15
        requestRewardSizeULorMS = 00;
@@ -61,14 +61,14 @@ switch subjIDs{1}
        
     case 'pv8lt' % Started 6/25/15  (permenant record wont show till 6/26/15)
        requestRewardSizeULorMS = 00;
-       rewardSizeULorMS        = 100;
+       rewardSizeULorMS        = 80;
        msPenalty               =3500;
        percentCorrectionTrials = .5;
        stim.slowSecs = 1;
        
     case 'pv8nt' % Started 6/25/15  (permenant record wont show till 6/26/15)
        requestRewardSizeULorMS = 00;
-       rewardSizeULorMS        = 100;
+       rewardSizeULorMS        = 80;
        msPenalty               =3500;
        percentCorrectionTrials = .5;
        stim.slowSecs = 1;

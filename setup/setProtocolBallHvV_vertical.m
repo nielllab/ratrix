a951d8cade02002bde7d33d836df22afc3c91822
erik <erik.flister@gmail.com>
2015-06-29 15:24:28 -0700

    Ryan

--- setup/setProtocolBallHvV_vertical.m
+++ setup/setProtocolBallHvV_vertical.m
@@ -35,7 +35,7 @@ end
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 60;
        msPenalty               =3000;
-       pixPerCycs              = [200];
+       pixPerCycs              = [350];
        percentCorrectionTrials = .5;
      
      case 'g62r3rt' % Started 5/18/15

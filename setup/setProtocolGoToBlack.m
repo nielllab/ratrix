8d3888994b760a2e21b98a37b259656a9e4e64c8
erik <erik.flister@gmail.com>
2015-07-01 16:11:36 -0700

    Ryan

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -57,7 +57,7 @@ switch subjIDs{1}
        requestRewardSizeULorMS = 00;
        rewardSizeULorMS        = 112;
        msPenalty               =3500;
-       percentCorrectionTrials = .5;
+       percentCorrectionTrials = .0;
        
     case 'pv8lt' % Started 6/25/15  (permenant record wont show till 6/26/15)
        requestRewardSizeULorMS = 00;

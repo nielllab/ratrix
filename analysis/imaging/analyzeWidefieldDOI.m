2bd5a8c474143f77e378d8550e734ae7e09c3f4b
jwekselb <joseph.wekselblatt@gmail.com>
2015-08-17 15:20:44 -0700

    add analyzeWidefieldDOI.m

---
+++ analysis/imaging/analyzeWidefieldDOI.m
@@ -0,0 +1,74 @@
+
+
+clear all
+%batchPassive2015;
+%batchTopography
+batchDOI0722
+%batchRigTest
+%batchTopoFrontiers
+close all
+psfilename = 'C:\tempPS.ps';
+if exist(psfilename,'file')==2;delete(psfilename);end
+
+
+   alluse = find(strcmp({files.inject},'saline')  & strcmp({files.timing},'post') & strcmp({files.label},'camk2 gc6') & strcmp({files.notes},'good imaging session')  ) 
+  
+length(alluse)
+%alluse=alluse(1:5)
+%alluse=alluse(end-5:end);
+allsubj = unique({files(alluse).subj})
+
+
+
+%%% use this one for subject by subject averaging
+%for s = 1:length(allsubj)
+%use = intersect(alluse,find(strcmp({files.subj},allsubj{s})))    
+
+%%% use this one to average all sessions that meet criteria
+for s=1:1
+use = alluse;
+
+allsubj{s}
+
+%%% calculate gradients and regions
+clear map merge
+
+x0 =0; y0=0; sz = 128;
+doTopography;
+
+doGratingsNew;
+% %%% analyze looming
+% for f = 1:length(use)
+%     loom_resp{f}=fourPhaseOverlay(files(use(f)),pathname,outpathname,'loom');
+% end
+% fourPhaseAvg(loom_resp,allxshift+x0,allyshift+y0,allthetashift,zoom, sz, avgmap);
+
+
+%%% analyze grating
+% for f = 1:length(use)
+%  f
+%  grating_resp{f}=fourPhaseOverlay(files(use(f)),pathname,outpathname,'grating');
+% end
+% fourPhaseAvg(grating_resp,allxshift+x0,allyshift+y0, allthetashift,zoom*0.57, sz, avgmap);
+
+
+
+end
+[f p] = uiputfile('*.mat','save data?');
+sessiondata = files(alluse);
+
+if f~=0
+    save(fullfile(p,f),'allsubj','sessiondata','shiftData','fit','mnfit','cycavg','sp_all','mv');
+end
+
+[f p] = uiputfile('*.pdf','save pdf');
+if f~=0
+    try
+   ps2pdf('psfile', psfilename, 'pdffile', fullfile(p,f));
+catch
+    display('couldnt generate pdf');
+    end
+end
+delete(psfilename);
+
+

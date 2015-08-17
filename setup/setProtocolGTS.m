fd3fec789fce87a61b718a73ba761edb580dc214
erik <erik.flister@gmail.com>
2015-08-17 10:38:45 -0700

    Ryan

--- setup/setProtocolGTS.m
+++ setup/setProtocolGTS.m
@@ -35,19 +35,19 @@ switch subjIDs{1}
      
     case 'g62w2lt' % Started 7/14/15 %%switch gts 8/12/15
        requestRewardSizeULorMS = 00;
-       rewardSizeULorMS        = 43;
+       rewardSizeULorMS        = 27;
        msPenalty               =3500;
        percentCorrectionTrials = .5;
     
     case 'g62q1lt' % Started 7/14/15 %%switched gts 8/12/15
        requestRewardSizeULorMS = 00;
-       rewardSizeULorMS        = 43;
+       rewardSizeULorMS        = 27;
        msPenalty               =3500;
        percentCorrectionTrials = .5;
     
     case 'pvchr9tt' % Started 7/14/15
        requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 43;
+       rewardSizeULorMS        = 27;
        msPenalty               =3500;
        percentCorrectionTrials = .5;
     
@@ -59,7 +59,7 @@ switch subjIDs{1}
     
     case 'pvchr9lt' % Started 7/14/15
        requestRewardSizeULorMS = 00;
-       rewardSizeULorMS        = 43;
+       rewardSizeULorMS        = 27;
        msPenalty               =3500;
        percentCorrectionTrials = .5;
     

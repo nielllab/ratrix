1458ed6416b7cafb77cbc2ec58817492d29b84c8
Maxwell <niell.lab.orders@gmail.com>
2015-08-27 18:44:38 -0700

    updating 4x3y and gratings
    phil

--- analysis/imaging/doDOIGratsplots.m
+++ analysis/imaging/doDOIGratsplots.m
@@ -1,10 +1,3 @@
-scsz = get( 0, 'Screensize' ); %get screensize for plots
-%screensize = get( groot, 'Screensize' ); %get screensize for plots in R2014b and later
-
-%second check
-
-load mapOverlay
-
 % %%%% tf tuning plots
 % figure('Position', [scsz(3)/2 scsz(2)/2 scsz(3)/scscale scsz(4)/scscale]);
 % subplot(2,4,1)
@@ -35,148 +28,148 @@ load mapOverlay
 % hold off
 
 
-%%%% sf tuning plots
-figure('Position', [scsz(1) scsz(2) scsz(3)/scscale scsz(4)/scscale]);
-subplot(2,4,1)
-plot(0,squeeze(allmnfit(1,1,1,conds)))
-title('SF tuning','position',[0.5 0.5],'color','r','fontsize',12)
-axis([0 1 0 1])
-axis off
-legend(datafiles(conds),'location','south')
-for area = 1:7
-    subplot(2,4,area+1)
-    d = squeeze(mean(mean(allmnfit(x(area)+range, y(area)+range,6:7,conds),2),1));
-    dsd = squeeze(mean(mean(sdmnfit(x(area)+range, y(area)+range,6:7,conds),2),1));
-    mseb(1:2,d',dsd',[],1)
-    title(sprintf(cell2mat(areanames(area))));
-    axis([1 2 0 1])
-end
-   
-%%%% y tuning plots
-figure('Position', [scsz(3)/2 scsz(2)/2 scsz(3)/scscale scsz(4)/scscale]);
-subplot(2,4,1)
-plot(0,squeeze(allmnfit(1,1,1,conds)))
-title('Y tuning','position',[0.5 0.5],'color','r','fontsize',12)
-axis([0 1 0 1])
-axis off
-legend(datafiles(conds),'location','south')
-for area = 1:7
-    subplot(2,4,area+1)
-    d = squeeze(mean(mean(allmnfit(x(area)+range, y(area)+range,4:5,conds),2),1));
-    dsd = squeeze(mean(mean(sdmnfit(x(area)+range, y(area)+range,4:5,conds),2),1));
-    mseb(1:2,d',dsd',[],1);
-    title(sprintf(cell2mat(areanames(area))));
-    axis([1 2 0 1])
-end
-
-%%%% x tuning plots
-figure('Position', [scsz(1) scsz(2) scsz(3)/scscale scsz(4)/scscale]);
-subplot(2,4,1)
-plot(0,squeeze(allmnfit(1,1,1,conds)))
-title('X tuning','position',[0.5 0.5],'color','r','fontsize',12)
-axis([0 1 0 1])
-axis off
-legend(datafiles(conds),'location','south')
-for area = 1:7
-    subplot(2,4,area+1)
-    d = squeeze(mean(mean(allmnfit(x(area)+range, y(area)+range,1:3,conds),2),1));
-    dsd = squeeze(mean(mean(sdmnfit(x(area)+range, y(area)+range,1:3,conds),2),1));
-    mseb(1:3,d',dsd',[],1);
-    title(sprintf(cell2mat(areanames(area))));
-    axis([1 3 0 max(max(max(allmnfit(:,:,1:3))))])
-end
-
-%cycle average for low spatial frequency stim
-labels = {'blank','blank+patch','patch','blank decon','b+p decon','patch decon' }
-for area = 1:length(areanames)
-    figure('Position', [scsz(3)/4 scsz(4)/2-scsz(4)/10 scsz(3)/scscale scsz(4)/scscale]);
-    for s = 1:6
-        subplot(2,3,s);        
-        hold on
-        if s==1 | s==4
-            d=squeeze(mean(mean(allcycavg(x(area)+range,y(area)+range,1:25,conds),2),1));
-            dsd=squeeze(mean(mean(sdcycavg(x(area)+range,y(area)+range,1:25,conds),2),1));
-        elseif s==2 | s==5
-            d=squeeze(mean(mean(allcycavg(x(area)+range,y(area)+range,26:50,conds),2),1));
-            dsd=squeeze(mean(mean(sdcycavg(x(area)+range,y(area)+range,26:50,conds),2),1))
-        elseif s==3 | s==6
-            d=squeeze(mean(mean(allcycavg(x(area)+range,y(area)+range,26:50,conds)-allcycavg(x(area)+range,y(area)+range,1:25,conds),2),1));
-            dsd=squeeze(mean(mean(sdcycavg(x(area)+range,y(area)+range,26:50,conds)-sdcycavg(x(area)+range,y(area)+range,1:25,conds),2),1));
-        end
-        if s>=4
-            repd = repmat(d,[10 1]);
-            for i = 1:length(conds)
-                dconvd(i,:) = deconvg6s(repd(:,i)'+0.5,0.1);
-            end
-            dcon = dconvd(:,6*length(d):(7*length(d)-1));
-            for i = 1:length(conds)
-                dcon(i,:) = (dcon(i,:) - min(dcon(i,:)));
-            end
-            plot(dcon');
-        else
-            for i = 1:length(conds)
-                d(:,i) = (d(:,i) - min(d(:,i)));
-            end
-            mseb(1:25,d',dsd',[],1);
-        end
-        plot([11 11],[0 0.2],':')
-        xlim([1 25]); 
-        if s<=3 
-            ylim([0 0.15]);
-        else
-            ylim([0 0.2])
-        end
-        title(sprintf('low sf %s %s',areanames{area},labels{s}));
-        xlabel('frames')
-    end
-    legend(datafiles(conds),'location','northeast')
-end
-
-%cycle average for high sf
-labels = {'blank','blank+patch','patch','blank decon','b+p decon','patch decon' }
-for area = 1:length(areanames)
-    figure('Position', [scsz(3)/4 scsz(4)/2-scsz(4)/10 scsz(3)/scscale scsz(4)/scscale]);
-    for s = 1:6
-        subplot(2,3,s);        
-        hold on
-        if s==1 | s==4
-            d=squeeze(mean(mean(allcycavg(x(area)+range,y(area)+range,51:75,conds),2),1));
-            dsd=squeeze(mean(mean(sdcycavg(x(area)+range,y(area)+range,51:75,conds),2),1));
-        elseif s==2 | s==5
-            d=squeeze(mean(mean(allcycavg(x(area)+range,y(area)+range,76:100,conds),2),1));
-            dsd=squeeze(mean(mean(sdcycavg(x(area)+range,y(area)+range,76:100,conds),2),1))
-        elseif s==3 | s==6
-            d=squeeze(mean(mean(allcycavg(x(area)+range,y(area)+range,76:100,conds)-allcycavg(x(area)+range,y(area)+range,51:75,conds),2),1));
-            dsd=squeeze(mean(mean(sdcycavg(x(area)+range,y(area)+range,76:100,conds)-sdcycavg(x(area)+range,y(area)+range,51:75,conds),2),1));
-        end
-        if s>=4
-            repd = repmat(d,[10 1]);
-            for i = 1:length(conds)
-                dconvd(i,:) = deconvg6s(repd(:,i)'+0.5,0.1);
-            end
-            dcon = dconvd(:,6*length(d):(7*length(d)-1));
-            for i = 1:length(conds)
-                dcon(i,:) = (dcon(i,:) - min(dcon(i,:)));
-            end
-            plot(dcon');
-        else
-            for i = 1:length(conds)
-                d(:,i) = (d(:,i) - min(d(:,i)));
-            end
-            mseb(1:25,d',dsd',[],1);
-        end
-        plot([11 11],[0 0.2],':')
-        xlim([1 25]); 
-        if s<=3 
-            ylim([0 0.15]);
-        else
-            ylim([0 0.2])
-        end
-        title(sprintf('high sf %s %s',areanames{area},labels{s}));
-        xlabel('frames')
-    end
-    legend(datafiles(conds),'location','northeast')
-end
+% %%%% sf tuning plots
+% figure('Position', [scsz(1) scsz(2) scsz(3)/scscale scsz(4)/scscale]);
+% subplot(2,4,1)
+% plot(0,squeeze(allmnfit(1,1,1,conds)))
+% title('SF tuning','position',[0.5 0.5],'color','r','fontsize',12)
+% axis([0 1 0 1])
+% axis off
+% legend(datafiles(conds),'location','south')
+% for area = 1:7
+%     subplot(2,4,area+1)
+%     d = squeeze(mean(mean(allmnfit(x(area)+range, y(area)+range,6:7,conds),2),1));
+%     dsd = squeeze(mean(mean(sdmnfit(x(area)+range, y(area)+range,6:7,conds),2),1));
+%     mseb(1:2,d',dsd',[],1)
+%     title(sprintf(cell2mat(areanames(area))));
+%     axis([1 2 0 1])
+% end
+%    
+% %%%% y tuning plots
+% figure('Position', [scsz(3)/2 scsz(2)/2 scsz(3)/scscale scsz(4)/scscale]);
+% subplot(2,4,1)
+% plot(0,squeeze(allmnfit(1,1,1,conds)))
+% title('Y tuning','position',[0.5 0.5],'color','r','fontsize',12)
+% axis([0 1 0 1])
+% axis off
+% legend(datafiles(conds),'location','south')
+% for area = 1:7
+%     subplot(2,4,area+1)
+%     d = squeeze(mean(mean(allmnfit(x(area)+range, y(area)+range,4:5,conds),2),1));
+%     dsd = squeeze(mean(mean(sdmnfit(x(area)+range, y(area)+range,4:5,conds),2),1));
+%     mseb(1:2,d',dsd',[],1);
+%     title(sprintf(cell2mat(areanames(area))));
+%     axis([1 2 0 1])
+% end
+% 
+% %%%% x tuning plots
+% figure('Position', [scsz(1) scsz(2) scsz(3)/scscale scsz(4)/scscale]);
+% subplot(2,4,1)
+% plot(0,squeeze(allmnfit(1,1,1,conds)))
+% title('X tuning','position',[0.5 0.5],'color','r','fontsize',12)
+% axis([0 1 0 1])
+% axis off
+% legend(datafiles(conds),'location','south')
+% for area = 1:7
+%     subplot(2,4,area+1)
+%     d = squeeze(mean(mean(allmnfit(x(area)+range, y(area)+range,1:3,conds),2),1));
+%     dsd = squeeze(mean(mean(sdmnfit(x(area)+range, y(area)+range,1:3,conds),2),1));
+%     mseb(1:3,d',dsd',[],1);
+%     title(sprintf(cell2mat(areanames(area))));
+%     axis([1 3 0 max(max(max(allmnfit(:,:,1:3))))])
+% end
+% 
+% %cycle average for low spatial frequency stim
+% labels = {'blank','blank+patch','patch','blank decon','b+p decon','patch decon' }
+% for area = 1:length(areanames)
+%     figure('Position', [scsz(3)/4 scsz(4)/2-scsz(4)/10 scsz(3)/scscale scsz(4)/scscale]);
+%     for s = 1:6
+%         subplot(2,3,s);        
+%         hold on
+%         if s==1 | s==4
+%             d=squeeze(mean(mean(allcycavg(x(area)+range,y(area)+range,1:25,conds),2),1));
+%             dsd=squeeze(mean(mean(sdcycavg(x(area)+range,y(area)+range,1:25,conds),2),1));
+%         elseif s==2 | s==5
+%             d=squeeze(mean(mean(allcycavg(x(area)+range,y(area)+range,26:50,conds),2),1));
+%             dsd=squeeze(mean(mean(sdcycavg(x(area)+range,y(area)+range,26:50,conds),2),1));
+%         elseif s==3 | s==6
+%             d=squeeze(mean(mean(allcycavg(x(area)+range,y(area)+range,26:50,conds)-allcycavg(x(area)+range,y(area)+range,1:25,conds),2),1));
+%             dsd=squeeze(mean(mean(sdcycavg(x(area)+range,y(area)+range,26:50,conds)-sdcycavg(x(area)+range,y(area)+range,1:25,conds),2),1));
+%         end
+%         if s>=4
+%             repd = repmat(d,[10 1]);
+%             for i = 1:length(conds)
+%                 dconvd(i,:) = deconvg6s(repd(:,i)'+0.5,0.1);
+%             end
+%             dcon = dconvd(:,6*length(d):(7*length(d)-1));
+%             for i = 1:length(conds)
+%                 dcon(i,:) = (dcon(i,:) - min(dcon(i,:)));
+%             end
+%             plot(dcon');
+%         else
+%             for i = 1:length(conds)
+%                 d(:,i) = (d(:,i) - min(d(:,i)));
+%             end
+%             mseb(1:25,d',dsd',[],1);
+%         end
+%         plot([11 11],[0 0.2],':')
+%         xlim([1 25]); 
+%         if s<=3 
+%             ylim([0 0.15]);
+%         else
+%             ylim([0 0.2])
+%         end
+%         title(sprintf('low sf %s %s',areanames{area},labels{s}));
+%         xlabel('frames')
+%     end
+%     legend(datafiles(conds),'location','northeast')
+% end
+% 
+% %cycle average for high sf
+% labels = {'blank','blank+patch','patch','blank decon','b+p decon','patch decon' }
+% for area = 1:length(areanames)
+%     figure('Position', [scsz(3)/4 scsz(4)/2-scsz(4)/10 scsz(3)/scscale scsz(4)/scscale]);
+%     for s = 1:6
+%         subplot(2,3,s);        
+%         hold on
+%         if s==1 | s==4
+%             d=squeeze(mean(mean(allcycavg(x(area)+range,y(area)+range,51:75,conds),2),1));
+%             dsd=squeeze(mean(mean(sdcycavg(x(area)+range,y(area)+range,51:75,conds),2),1));
+%         elseif s==2 | s==5
+%             d=squeeze(mean(mean(allcycavg(x(area)+range,y(area)+range,76:100,conds),2),1));
+%             dsd=squeeze(mean(mean(sdcycavg(x(area)+range,y(area)+range,76:100,conds),2),1));
+%         elseif s==3 | s==6
+%             d=squeeze(mean(mean(allcycavg(x(area)+range,y(area)+range,76:100,conds)-allcycavg(x(area)+range,y(area)+range,51:75,conds),2),1));
+%             dsd=squeeze(mean(mean(sdcycavg(x(area)+range,y(area)+range,76:100,conds)-sdcycavg(x(area)+range,y(area)+range,51:75,conds),2),1));
+%         end
+%         if s>=4
+%             repd = repmat(d,[10 1]);
+%             for i = 1:length(conds)
+%                 dconvd(i,:) = deconvg6s(repd(:,i)'+0.5,0.1);
+%             end
+%             dcon = dconvd(:,6*length(d):(7*length(d)-1));
+%             for i = 1:length(conds)
+%                 dcon(i,:) = (dcon(i,:) - min(dcon(i,:)));
+%             end
+%             plot(dcon');
+%         else
+%             for i = 1:length(conds)
+%                 d(:,i) = (d(:,i) - min(d(:,i)));
+%             end
+%             mseb(1:25,d',dsd',[],1);
+%         end
+%         plot([11 11],[0 0.2],':')
+%         xlim([1 25]); 
+%         if s<=3 
+%             ylim([0 0.15]);
+%         else
+%             ylim([0 0.2])
+%         end
+%         title(sprintf('high sf %s %s',areanames{area},labels{s}));
+%         xlabel('frames')
+%     end
+%     legend(datafiles(conds),'location','northeast')
+% end
 
 % %circleshifted non-normalized cycle average
 % figure('Position', [scsz(3)/4 scsz(4)/2-scsz(4)/10 scsz(3)/scscale scsz(4)/scscale]);%displays at top of screen
@@ -389,7 +382,7 @@ for area = 1:length(areanames)
             dsd=squeeze(mean(mean(sdcycavg(x(area)+range,y(area)+range,1:25,conds),2),1));
         elseif s==2 | s==5
             d=squeeze(mean(mean(allcycavg(x(area)+range,y(area)+range,26:50,conds),2),1));
-            dsd=squeeze(mean(mean(sdcycavg(x(area)+range,y(area)+range,26:50,conds),2),1))
+            dsd=squeeze(mean(mean(sdcycavg(x(area)+range,y(area)+range,26:50,conds),2),1));
         elseif s==3 | s==6
             d=squeeze(mean(mean(allcycavg(x(area)+range,y(area)+range,26:50,conds)-allcycavg(x(area)+range,y(area)+range,1:25,conds),2),1));
             dsd=squeeze(mean(mean(sdcycavg(x(area)+range,y(area)+range,26:50,conds)-sdcycavg(x(area)+range,y(area)+range,1:25,conds),2),1));
@@ -413,9 +406,9 @@ for area = 1:length(areanames)
         plot([11 11],[0 0.2],':')
         xlim([1 25]); 
         if s<=3 
-            ylim([0 0.1]);
+            ylim([0 0.15]);
         else
-            ylim([0 0.15])
+            ylim([0 0.2])
         end
         title(sprintf('low sf %s %s',areanames{area},labels{s}),'fontsize',8);
         xlabel('frames')
@@ -437,7 +430,7 @@ for area = 1:length(areanames)
             dsd=squeeze(mean(mean(sdcycavg(x(area)+range,y(area)+range,51:75,conds),2),1));
         elseif s==2 | s==5
             d=squeeze(mean(mean(allcycavg(x(area)+range,y(area)+range,76:100,conds),2),1));
-            dsd=squeeze(mean(mean(sdcycavg(x(area)+range,y(area)+range,76:100,conds),2),1))
+            dsd=squeeze(mean(mean(sdcycavg(x(area)+range,y(area)+range,76:100,conds),2),1));
         elseif s==3 | s==6
             d=squeeze(mean(mean(allcycavg(x(area)+range,y(area)+range,76:100,conds)-allcycavg(x(area)+range,y(area)+range,51:75,conds),2),1));
             dsd=squeeze(mean(mean(sdcycavg(x(area)+range,y(area)+range,76:100,conds)-sdcycavg(x(area)+range,y(area)+range,51:75,conds),2),1));

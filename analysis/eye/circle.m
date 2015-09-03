f26c3779fc06e5c9231d8a42ba26e3f55290a509
Jackson <niell.lab.orders@gmail.com>
2015-09-02 19:59:06 -0700

    eye tracking update
    trying to add closed loop component - phil

--- analysis/eye/circle.m
+++ analysis/eye/circle.m
@@ -1,9 +1,8 @@
-function circle(x,y,area)
+function circle(x,y,r)
 %x and y are the coordinates of the center of the circle
 %r is the radius of the circle
 %0.01 is the angle step, bigger values will draw the circle faster but
 %you might notice imperfections (not very smooth)
-r = sqrt(area/pi);
 ang=0:0.01:2*pi; 
 xp=r*cos(ang);
 yp=r*sin(ang);
--- analysis/eye/eyetracking.m
+++ analysis/eye/eyetracking.m
@@ -1,69 +1,86 @@
 clear all
 close all
 
-%load data
-name = '08_31_15_postlis_eye';
-load(name); % should be a '*_eye.mat' file
+name = 'trial5'; %data file
+thresh = 0.85; %pupil threshold for binarization
+pupercent = 0.15; %set range pupil radius window
+pupchange = 0.35; %acceptable percent change in radius per framerange
+framerange = 3; %number of frames to smooth over
 
-%set circle size and b/w threshold
-rad_range = [20 35]; % range of radii to search for
-thresh = 0.7; %pupil threshold
-
-%reformat data and find center points
-data = squeeze(data); % the raw images...
-xc = size(data,2)/2; % image center
-yc = size(data,1)/2;
+load(name);
+data = squeeze(data);
 warning off;
 
 %user input to select center and right points
-figure
+printf('Please select pupil center and top, eyeball top and right points, outside pupil')
+h = figure('units','normalized','outerposition',[0 0 1 1])
 imshow(data(:,:,1))
-[cent] = ginput(2);
-yoff = yc - cent(1,2);
-xoff = xc -cent(1,1);
-xc = xc - xoff;
-yc = yc - yoff;
-W = (cent(2,1) - xc);
-figure
-imshow(data(yc-W:yc+W,xc-W:xc+W));
+[cent] = ginput(5);
+close(h);
+yc = cent(1,2); %pupil center y val
+xc = cent(1,1); %pupil center x val
+horiz = (cent(4,1) - xc); %1/2 x search range
+vert = (yc - cent(3,2)); %1/2 y search range
+puprad = yc - cent(2,2); %initial pupil radius
+puprange = [round(puprad - puprad*pupercent) round(puprad + puprad*pupercent)]; %range of pupil sizes to search over
 
-bindata = double(data); %convert from uint8 into doubles and threshold
-for i = 1:size(data,3);
-    bindata(:,:,i) = bindata(:,:,i)/max(max(bindata(:,:,i)))<thresh;
+%convert from uint8 into doubles and threshold, then binarize
+ddata = double(data); 
+binmaxx = cent(5,1);
+binmaxy = cent(5,2);
+for i = 1:size(data,3)
+    binmax(i) = mean(mean(mean(ddata(binmaxy-3:binmaxy+3,binmaxx-3:binmaxx+3,i))));
+end
+for i = 1:size(ddata,3)
+    bindata(:,:,i) = (ddata(yc-vert:yc+vert,xc-horiz:xc+horiz,i)/binmax(i) < thresh);
 end
 
 tic
-for n = 1:size(data,3)
-    %       [center,radii,metric] = imfindcircles(squeeze(data(yc-W:yc+W,xc-W:xc+W,n)),rad_range,'Sensitivity',1);
-    [center,radii,metric] = imfindcircles(bindata(yc-W:yc+W,xc-W:xc+W,n),rad_range,'Sensitivity',1,'ObjectPolarity','dark');
+centroid = nan(size(data,3),2);
+rad = nan(size(data,3),1);
+centroid(1,:) = [horiz vert];
+rad(1,1) = puprad;
+for n = 2:size(data,3)
+    [center,radii,metric] = imfindcircles(bindata(:,:,n),puprange,'Sensitivity',0.995,'ObjectPolarity','bright');
     if(isempty(center))
         centroid(n,:) = [NaN NaN]; % could not find anything...
-        area(n) = NaN;
+        rad(n) = NaN;
     else
         [~,idx] = max(metric); % pick the circle with best score
         centroid(n,:) = center(idx,:);
-        area(n) = pi*radii(idx)^2;
+        rad(n,:) = radii(idx);
+    end
+    if n>framerange && (isnan(rad(n-1)) | isnan(rad(n))) %if it's a nan or preceeded by all nans don't change puprange
+        puprange = puprange;
+    elseif n>framerange && (abs(1 - rad(n)/nanmean(rad(n-framerange:n-1))) > pupchange) %if % change is bigger than specified don't change puprange
+        puprange = puprange;
+    elseif n>framerange && (rad(n)>nanmean(rad(n-framerange:n-1))) %if radius goes up, shift range up
+        puprange = puprange + round(rad(n) - nanmean(rad(n-1)));
+    elseif n>framerange && (rad(n)<nanmean(rad(n-framerange:n-1))) %if radius goes down, shift range down
+        puprange = puprange - round(nanmean(rad(n-1)) - rad(n));
+    else
+        puprange = puprange;
     end
 end
 toc
 
-figure
-imshow(data(yc-W:yc+W,xc-W:xc+W));
-hold on
-circle(eye(i).Centroid(2),eye(i).Centroid(1),eye(i).Area)
-hold off
+% figure
+% imshow(data(yc-W:yc+W,xc-W:xc+W));
+% hold on
+% circle(eye(i).Centroid(2),eye(i).Centroid(1),eye(i).Area)
+% hold off
 
-imshow(data(:,:,1))
-for i=i:size(data,3)/100
-   eye(i).Centroid(1) = yc - (yc - eye(i).Centroid(1));
-   eye(i).Centroid(2) = xc - (xc - eye(i).Centroid(2));
-end
+% imshow(data(:,:,1))
+% for i=i:size(data,3)
+%    eye(i).Centroid(1) = yc - (yc - eye(i).Centroid(1));
+%    eye(i).Centroid(2) = xc - (xc - eye(i).Centroid(2));
+% end
 save(name, 'centroid','area', '-append'); % append the motion estimate data...
 % profile viewer
 
 figure
 hold on
-plot(0.1:0.1:size(data,3)/10,sqrt(area/pi),'b-')
+plot(0.1:0.1:size(data,3)/10,sqrt(rad/pi),'b-')
 plot(0.1:0.1:size(data,3)/10,centroid(:,1),'.g')
 plot(0.1:0.1:size(data,3)/10,centroid(:,2),'.r')
 hold off
@@ -73,19 +90,19 @@ legend('area','x pos','ypos')
 figure
 for i = 1:size(data,3)
     subplot(1,2,1)
-    imshow(data(yc-W:yc+W,xc-W:xc+W,i));
+    imshow(data(yc-vert:yc+vert,xc-horiz:xc+horiz,i));
     colormap gray
     hold on
-    circle(centroid(i,1),centroid(i,2),area(i))
+    circle(centroid(i,1),centroid(i,2),rad(i))
     %     viscircles([eye(i).Centroid(1) eye(i).Centroid(2)],sqrt(eye(i).Area)/pi);
     drawnow
     hold off
     
     subplot(1,2,2)
-    imshow(bindata(yc-W:yc+W,xc-W:xc+W,i));
+    imshow(bindata(:,:,i));
     colormap gray
     hold on
-    circle(centroid(i,1),centroid(i,2),area(i))
+    circle(centroid(i,1),centroid(i,2),rad(i))
     %     viscircles([eye(i).Centroid(1) eye(i).Centroid(2)],sqrt(eye(i).Area)/pi);
     drawnow
     hold off

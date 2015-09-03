61b3583b75314b4bc00371120715eb73977946fd
Jackson <niell.lab.orders@gmail.com>
2015-09-03 13:08:48 -0700

    updating eye tracking
    nearing final version

--- analysis/eye/eyetracking.m
+++ analysis/eye/eyetracking.m
@@ -1,11 +1,15 @@
 clear all
 close all
 
-name = 'trial5'; %data file
+dir = '\\lorentz\backup\widefield\DOI experiments\Matlab Widefield Analysis\';
+name = 'trial6'; %data file
 thresh = 0.85; %pupil threshold for binarization
-pupercent = 0.15; %set range pupil radius window
-pupchange = 0.35; %acceptable percent change in radius per framerange
-framerange = 3; %number of frames to smooth over
+puprange = [5 20]; %set
+
+%%closed loop parameters
+% pupercent = 0.15; %set range pupil radius window
+% pupchange = 0.25; %acceptable percent change in radius per framerange
+% framerange = 1; %number of frames to smooth over
 
 load(name);
 data = squeeze(data);
@@ -13,16 +17,16 @@ warning off;
 
 %user input to select center and right points
 printf('Please select pupil center and top, eyeball top and right points, outside pupil')
-h = figure('units','normalized','outerposition',[0 0 1 1])
+h1 = figure('units','normalized','outerposition',[0 0 1 1])
 imshow(data(:,:,1))
 [cent] = ginput(5);
-close(h);
+close(h1);
 yc = cent(1,2); %pupil center y val
 xc = cent(1,1); %pupil center x val
 horiz = (cent(4,1) - xc); %1/2 x search range
 vert = (yc - cent(3,2)); %1/2 y search range
 puprad = yc - cent(2,2); %initial pupil radius
-puprange = [round(puprad - puprad*pupercent) round(puprad + puprad*pupercent)]; %range of pupil sizes to search over
+% puprange = [round(puprad - puprad*pupercent) round(puprad + puprad*pupercent)]; %range of pupil sizes to search over
 
 %convert from uint8 into doubles and threshold, then binarize
 ddata = double(data); 
@@ -50,41 +54,29 @@ for n = 2:size(data,3)
         centroid(n,:) = center(idx,:);
         rad(n,:) = radii(idx);
     end
-    if n>framerange && (isnan(rad(n-1)) | isnan(rad(n))) %if it's a nan or preceeded by all nans don't change puprange
-        puprange = puprange;
-    elseif n>framerange && (abs(1 - rad(n)/nanmean(rad(n-framerange:n-1))) > pupchange) %if % change is bigger than specified don't change puprange
-        puprange = puprange;
-    elseif n>framerange && (rad(n)>nanmean(rad(n-framerange:n-1))) %if radius goes up, shift range up
-        puprange = puprange + round(rad(n) - nanmean(rad(n-1)));
-    elseif n>framerange && (rad(n)<nanmean(rad(n-framerange:n-1))) %if radius goes down, shift range down
-        puprange = puprange - round(nanmean(rad(n-1)) - rad(n));
-    else
-        puprange = puprange;
-    end
+    %%closed loop execution
+%     if n>framerange && (isnan(rad(n-1)) | isnan(rad(n))) %if it's a nan or preceeded by all nans don't change puprange
+%         puprange = puprange;
+%     elseif n>framerange && (abs(1 - rad(n)/nanmean(rad(n-framerange:n-1))) > pupchange) %if % change is bigger than specified don't change puprange
+%         puprange = puprange;
+%     elseif n>framerange && (rad(n)>nanmean(rad(n-framerange:n-1))) %if radius goes up, shift range up
+%         puprange = puprange + round(rad(n) - nanmean(rad(n-1)));
+%     elseif n>framerange && (rad(n)<nanmean(rad(n-framerange:n-1))) %if radius goes down, shift range down
+%         puprange = puprange - round(nanmean(rad(n-1)) - rad(n));
+%     else
+%         puprange = puprange;
+%     end
 end
 toc
 
-% figure
-% imshow(data(yc-W:yc+W,xc-W:xc+W));
-% hold on
-% circle(eye(i).Centroid(2),eye(i).Centroid(1),eye(i).Area)
-% hold off
-
-% imshow(data(:,:,1))
-% for i=i:size(data,3)
-%    eye(i).Centroid(1) = yc - (yc - eye(i).Centroid(1));
-%    eye(i).Centroid(2) = xc - (xc - eye(i).Centroid(2));
-% end
-save(name, 'centroid','area', '-append'); % append the motion estimate data...
-% profile viewer
-
-figure
+%plot centroid and radius
+h2 = figure
 hold on
-plot(0.1:0.1:size(data,3)/10,sqrt(rad/pi),'b-')
+plot(0.1:0.1:size(data,3)/10,rad,'b-')
 plot(0.1:0.1:size(data,3)/10,centroid(:,1),'.g')
 plot(0.1:0.1:size(data,3)/10,centroid(:,2),'.r')
 hold off
-legend('area','x pos','ypos')
+legend('radius','x pos','ypos')
 
 % %
 figure
@@ -108,6 +100,8 @@ for i = 1:size(data,3)
     hold off
     
 end
+
+save(fullfile(dir,name),'centroid','rad','h2','-append');
 
 
 

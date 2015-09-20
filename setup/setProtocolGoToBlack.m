71ce5c8dc980737719ad4f274888abbaff3daad5
mtrix10 <niell.lab.orders@gmail.com>
2015-09-20 10:11:40 -0700

    g62w4tt changed name to g6w4tt
    change for update to blog

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -40,11 +40,11 @@ switch subjIDs{1}
        msPenalty               =3500;
        percentCorrectionTrials = .50; 
        
-    case 'g62w4tt' % Started 9/15/15
+    case 'g6w4tt' % Started 9/15/15
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 107;
        msPenalty               =3500;
-       percentCorrectionTrials = .50; 
+       percentCorrectionTrials = .25; 
        
     case 'g62t5rt' % Started 9/11/15
        requestRewardSizeULorMS = 0;

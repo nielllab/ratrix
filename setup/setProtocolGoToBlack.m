f7a18fd9989dfe4774a2ba0d600d46c64c559fea
mtrix10 <niell.lab.orders@gmail.com>
2015-10-12 14:14:21 -0700

    g62r9tt go to black
    g62r9tt go to black

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -33,7 +33,12 @@ switch subjIDs{1}
        percentCorrectionTrials = .5;
        stim.slowSecs = 5;
        
-    
+    case 'g62r9tt' % Started 10/12/15
+       requestRewardSizeULorMS = 20;
+       rewardSizeULorMS        = 107;
+       msPenalty               =3500;
+       percentCorrectionTrials = .50; 
+       
     case 'g62x1rt' % Started 10/8/15
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 107;

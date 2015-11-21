1d8272da0a89d560d6304698e5da339e1efc43e7
mtrix10 <niell.lab.orders@gmail.com>
2015-11-20 18:02:54 -0800

    add stop reward 2.6rt

--- setup/setProtocolBallHvV_center_vertical.m
+++ setup/setProtocolBallHvV_center_vertical.m
@@ -56,7 +56,7 @@ switch subjIDs{1}
        
     
     case 'g62tx2.6rt' % Started 11/8/15
-       requestRewardSizeULorMS = 0;
+       requestRewardSizeULorMS = 15;
        rewardSizeULorMS        = 107;
        msPenalty               =3500;
        percentCorrectionTrials = .50; 

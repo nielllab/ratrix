193d5b808ab1732434c0d8ec1d81e2cc025a2b77
mtrix10 <niell.lab.orders@gmail.com>
2015-11-24 16:09:45 -0800

    Behavior
    Behavior

--- setup/setProtocolBallHvV_center_vertical.m
+++ setup/setProtocolBallHvV_center_vertical.m
@@ -47,7 +47,7 @@ switch subjIDs{1}
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 107;
        msPenalty               =3500;
-       percentCorrectionTrials = .5; 
+       percentCorrectionTrials = .0; 
        
     
     case 'g62tx2.6rt' % Started 11/8/15

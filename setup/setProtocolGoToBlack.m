16fa41f2a8b53d3c37c42e6c73ff5f252281bacb
mtrix10 <niell.lab.orders@gmail.com>
2015-12-01 16:02:32 -0800

    Behavior
    Behavior

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -43,7 +43,7 @@ switch subjIDs{1}
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 107;
        msPenalty               =3500;
-       percentCorrectionTrials = .0; 
+       percentCorrectionTrials = .5; 
 
 %    case 'g62tx2.8lt' % Started 11/16/15
 %        requestRewardSizeULorMS = 0;

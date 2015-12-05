c9c8e9a81d29093587acd96e80a0bdf6f3b0271f
mtrix10 <niell.lab.orders@gmail.com>
2015-12-05 14:20:39 -0800

    stop rewards off for r9

--- setup/setProtocolGTS.m
+++ setup/setProtocolGTS.m
@@ -110,7 +110,7 @@ switch subjIDs{1}
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 107;
        msPenalty               =3500;
-       percentCorrectionTrials = .50; 
+       percentCorrectionTrials = 0; 
     
     case 'g62x1rt' % Started 10/8/15 %%started gts 10/24/15
        requestRewardSizeULorMS = 0;
@@ -119,7 +119,7 @@ switch subjIDs{1}
        percentCorrectionTrials = .50; 
     
     case 'g62r9tt' % Started 10/12/15 %%started gts 10/24/15
-       requestRewardSizeULorMS = 27;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 107;
        msPenalty               =3500;
        percentCorrectionTrials = .15; 

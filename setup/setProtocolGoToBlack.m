cc9387779c6e7044f0b28a03b50af189118792c9
mtrix10 <niell.lab.orders@gmail.com>
2015-12-13 11:35:23 -0800

    Behavior
    Behavior CHanges

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -34,7 +34,7 @@ switch subjIDs{1}
        stim.slowSecs = 1;
        
    case 'g62bb3rt' % Started 12/9/15
-       requestRewardSizeULorMS = 11;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 107;
        msPenalty               =3500;
        percentCorrectionTrials = .50; 

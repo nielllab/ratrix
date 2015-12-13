5f89deb89c27c006159f83d266a0da597028e623
mtrix10 <niell.lab.orders@gmail.com>
2015-12-13 14:19:20 -0800

    Behavior
    Behavior

--- setup/setProtocolGTS.m
+++ setup/setProtocolGTS.m
@@ -81,11 +81,11 @@ switch subjIDs{1}
        msPenalty               =3500;
        percentCorrectionTrials = .50; 
     
-%     case 'g62w7tt' % Started 11/2/15
-%        requestRewardSizeULorMS = 0;
-%        rewardSizeULorMS        = 107;
-%        msPenalty               =3500;
-%        percentCorrectionTrials = .50; 
+    case 'g62w7tt' % Started 11/2/15
+       requestRewardSizeULorMS = 0;
+       rewardSizeULorMS        = 107;
+       msPenalty               =3500;
+       percentCorrectionTrials = .50; 
     
     
     case 'g62bb2rt' % Started 11/8/15 

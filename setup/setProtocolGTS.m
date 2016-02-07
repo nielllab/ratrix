05fb6276e1f3072dc68a412e7e129114ec070567
mtrix10 <niell.lab.orders@gmail.com>
2016-02-06 16:22:36 -0800

    behavior
    switch to GTS

--- setup/setProtocolGTS.m
+++ setup/setProtocolGTS.m
@@ -32,6 +32,18 @@ percentCorrectionTrials = .5;
 
 switch subjIDs{1}
 
+   case 'g62dd2ln' % Started 1/26/16
+       requestRewardSizeULorMS = 0;
+       rewardSizeULorMS        = 123;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5; 
+       
+   case 'g62tx1.9tt' % Started 1/26/16
+       requestRewardSizeULorMS = 0;
+       rewardSizeULorMS        = 123;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5;        
+    
    case 'g62tx1.9lt' % Started 1/26/16 (GTS 2/3/16)
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 123;

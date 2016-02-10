7cf725d7f811d686b63e19e252b604f2390b9030
mtrix10 <niell.lab.orders@gmail.com>
2016-02-09 16:04:24 -0800

    Behave
    Behave

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -96,7 +96,7 @@ switch subjIDs{1}
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 123;
        msPenalty               =3500;
-       percentCorrectionTrials = .25; 
+       percentCorrectionTrials = .5; 
 
 
             

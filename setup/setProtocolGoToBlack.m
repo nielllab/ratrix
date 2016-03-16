583a5db327a1f78a019d36188f55f562e0270d18
mtrix10 <niell.lab.orders@gmail.com>
2016-03-16 13:43:58 -0700

    Behavior
    Behavior

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -50,11 +50,11 @@ switch subjIDs{1}
        msPenalty               =3500;
        percentCorrectionTrials = .5; 
        
-    case 'g62dd4' % Started 3/3/16
-       requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 139;
-       msPenalty               =3500;
-       percentCorrectionTrials = .5;       
+%     case 'g62dd4' % Started 3/3/16
+%        requestRewardSizeULorMS = 0;
+%        rewardSizeULorMS        = 139;
+%        msPenalty               =3500;
+%        percentCorrectionTrials = .5;       
        
     case 'testgotoblackstopime' % Started 6/25/15
        requestRewardSizeULorMS = 20;

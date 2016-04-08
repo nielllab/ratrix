8729f71e5efbc2d6b2a58c6ae6ea00edbcab2f56
mtrix10 <niell.lab.orders@gmail.com>
2016-04-07 18:21:27 -0700

    Behavior
    behavior

--- setup/setProtocolGTS.m
+++ setup/setProtocolGTS.m
@@ -77,7 +77,12 @@ switch subjIDs{1}
        msPenalty               =3500;
        percentCorrectionTrials = .5;        
     
-    
+   case 'g62tx1.9lt' % Started 1/26/16 (GTS 2/3/16) %back to GoToBlack 4/2/16
+       requestRewardSizeULorMS = 0;
+       rewardSizeULorMS        = 123;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5; 
+       
    case 'g62tx1.9ln' % Started 1/26/16 (GTS 2/3/16)
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 123;

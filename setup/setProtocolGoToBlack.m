e5ac93ce97e3060d4282e16544cc495729368d11
mtrix10 <niell.lab.orders@gmail.com>
2016-05-06 10:27:27 -0700

    Behavior
    Ryan

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -28,13 +28,13 @@ stim.gain = 0.7 * ones(2,1);
 switch subjIDs{1}
 
    case 'g62jj2lt' % Started 5/4/16
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 123;
        msPenalty               =3500;
        percentCorrectionTrials = .5;
     
    case 'g62jj2rt' % Started 5/4/16
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 123;
        msPenalty               =3500;
        percentCorrectionTrials = .5;

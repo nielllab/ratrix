340f28dbde889252b4f3d026d5fc0b8ed24084d3
mtrix10 <niell.lab.orders@gmail.com>
2016-05-12 11:14:03 -0700

    Behavior
    Behavior

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -27,9 +27,21 @@ stim.gain = 0.7 * ones(2,1);
 
 switch subjIDs{1}
     
-   case 'g62cc9rt' % Started 4/24/16
-       requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 123;
+   case 'g62gg7' % Started 5/12/16
+       requestRewardSizeULorMS = 43;
+       rewardSizeULorMS        = 219;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5;    
+    
+   case 'g62bb12tt' % Started 5/12/16
+       requestRewardSizeULorMS = 43;
+       rewardSizeULorMS        = 219;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5;   
+    
+   case 'g62bb12lt' % Started 5/12/16
+       requestRewardSizeULorMS = 43;
+       rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5;
 

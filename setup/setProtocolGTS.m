48f446b3d7c7d2cf62a0530b4f708d133adacce2
mtrix10 <niell.lab.orders@gmail.com>
2016-05-24 11:13:27 -0700

    Correction trials gg7
    Ryan behavior

--- setup/setProtocolGTS.m
+++ setup/setProtocolGTS.m
@@ -37,7 +37,7 @@ switch subjIDs{1}
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
-       percentCorrectionTrials = .5;        
+       percentCorrectionTrials = .75;        
   
    case 'g62ff6lt' % Started 4/13/16 
        requestRewardSizeULorMS = 0;

4dfe5cc29a7f9e0119b60874d5ba39014d62566b
mtrix10 <niell.lab.orders@gmail.com>
2016-05-27 13:47:15 -0700

    joe
    behavior

--- setup/setProtocolBallHvV_center_vertical.m
+++ setup/setProtocolBallHvV_center_vertical.m
@@ -79,11 +79,12 @@ switch subjIDs{1}
 %        percentCorrectionTrials = .8;    
        
        
-%     case 'g62tx2.11lt' % Started 2/9/16 %started HvV_center 2/20/16
-%        requestRewardSizeULorMS = 0;
-%        rewardSizeULorMS        = 139;
-%        msPenalty               =3500;
-%        percentCorrectionTrials = .8; 
+    case 'g62tx2.11lt' % Started 2/9/16 %started HvV_center 2/20/16
+       requestRewardSizeULorMS = 0;
+       rewardSizeULorMS        = 139;
+       msPenalty               =3500;
+       percentCorrectionTrials = .8; 
+       radius                 = .3;
        
 %    case 'g62hh4ln' % Started 2/9/16  %started HvV_center 2/20/16 back to GoToBlack 2/28/16
 %        requestRewardSizeULorMS = 0;

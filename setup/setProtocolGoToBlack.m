be8d2df373ad26974cc484fb8cb35e7438cce063
mtrix10 <niell.lab.orders@gmail.com>
2016-05-30 13:42:22 -0700

    Add ll3rt
    Ryan Beavior

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -27,6 +27,12 @@ stim.gain = 0.7 * ones(2,1);
 
 switch subjIDs{1}
     
+    case 'g62ll3rt' % Started 5/30/16
+       requestRewardSizeULorMS = 43;
+       rewardSizeULorMS        = 219;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5;     
+    
     case 'g62ll4lt' % Started 5/22/16
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;

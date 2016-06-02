a71194496b57953e34de6a2b88f8cb13aa347643
mtrix10 <niell.lab.orders@gmail.com>
2016-06-02 16:35:01 -0700

    Add jj1lt Gotoblack
    Ryan behavior

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -27,6 +27,12 @@ stim.gain = 0.7 * ones(2,1);
 
 switch subjIDs{1}
     
+    case 'g62jj1lt' % Started 6/3/16
+       requestRewardSizeULorMS = 43;
+       rewardSizeULorMS        = 219;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5;        
+    
     case 'g62ll3rt' % Started 5/30/16
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;

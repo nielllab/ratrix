8fe2f07ee1dafed305664526b673a0089766e25e
mtrix10 <niell.lab.orders@gmail.com>
2016-06-20 12:15:54 -0700

    Add pvchr2b16tt
    Ryan

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -27,6 +27,13 @@ stim.gain = 0.7 * ones(2,1);
 
 switch subjIDs{1}
     
+      case 'pvchr2b16tt' % Started 
+       requestRewardSizeULorMS = 0;
+       rewardSizeULorMS        = 219;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5;   
+      
+    
     case 'g62ff7ln' % Started 6/14/16
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;

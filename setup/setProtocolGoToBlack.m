ebcd3108cbdd74227d937f8081960025b33ab9ed
mtrix10 <niell.lab.orders@gmail.com>
2016-07-08 13:07:24 -0700

    Add pvchr to Gotoblack
    Ryan

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -27,6 +27,18 @@ stim.gain = 0.7 * ones(2,1);
 
 switch subjIDs{1}
     
+    case 'pvchr2b16tt' % Started 7/11/16 
+       requestRewardSizeULorMS = 0;
+       rewardSizeULorMS        = 219;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5;     
+    
+    case 'pvchr2b16lt' % Started 7/11/16 
+       requestRewardSizeULorMS = 0;
+       rewardSizeULorMS        = 219;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5;      
+    
     case 'g62ff8rt' % Started 7/1/16 
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;

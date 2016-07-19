29f90034a256369e4da6c627f88dada962ddf74a
mtrix10 <niell.lab.orders@gmail.com>
2016-07-19 16:59:41 -0700

    Add gg10lt
    Ryan

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -37,9 +37,15 @@ switch subjIDs{1}
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
-       percentCorrectionTrials = .5;      
+       percentCorrectionTrials = .5;  
+       
+    case 'g62gg10lt' % Started 7/20/16 
+       requestRewardSizeULorMS = 43;
+       rewardSizeULorMS        = 219;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5;     
     
-     case 'g62cc13lt' % Started 7/8/16 
+    case 'g62cc13lt' % Started 7/8/16 
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;

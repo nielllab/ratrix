e2f7da2e760b781db49b7dd9f4db3fda41f781bf
mtrix10 <niell.lab.orders@gmail.com>
2016-08-04 11:29:22 -0700

    stop rewards off
    behavior

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -28,13 +28,13 @@ stim.gain = 0.7 * ones(2,1);
 switch subjIDs{1}
     
     case 'g62ff10rt' % Started 8/2/16 
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5;    
        
      case 'g62kk7lt' % Started 8/2/16 
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5;      

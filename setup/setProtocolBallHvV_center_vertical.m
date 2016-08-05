38a8adb4249819724a673f8d6d7eb3e3773a1c21
Nlab-w7-64 <niell.lab.orders@gmail.com>
2016-08-04 17:37:04 -0700

    add dd9
    hvv

--- setup/setProtocolBallHvV_center_vertical.m
+++ setup/setProtocolBallHvV_center_vertical.m
@@ -63,11 +63,11 @@ switch subjIDs{1}
        msPenalty               =3500;
        percentCorrectionTrials = .75;        
        
-%    case 'g62dd9lt' % Started 6/14/16
-%        requestRewardSizeULorMS = 0;
-%        rewardSizeULorMS        = 219;
-%        msPenalty               =3500;
-%        percentCorrectionTrials = .75;   
+    case 'g62dd9lt' % Started 6/14/16  restarted 8/5/16.
+        requestRewardSizeULorMS = 0;
+        rewardSizeULorMS        = 219;
+        msPenalty               =3500;
+        percentCorrectionTrials = .75;   
     
    case 'g62ff7ln' % Started 6/14/16
        requestRewardSizeULorMS = 0;

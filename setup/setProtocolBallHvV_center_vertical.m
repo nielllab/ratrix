ba814a2898696c5eca4f45de4ee1467001c0d18a
nielllab - ryans comp <niell.lab.orders@gmail.com>
2016-09-13 15:51:35 -0700

    behavior
    bewhiv

--- setup/setProtocolBallHvV_center_vertical.m
+++ setup/setProtocolBallHvV_center_vertical.m
@@ -31,6 +31,11 @@ if ~isscalar(subjIDs)
 end
 switch subjIDs{1}
     
+    case 'hvvtest' 
+       requestRewardSizeULorMS = 0;
+       rewardSizeULorMS        = 219;
+       msPenalty               =3500;
+       percentCorrectionTrials = .75; 
     
     case 'pvchr3b11lt' % Started 9/8/16 
        requestRewardSizeULorMS = 0;

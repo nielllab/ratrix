d9d2242ee660d73f21856544de17b5c6b6716d5a
nielllab - ryans comp <niell.lab.orders@gmail.com>
2016-09-18 17:06:14 -0700

    remove stop
    Ryan

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -28,19 +28,19 @@ stim.gain = 0.7 * ones(2,1);
 switch subjIDs{1}
 
      case 'test2' % Started 9/9/16 
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5;     
 
     case 'g62ss2rt' % Started 9/16/16 
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5;              
        
     case 'g62ss1lt' % Started 9/16/16 
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5;       

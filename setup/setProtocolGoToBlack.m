eb615d75fd4ef8b782ed116cf1205dd3a2b987e7
nielllab - ryans comp <niell.lab.orders@gmail.com>
2016-09-28 13:15:38 -0700

    Add cc16
    Ryan

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -26,7 +26,19 @@ stim.gain = 0.7 * ones(2,1);
 
 
 switch subjIDs{1}
-
+    
+    case 'g62cc16ln' % Started 9/28/16 
+       requestRewardSizeULorMS = 43;
+       rewardSizeULorMS        = 219;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5; 
+    
+    case 'g62cc16lt' % Started 9/28/16 
+       requestRewardSizeULorMS = 43;
+       rewardSizeULorMS        = 219;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5; 
+       
      case 'test2' % Started 9/9/16 
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
@@ -38,12 +50,7 @@ switch subjIDs{1}
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5;              
-       
-    case 'g62ss1lt' % Started 9/16/16 
-       requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 219;
-       msPenalty               =3500;
-       percentCorrectionTrials = .5;       
+             
        
     case 'g62mm9ln' % Started 9/9/16 
        requestRewardSizeULorMS = 0;

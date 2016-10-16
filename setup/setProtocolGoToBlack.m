c2171d92ec8f32cd688cb0b7631aeda151e11b13
nielllab - ryans comp <niell.lab.orders@gmail.com>
2016-10-15 21:15:19 -0700

    push
    push

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -45,7 +45,7 @@ switch subjIDs{1}
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 59;
        msPenalty               =3500;
-       percentCorrectionTrials = .7;   
+       percentCorrectionTrials = .6;   
     
     case 'g62tt1lt' % Started 10/6/16 
        requestRewardSizeULorMS = 0;
@@ -368,7 +368,7 @@ switch subjIDs{1}
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 59;
        msPenalty               =3500;
-       percentCorrectionTrials = .7;  
+       percentCorrectionTrials = .6;  
 %     
 %     case 'g62tx1.5lt' % Started 1/12/16
 %        requestRewardSizeULorMS = 0;

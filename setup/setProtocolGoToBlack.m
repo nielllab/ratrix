6de072249e76bacd831adde68dacc8baaf7c4d8f
nielllab - ryans comp <niell.lab.orders@gmail.com>
2016-10-15 17:24:24 -0700

    push behav
    gotoblack

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -96,17 +96,17 @@ switch subjIDs{1}
        msPenalty               =3500;
        percentCorrectionTrials = .5;  
     
-%     case 'pvchr3b15rt' % Started 9/9/16
-%        requestRewardSizeULorMS = 0;
-%        rewardSizeULorMS        = 219;
-%        msPenalty               =3500;
-%        percentCorrectionTrials = .5;       
-%     
-%     case 'pvchr3b15lt' % Started 9/9/16
-%        requestRewardSizeULorMS = 0;
-%        rewardSizeULorMS        = 219;
-%        msPenalty               =3500;
-%        percentCorrectionTrials = .5;   
+    case 'pvchr3b15rt' % Started 9/9/16
+       requestRewardSizeULorMS = 0;
+       rewardSizeULorMS        = 219;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5;       
+    
+    case 'pvchr3b15lt' % Started 9/9/16
+       requestRewardSizeULorMS = 0;
+       rewardSizeULorMS        = 219;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5;   
 %     
 %     case 'pvchr3b14tt' % Started 9/9/16
 %        requestRewardSizeULorMS = 0;
@@ -120,11 +120,11 @@ switch subjIDs{1}
 %        msPenalty               =3500;
 %        percentCorrectionTrials = .5;   
        
-%     case 'pvchr3b14lt' % Started 9/9/16
-%        requestRewardSizeULorMS = 0;
-%        rewardSizeULorMS        = 219; 
-%        msPenalty               =3500;
-%        percentCorrectionTrials = .5;  
+    case 'pvchr3b14lt' % Started 9/9/16
+       requestRewardSizeULorMS = 0;
+       rewardSizeULorMS        = 219; 
+       msPenalty               =3500;
+       percentCorrectionTrials = .5;  
        
        
 %    case 'pvchr3b11rt' % Started 8/30/16 

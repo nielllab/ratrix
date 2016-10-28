bf22ffa7c066b2ff9cb9de76016cf598f65b7cf8
Newton <niell.lab.orders@gmail.com>
2016-10-28 13:14:41 -0700

    check timing
    fix this

--- analysis/imaging/2p functions/checkBehavTiming.m
+++ analysis/imaging/2p functions/checkBehavTiming.m
@@ -10,5 +10,6 @@ stopRatrix = abs(starts(:,1));
 err = mean(abs(stopTTL(1:length(stopRatrix))-stopRatrix));
 
 figure
-plot(stopTTL); hold on; plot(stopRatrix,'.'); title(sprintf('%s %s err = %0.3f',files(i).subj,files(i).expt,err)); legend({'TTL','Ratrix'}); xlabel('trial #'); ylabel('secs');
+plot(stopTTL); hold on; plot(stopRatrix,'.'); title(sprintf('err = 0.3',err)); legend({'TTL','Ratrix'}); xlabel('trial #'); ylabel('secs');
  
+

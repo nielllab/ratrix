eed7acbb2f5d0d0ac667c434e73fedcaba653edf
SENDAK <niell.lab.orders@gmail.com>
2016-10-29 15:00:46 -0700

    downsamplebin un-delete
    phil

---
+++ analysis/imaging/downsamplebin.m
@@ -0,0 +1,14 @@
+function [outv] = downsamplebin(invec,dimnum,amount,usemean)
+  %%% courtesy patrick mineault
+  %%% http://xcorr.net/2008/03/13/binning-in-matlab-a-one-liner/
+  %%% inputs = data, dimension to reduce, bin factor, option to use nanmean (gets rid of nans, but normalizes) 
+  sizebefore = size(invec);
+    sizemiddle = [sizebefore(1:dimnum-1),amount,sizebefore(dimnum)/amount,sizebefore(dimnum+1:length(sizebefore))];
+    sizeafter = sizebefore;
+    sizeafter(dimnum) = sizeafter(dimnum)/amount;
+   if exist('usemean','var') & usemean==1
+       outv = reshape(nanmean(reshape(invec,sizemiddle),dimnum),sizeafter);
+   else
+       outv = reshape(sum(reshape(invec,sizemiddle),dimnum),sizeafter);
+   end
+end

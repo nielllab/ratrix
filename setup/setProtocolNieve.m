9b96c449ca3686b86ac67a3306707a2026115733
nielllab - ryans comp <niell.lab.orders@gmail.com>
2016-11-02 14:36:48 -0700

    behavior
    Ryan

--- setup/setProtocolNieve.m
+++ setup/setProtocolNieve.m
@@ -30,6 +30,13 @@ if ~isscalar(subjIDs)
 end
 switch subjIDs{1}
     
+    
+     case 'g62qq1rt' % Started 11/2/16
+       requestRewardSizeULorMS = 0;
+       rewardSizeULorMS        = 219;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5;  
+    
      case 'g62qq2lt' % start nieve 10/27/16
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 60;

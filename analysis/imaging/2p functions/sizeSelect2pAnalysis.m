497b8ffa3d1cfa5c41c1d575a940e028e2baea49
SENDAK <niell.lab.orders@gmail.com>
2016-11-28 16:39:26 -0800

    2p size select analysis update
    phil

--- analysis/imaging/2p functions/sizeSelect2pAnalysis.m
+++ analysis/imaging/2p functions/sizeSelect2pAnalysis.m
@@ -4,7 +4,7 @@ clear all
 close all
 dbstop if error
 
-pre = 0; %1 if pre, 0 if post, determines naming of output file
+pre = 1; %1 if pre, 0 if post, determines naming of output file
 exclude = 0; %0 removes trials above threshold, 1 clips them to the threshold
 peakWindow = 9:12;
 dt = 0.1;

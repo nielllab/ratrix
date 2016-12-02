aa6610af89e11158098026e70c205ca9fac687a0
nielllab - ryans comp <niell.lab.orders@gmail.com>
2016-12-02 11:29:54 -0800

    request reward removal
    Ryan

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -34,7 +34,7 @@ switch subjIDs{1}
        percentCorrectionTrials = .5;  
     
    case 'g62mm11lt' 
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 123;
        msPenalty               =3500;
        percentCorrectionTrials = .5; 

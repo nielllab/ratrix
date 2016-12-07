bb3593be8f613f8ddc9892a17537ce758872ce6f
nielllab - ryans comp <niell.lab.orders@gmail.com>
2016-12-07 13:33:51 -0800

    behavior
    Ryan

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -27,6 +27,25 @@ stim.gain = 0.7 * ones(2,1);
 
 switch subjIDs{1}
 
+    
+   case 'g62rr1lt' 
+       requestRewardSizeULorMS = 43;
+       rewardSizeULorMS        = 123;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5;      
+    
+   case 'g62aa2lt' 
+       requestRewardSizeULorMS = 43;
+       rewardSizeULorMS        = 123;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5;     
+    
+   case 'g62pp6lt' 
+       requestRewardSizeULorMS = 43;
+       rewardSizeULorMS        = 123;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5;      
+    
    case 'g62pp4rt' 
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 123;

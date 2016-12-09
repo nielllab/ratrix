0b72e571460bdd855cf2fc3c3f0a755af4469f7b
nielllab - ryans comp <niell.lab.orders@gmail.com>
2016-12-08 16:14:22 -0800

    behavior
    Ryan

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -29,19 +29,19 @@ switch subjIDs{1}
 
     
    case 'g62rr1lt' 
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 123;
        msPenalty               =3500;
        percentCorrectionTrials = .5;      
     
    case 'g62aa2lt' 
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 123;
        msPenalty               =3500;
        percentCorrectionTrials = .5;     
     
    case 'g62pp6rt' 
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 123;
        msPenalty               =3500;
        percentCorrectionTrials = .5;      

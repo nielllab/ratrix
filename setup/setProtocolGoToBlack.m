a245516dd4077af8faae8e88c645b635410e34d2
nielllab - ryans comp <niell.lab.orders@gmail.com>
2017-01-26 06:58:02 -0800

    Aldis
    request reward dropped to 0

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -28,31 +28,31 @@ stim.gain = 0.7 * ones(2,1);
 switch subjIDs{1}
 
    case 'pvchr4b4nt' %started 1/23/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5; 
   
    case 'pvchr4b4rt' %started 1/23/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5; 
     
    case 'pvchr4b4lt' %started 1/23/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5; 
     
    case 'pvchr4b3rt' %started 1/23/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5;  
     
    case 'pvchr4b3tt' %started 1/23/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5; 

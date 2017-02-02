5e5a26cb9ee25273a24b90b06c48d612d0ba30ba
nielllab - ryans comp <niell.lab.orders@gmail.com>
2017-02-02 11:31:00 -0800

    Remove Stop
    Ryan

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -82,7 +82,7 @@ switch subjIDs{1}
        percentCorrectionTrials = .5;      
        
    case 'pvchr3b14rt' % Started 2/1/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5;       
@@ -106,25 +106,25 @@ switch subjIDs{1}
        percentCorrectionTrials = .5;  
     
    case 'g62mm11lt' % Started 2/1/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 123;
        msPenalty               =3500;
        percentCorrectionTrials = .5; 
     
    case 'g62uu1tt' % Started 2/1/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 123;
        msPenalty               =3500;
        percentCorrectionTrials = .5;    
     
    case 'g62uu1rt' % Started 2/1/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 123;
        msPenalty               =3500;
        percentCorrectionTrials = .5; 
        
     case 'g62uu1lt' % Started 2/1/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 123;
        msPenalty               =3500;
        percentCorrectionTrials = .5;       

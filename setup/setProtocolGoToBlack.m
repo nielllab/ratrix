405cc9ce8efa26fd447cbb36576a32c26d0547cf
nielllab - ryans comp <niell.lab.orders@gmail.com>
2017-02-02 17:34:50 -0800

    Remove Stop
    Ryan

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -28,55 +28,55 @@ stim.gain = 0.7 * ones(2,1);
 switch subjIDs{1}
 
    case 'g62aaa3rt' % Started 2/1/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5;      
     
    case 'g62aaa3lt' % Started 2/1/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5;      
     
    case 'g62aaa2lt' % Started 2/1/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5;      
     
    case 'g62uu4lt' % Started 2/1/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5;      
     
    case 'pvchr4b1rt' % Started 2/1/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5;      
 
    case 'pvchr4b1lt' % Started 2/1/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5;      
     
    case 'pvchr4b2ln' % Started 2/1/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5;      
     
    case 'pvchr4b2tt' % Started 2/1/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5;  
     
    case 'g62zz2tt' % Started 2/1/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;
        percentCorrectionTrials = .5;      
@@ -88,7 +88,7 @@ switch subjIDs{1}
        percentCorrectionTrials = .5;       
     
    case 'g62rr1lt' % Started 2/1/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 123;
        msPenalty               =3500;
        percentCorrectionTrials = .5;      
@@ -100,7 +100,7 @@ switch subjIDs{1}
        percentCorrectionTrials = .5;      
     
    case 'g62pp4rt' % Started 2/1/17
-       requestRewardSizeULorMS = 43;
+       requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 123;
        msPenalty               =3500;
        percentCorrectionTrials = .5;  

a503af1dbf69c478765ca9683e16772f96756419
nielllab - ryans comp <niell.lab.orders@gmail.com>
2017-02-14 07:40:48 -0800

    Aldis
    new mice

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -27,7 +27,20 @@ stim.gain = 0.7 * ones(2,1);
 
 switch subjIDs{1}
 
-%    case 'g62aaa3rt' % Started 2/1/17
+
+    case 'pvchr4b8lt' %started 2/16/17
+       requestRewardSizeULorMS = 43;
+       rewardSizeULorMS        = 219;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5; 
+    
+    case 'pvchr4b8tt' %started 2/16/17
+       requestRewardSizeULorMS = 43;
+       rewardSizeULorMS        = 219;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5; 
+    
+    %    case 'g62aaa3rt' % Started 2/1/17
 %        requestRewardSizeULorMS = 0;
 %        rewardSizeULorMS        = 219;
 %        msPenalty               =3500;

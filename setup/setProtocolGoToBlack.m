b28c3ff94f713896ea5ba57504b9d8bd192f581e
nielllab - ryans comp <niell.lab.orders@gmail.com>
2017-02-17 09:36:16 -0800

    Aldis
    test commit

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -28,7 +28,7 @@ stim.gain = 0.7 * ones(2,1);
 switch subjIDs{1}
 
 
-    case 'pvchr4b8lt' %started 2/16/17
+    case 'pvchr4b8lt'  %started 2/16/17
        requestRewardSizeULorMS = 0;
        rewardSizeULorMS        = 219;
        msPenalty               =3500;

0c15d7f89233a01dd6f8d90a73770c7395f55b33
nielllab - ryans comp <niell.lab.orders@gmail.com>
2017-03-06 10:56:15 -0800

    behavior
    joe hvv full

--- setup/setProtocolBallHvV_vertical.m
+++ setup/setProtocolBallHvV_vertical.m
@@ -38,8 +38,8 @@ end
        msPenalty               =3500;
        percentCorrectionTrials = .5;     
        pixPerCycs              = [200];
-       normalizedPosition      = [0.40 0.60];
-       radius                 = .29;     
+       normalizedPosition      = [0.37 0.63];
+       radius                 = .27;     
      
     case 'g62mm11lt' % Started 2/1/17 %%switch 2/8/17 %%%switch full 2/27/17
        requestRewardSizeULorMS = 0;
@@ -47,8 +47,8 @@ end
        msPenalty               =3500;
        percentCorrectionTrials = .5;     
        pixPerCycs              = [200];
-       normalizedPosition      = [0.40 0.60];
-       radius                 = .29;
+       normalizedPosition      = [0.37 0.63];
+       radius                 = .27;
        
     case 'g62aaa3rt' % Started 2/1/17  %%switch 2/9/17 %%%switch full 2/27/17
        requestRewardSizeULorMS = 0;
@@ -56,8 +56,8 @@ end
        msPenalty               =3500;
        percentCorrectionTrials = .5;
        pixPerCycs              = [200];
-       normalizedPosition      = [0.40 0.60];
-       radius                 = .29;
+       normalizedPosition      = [0.47 0.63];
+       radius                 = .27;
      
   case 'g62kk10tt' % Started 8/23/16 %%start hvv 9/19/16
        requestRewardSizeULorMS = 0;

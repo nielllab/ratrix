cbeaa20a3ca39baa7ec176de1c0f13863c058a46
nielllab - ryans comp <niell.lab.orders@gmail.com>
2017-03-27 12:30:38 -0700

    Behavior
    Ryan

--- setup/setProtocolNieve.m
+++ setup/setProtocolNieve.m
@@ -31,24 +31,24 @@ if ~isscalar(subjIDs)
 end
 switch subjIDs{1}
 
-%    case 'g62aaa3lt' % Started 2/1/17 %%started nieve 2/21/17
-%        requestRewardSizeULorMS = 0;
-%        rewardSizeULorMS        = 219;
-%        msPenalty               =3500;
-%        percentCorrectionTrials = .0;        
-%     
-%     
-%    case 'g62uu4lt' % Started 2/1/17  %%start random 2/17/17
-%        requestRewardSizeULorMS = 0;
-%        rewardSizeULorMS        = 219;
-%        msPenalty               =3500;
-%         percentCorrectionTrials = .0;  
-%        
-%    case 'g62aaa2lt' % Started 2/1/17  %%start random 2/15/17
-%        requestRewardSizeULorMS = 0;
-%        rewardSizeULorMS        = 219;
-%        msPenalty               =3500; 
-%         percentCorrectionTrials = .0;  
+   case 'g62aaa3lt' % Started 2/1/17 %%started nieve 2/21/17
+       requestRewardSizeULorMS = 0;
+       rewardSizeULorMS        = 219;
+       msPenalty               =3500;
+       percentCorrectionTrials = .0;        
+    
+    
+   case 'g62uu4lt' % Started 2/1/17  %%start random 2/17/17
+       requestRewardSizeULorMS = 0;
+       rewardSizeULorMS        = 219;
+       msPenalty               =3500;
+        percentCorrectionTrials = .0;  
+       
+   case 'g62aaa2lt' % Started 2/1/17  %%start random 2/15/17
+       requestRewardSizeULorMS = 0;
+       rewardSizeULorMS        = 219;
+       msPenalty               =3500; 
+        percentCorrectionTrials = .0;  
     
        
  

60fee8f44c1e9dd2c1d9286e1683989b88e72000
nielllab - ryans comp <niell.lab.orders@gmail.com>
2017-03-28 12:14:50 -0700

    Behavior
    Ryan

--- setup/setProtocolBallHvV_vertical.m
+++ setup/setProtocolBallHvV_vertical.m
@@ -48,7 +48,7 @@ end
        percentCorrectionTrials = .6;
        pixPerCycs              = [200];
        normalizedPosition      = [0.47 0.53];
-       radius                 = .32;  
+       radius                 = .31;  
        
     case 'g62mm11lt' % Started 2/1/17 %%switch 2/8/17 %%%switch full 2/27/17
        requestRewardSizeULorMS = 0;
@@ -57,7 +57,7 @@ end
        percentCorrectionTrials = .5;     
        pixPerCycs              = [200];
        normalizedPosition      = [0.43 0.57];
-       radius                 = .26;
+       radius                 = .25;
        
     case 'g62aaa3rt' % Started 2/1/17  %%switch 2/9/17 %%%switch full 2/27/17
        requestRewardSizeULorMS = 0;
@@ -66,7 +66,7 @@ end
        percentCorrectionTrials = .5;
        pixPerCycs              = [200];
        normalizedPosition      = [0.43 0.57];
-       radius                 = .29;
+       radius                 = .28;
      
    
   case 'test' % Started 5/4/16 %started HvV 9/6/16
--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -27,24 +27,24 @@ stim.gain = 0.7 * ones(2,1);
 
 switch subjIDs{1}
 
-   case 'g62aaa3lt' % Started 2/1/17 %%started nieve 2/21/17
-       requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 219;
-       msPenalty               =3500;
-       percentCorrectionTrials = .0;        
-    
-    
-   case 'g62uu4lt' % Started 2/1/17  %%start random 2/17/17
-       requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 219;
-       msPenalty               =3500;
-        percentCorrectionTrials = .0;  
-       
-   case 'g62aaa2lt' % Started 2/1/17  %%start random 2/15/17
-       requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 219;
-       msPenalty               =3500; 
-        percentCorrectionTrials = .0;  
+%    case 'g62aaa3lt' % Started 2/1/17 %%started nieve 2/21/17
+%        requestRewardSizeULorMS = 0;
+%        rewardSizeULorMS        = 219;
+%        msPenalty               =3500;
+%        percentCorrectionTrials = .0;        
+%     
+%     
+%    case 'g62uu4lt' % Started 2/1/17  %%start random 2/17/17
+%        requestRewardSizeULorMS = 0;
+%        rewardSizeULorMS        = 219;
+%        msPenalty               =3500;
+%         percentCorrectionTrials = .0;  
+%        
+%    case 'g62aaa2lt' % Started 2/1/17  %%start random 2/15/17
+%        requestRewardSizeULorMS = 0;
+%        rewardSizeULorMS        = 219;
+%        msPenalty               =3500; 
+%         percentCorrectionTrials = .0;  
         
     case 'pvchr238'  %started 3/6/17
        requestRewardSizeULorMS = 0;

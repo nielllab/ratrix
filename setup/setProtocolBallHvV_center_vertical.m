90d8d4c420cbc6410285dc15de1b0d1223ff862f
nielllab - ryans comp <niell.lab.orders@gmail.com>
2017-04-13 11:51:25 -0700

    Behavior
    Ryan

--- setup/setProtocolBallHvV_center_vertical.m
+++ setup/setProtocolBallHvV_center_vertical.m
@@ -43,6 +43,12 @@ switch subjIDs{1}
        rewardSizeULorMS        = 59;
        msPenalty               =6300;
         
+       
+    case 'g62ww3rt' % Started 4/05/17
+       requestRewardSizeULorMS = 0;
+       rewardSizeULorMS        = 219;
+       msPenalty               =3500;
+       percentCorrectionTrials = .5;       
 
     case 'g62aaa4tt' % Started 3/29/17 %%hvv start 4/8/17
        requestRewardSizeULorMS = 0;
--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -57,11 +57,11 @@ switch subjIDs{1}
        msPenalty               =3500;
        percentCorrectionTrials = .5;      
     
-   case 'g62ww3rt' % Started 4/05/17
-       requestRewardSizeULorMS = 0;
-       rewardSizeULorMS        = 219;
-       msPenalty               =3500;
-       percentCorrectionTrials = .5;        
+%    case 'g62ww3rt' % Started 4/05/17
+%        requestRewardSizeULorMS = 0;
+%        rewardSizeULorMS        = 219;
+%        msPenalty               =3500;
+%        percentCorrectionTrials = .5;        
     
    case 'g62ddd3ln' % Started 4/05/17
        requestRewardSizeULorMS = 0;

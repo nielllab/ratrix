2a0d8b37bab163ee5ed3d86d6018eb119af886d7
Joze-Monster <niell.lab.orders@gmail.com>
2017-04-14 13:24:06 -0700

    suite 2p modifications for joe behavior
    joe

--- analysis/imaging/2p functions/runSessionBatchPhil.m
+++ analysis/imaging/2p functions/runSessionBatchPhil.m
@@ -441,21 +441,21 @@ tic
 % sizeSelect2pSession('G62QQ2LT_001_005.sbx','sizeSession_POST_V2.mat',psfilename);
 % topo2pSession('G62QQ2LT_001_006.sbx','darkness_POST_V2.mat',psfilename);
 
-% %%%0030817
-% topo2pSession('G62W9RT_001_001.sbx','topoXsession_V2.mat',psfilename);
-% topo2pSession('G62W9RT_001_002.sbx','topoYsession_V2.mat',psfilename);
-% topo2pSession('G62W9RT_001_003.sbx','darkness_PRE_V2.mat',psfilename);
-% sizeSelect2pSession('G62W9RT_001_004.sbx','sizeSession_PRE_V2.mat',psfilename);
-% sizeSelect2pSession('G62W9RT_001_005.sbx','sizeSession_POST_V2.mat',psfilename);
-% topo2pSession('G62W9RT_001_006.sbx','darkness_POST_V2.mat',psfilename);
+%%%0030817
+topo2pSession('G62W9RT_001_001.sbx','topoXsession_V2.mat',psfilename);
+topo2pSession('G62W9RT_001_002.sbx','topoYsession_V2.mat',psfilename);
+topo2pSession('G62W9RT_001_003.sbx','darkness_PRE_V2.mat',psfilename);
+sizeSelect2pSession('G62W9RT_001_004.sbx','sizeSession_PRE_V2.mat',psfilename);
+sizeSelect2pSession('G62W9RT_001_005.sbx','sizeSession_POST_V2.mat',psfilename);
+topo2pSession('G62W9RT_001_006.sbx','darkness_POST_V2.mat',psfilename);
 
-%%%0030917
-topo2pSession('G62QQ2LT_001_001.sbx','topoXsession_V2.mat',psfilename);
-topo2pSession('G62QQ2LT_001_002.sbx','topoYsession_V2.mat',psfilename);
-topo2pSession('G62QQ2LT_001_003.sbx','darkness_PRE_V2.mat',psfilename);
-sizeSelect2pSession('G62QQ2LT_001_004.sbx','sizeSession_PRE_V2.mat',psfilename);
-sizeSelect2pSession('G62QQ2LT_001_005.sbx','sizeSession_POST_V2.mat',psfilename);
-topo2pSession('G62QQ2LT_001_006.sbx','darkness_POST_V2.mat',psfilename);
+% %%%0030917
+% topo2pSession('G62QQ2LT_001_001.sbx','topoXsession_V2.mat',psfilename);
+% topo2pSession('G62QQ2LT_001_002.sbx','topoYsession_V2.mat',psfilename);
+% topo2pSession('G62QQ2LT_001_003.sbx','darkness_PRE_V2.mat',psfilename);
+% sizeSelect2pSession('G62QQ2LT_001_004.sbx','sizeSession_PRE_V2.mat',psfilename);
+% sizeSelect2pSession('G62QQ2LT_001_005.sbx','sizeSession_POST_V2.mat',psfilename);
+% topo2pSession('G62QQ2LT_001_006.sbx','darkness_POST_V2.mat',psfilename);
 
 % %%%0031417
 % topo2pSession('G62BB10LT_001_001.sbx','topoXsession_V2.mat',psfilename);

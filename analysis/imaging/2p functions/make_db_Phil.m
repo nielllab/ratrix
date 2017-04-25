225101962a9e1f7b1851480d9f66b0dac7ae924c
Joze-Monster <niell.lab.orders@gmail.com>
2017-04-24 20:28:22 -0700

    joe update S2P batch
    joe

--- analysis/imaging/2p functions/make_db_Phil.m
+++ analysis/imaging/2p functions/make_db_Phil.m
@@ -36,13 +36,21 @@ i = 0;
 % db(i).expts         = [1 2 3 4 5 6];
 % db(i).diameter      = 14;
 
-i = i+1;
-db(i).mouse_name    = 'G62GG10LT';
-db(i).date          = 'S2P';
-db(i).expts         = [1 2 3 4 5 6];
-db(i).diameter      = 14;
-db(i).RootStorage   = '\\langevin\backup\twophoton\Phil\Compiled2p\022117 G62GG10LT DOI sizeselect22min';
-db(i).ResultsSavePath = '\\langevin\backup\twophoton\Phil\Compiled2p\022117 G62GG10LT DOI sizeselect22min';
+% i = i+1;
+% db(i).mouse_name    = 'G62BB6RT';
+% db(i).date          = 'S2P';
+% db(i).expts         = [1 2 3 4 5 6];
+% db(i).diameter      = 14;
+% db(i).RootStorage   = '\\langevin\backup\twophoton\Phil\Compiled2p\022017 G62BB6RT saline sizeselect22min';
+% db(i).ResultsSavePath = '\\langevin\backup\twophoton\Phil\Compiled2p\022017 G62BB6RT saline sizeselect22min';
+
+% i = i+1;
+% db(i).mouse_name    = 'G62GG10LT';
+% db(i).date          = 'S2P';
+% db(i).expts         = [1 2 3 4 5 6];
+% db(i).diameter      = 14;
+% db(i).RootStorage   = '\\langevin\backup\twophoton\Phil\Compiled2p\022117 G62GG10LT DOI sizeselect22min';
+% db(i).ResultsSavePath = '\\langevin\backup\twophoton\Phil\Compiled2p\022117 G62GG10LT DOI sizeselect22min';
 
 % i = i+1;
 % db(i).mouse_name    = 'G62BB6RT';
@@ -60,6 +68,14 @@ db(i).ResultsSavePath = '\\langevin\backup\twophoton\Phil\Compiled2p\022117 G62G
 % db(i).diameter      = 14;
 % db(i).RootStorage   = '\\langevin\backup\twophoton\Phil\Compiled2p\022717 G62Y3LT DOI sizeselect22min';
 % db(i).ResultsSavePath = '\\langevin\backup\twophoton\Phil\Compiled2p\022717 G62Y3LT DOI sizeselect22min';
+
+i = i+1;
+db(i).mouse_name    = 'G62BB10LT';
+db(i).date          = 'S2P';
+db(i).expts         = [1 2 3 4 5 6];
+db(i).diameter      = 14;
+db(i).RootStorage   = '\\langevin\backup\twophoton\Phil\Compiled2p\031417 G62BB10LT saline sizeselect22min';
+db(i).ResultsSavePath = '\\langevin\backup\twophoton\Phil\Compiled2p\031417 G62BB10LT saline sizeselect22min';
 
 % i = i+1;
 % db(i).mouse_name    = 'G62GG10LT';

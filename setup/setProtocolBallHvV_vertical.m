8da5e94cfa1d68b1b319e26cb561ebecab5b28e2
nielllab - ryans comp <niell.lab.orders@gmail.com>
2017-04-25 11:57:59 -0700

    Behavior
    Ryan

--- setup/setProtocolBallHvV_vertical.m
+++ setup/setProtocolBallHvV_vertical.m
@@ -47,7 +47,7 @@ end
        msPenalty               =3500;
        percentCorrectionTrials = .5;
        pixPerCycs              = [200];
-       normalizedPosition      = [0.46 0.54];
+       normalizedPosition      = [0.44 0.56];
        radius                 = .35;         
 
    case 'g62aaa4tt' % Started 3/29/17 %%hvv start 4/8/17 %%full 4/24/17
@@ -56,7 +56,7 @@ end
        msPenalty               =3500;
        percentCorrectionTrials = .5;
        pixPerCycs              = [200];
-       normalizedPosition      = [0.46 0.54];
+       normalizedPosition      = [0.44 0.56];
        radius                 = .35;  
 
    case 'g62ggg6tt' % Started 3/29/17 %%hvv start 4/8/17 %%full 4/24/17
@@ -65,8 +65,8 @@ end
        msPenalty               =3500;
        percentCorrectionTrials = .5;
        pixPerCycs              = [200];
-       normalizedPosition      = [0.46 0.54];
-       radius                 = .33;         
+       normalizedPosition      = [0.44 0.56];
+       radius                 = .32;         
        
    case 'g62ss4lt' % Started 4/07/17  %%hvv center 4/15/17 %%full 4/24/17
        requestRewardSizeULorMS = 0;
@@ -74,7 +74,7 @@ end
        msPenalty               =3500;
        percentCorrectionTrials = .5;
        pixPerCycs              = [200];
-       normalizedPosition      = [0.46 0.54];
+       normalizedPosition      = [0.44 0.56];
        radius                 = .35;  
      
    case 'g62uu1tt' % Started 2/1/17 %%switch 2/8/17
@@ -83,7 +83,7 @@ end
        msPenalty               =3500;
        percentCorrectionTrials = .6;
        pixPerCycs              = [200];
-       normalizedPosition      = [0.37 0.63];
+       normalizedPosition      = [0.35 0.65];
        radius                 = .25;  
        
     case 'g62mm11lt' % Started 2/1/17 %%switch 2/8/17 %%%switch full 2/27/17

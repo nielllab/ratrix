439d63010a9b9298e8382fb1644ac1ffc2738237
nielllab - ryans comp <niell.lab.orders@gmail.com>
2017-05-02 10:01:38 -0700

    test
    test

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -56,7 +56,7 @@ switch subjIDs{1}
          requestRewardSizeULorMS = 0;
          rewardSizeULorMS        = 219;
          msPenalty               =3500;
-         percentCorrectionTrials = .5;
+         percentCorrectionTrials = .5; 
     
          case 'pvchr4b14rt'  %started 4/26/17
          requestRewardSizeULorMS = 0;

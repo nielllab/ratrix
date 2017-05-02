8493534437aa9fc724f5e194719d4555c23a15f7
nielllab - ryans comp <niell.lab.orders@gmail.com>
2017-05-02 15:14:09 -0700

    Behavior
    Ryan

--- setup/setProtocolBallHvV_vertical.m
+++ setup/setProtocolBallHvV_vertical.m
@@ -47,7 +47,7 @@ end
        msPenalty               =3500;
        percentCorrectionTrials = .5;       
        pixPerCycs              = [200];
-       normalizedPosition      = [0.44 0.56];
+       normalizedPosition      = [0.42 0.58];
        radius                 = .33;  
        
    case 'g62ww3rt' % Started 4/05/17 %%hvv start 4/13/17 %%full 4/24/17
@@ -57,7 +57,7 @@ end
        percentCorrectionTrials = .5;
        pixPerCycs              = [200];
        normalizedPosition      = [0.33 0.66];
-       radius                 = .35;         
+       radius                 = .34;         
 
    case 'g62aaa4tt' % Started 3/29/17 %%hvv start 4/8/17 %%full 4/24/17
        requestRewardSizeULorMS = 0;
@@ -84,7 +84,7 @@ end
        percentCorrectionTrials = .5;
        pixPerCycs              = [200];
        normalizedPosition      = [0.33 0.66];
-       radius                 = .34;  
+       radius                 = .33;  
      
    case 'g62uu1tt' % Started 2/1/17 %%switch 2/8/17
        requestRewardSizeULorMS = 0;

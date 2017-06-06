5259cd947aeac39754f47489026b9f73344f6607
nielllab - ryans comp <niell.lab.orders@gmail.com>
2017-06-06 14:21:15 -0700

    Behavior
    Ryan

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -58,11 +58,11 @@ switch subjIDs{1}
 %          msPenalty               =3500;
 %          percentCorrectionTrials = .5; 
          
-%          case 'pvchr4b14lt'  %started 4/27/17
-%          requestRewardSizeULorMS = 0;
-%          rewardSizeULorMS        = 219;
-%          msPenalty               =3500;
-%          percentCorrectionTrials = .5;     
+         case 'pvchr4b14lt'  %started 4/27/17
+         requestRewardSizeULorMS = 0;
+         rewardSizeULorMS        = 219;
+         msPenalty               =3500;
+         percentCorrectionTrials = .5;     
     
 %          case 'g62aaa11rt'  %started 4/25/17
 %          requestRewardSizeULorMS = 0;

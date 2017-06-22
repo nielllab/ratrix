931c853c94936aff878fe1472449a12d9e7604b5
nielllab - ryans comp <niell.lab.orders@gmail.com>
2017-06-22 14:22:07 -0700

    Behavior
    Ryan

--- setup/setProtocolBallHvV_vertical.m
+++ setup/setProtocolBallHvV_vertical.m
@@ -56,7 +56,7 @@ end
          msPenalty               =3500;
          percentCorrectionTrials = .5;
          pixPerCycs              = [200];
-         normalizedPosition      = [0.38 0.62];
+         normalizedPosition      = [0.36 0.64];
          radius                 = .35; 
        
    case 'g62aaa11lt'  %started 4/25/17 %%start center 5/4/17 %%%full 5/24/17

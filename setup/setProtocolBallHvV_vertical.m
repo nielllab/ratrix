c9cb538b92e7b110f03f3c0b71e5d6d7275e6f19
nielllab - ryans comp <niell.lab.orders@gmail.com>
2017-07-04 10:11:11 -0700

    Behavior
    Ryan

--- setup/setProtocolBallHvV_vertical.m
+++ setup/setProtocolBallHvV_vertical.m
@@ -57,7 +57,7 @@ end
          percentCorrectionTrials = .5;
          pixPerCycs              = [200];
          normalizedPosition      = [0.33 0.66];
-         radius                 = .31; 
+         radius                 = .30; 
        
    case 'g62aaa11lt'  %started 4/25/17 %%start center 5/4/17 %%%full 5/24/17
          requestRewardSizeULorMS = 0;
@@ -65,7 +65,7 @@ end
          msPenalty               =3500;
          percentCorrectionTrials = .5;         
          normalizedPosition      = [0.33 0.66];
-         radius                 = .33;        
+         radius                 = .32;        
        
    case 'g62aaa10tt' % Started 4/07/17 %%start hvv center 4/15/17 %%%full 5/23/17
        requestRewardSizeULorMS = 0;
@@ -81,7 +81,7 @@ end
        msPenalty               =3500;
        percentCorrectionTrials = .5;          
        pixPerCycs              = [200];
-       normalizedPosition      = [0.44 0.56];
+       normalizedPosition      = [0.42 0.58];
        radius                 = .35;        
 
     case 'g62ddd3tt' % Started 4/05/17 %%hvv start 4/14/17 %%%full 5/17/17
@@ -133,7 +133,7 @@ end
        msPenalty               =3500;
        percentCorrectionTrials = .5;
        pixPerCycs              = [200];
-       normalizedPosition      = [0.42 0.58];
+       normalizedPosition      = [0.40 0.60];
        radius                 = .29;         
        
    case 'g62ss4lt' % Started 4/07/17  %%hvv center 4/15/17 %%full 4/24/17
@@ -152,7 +152,7 @@ end
        percentCorrectionTrials = .6;
        pixPerCycs              = [200];
        normalizedPosition      = [0.33 0.66];
-       radius                 = .31;  
+       radius                 = .30;  
        
     case 'g62mm11lt' % Started 2/1/17 %%switch 2/8/17 %%%switch full 2/27/17
        requestRewardSizeULorMS = 0;

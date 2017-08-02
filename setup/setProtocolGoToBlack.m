3faddf560643296ee8c7085f5295879ae28ebb4a
nielllab - ryans comp <niell.lab.orders@gmail.com>
2017-08-02 16:47:00 -0700

    Behavior
    Ryan

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -28,37 +28,37 @@ stim.gain = 0.7 * ones(2,1);
 switch subjIDs{1}
 
          case 'j351tt'  %started 7/26/17
-         requestRewardSizeULorMS = 43;
+         requestRewardSizeULorMS = 0;
          rewardSizeULorMS        = 219;
          msPenalty               =3500;
          percentCorrectionTrials = .5;  
          
          case 'j351rt'  %started 7/26/17
-         requestRewardSizeULorMS = 43;
+         requestRewardSizeULorMS = 0;
          rewardSizeULorMS        = 219;
          msPenalty               =3500;
          percentCorrectionTrials = .5;   
          
          case 'j351lt'  %started 7/26/17
-         requestRewardSizeULorMS = 43;
+         requestRewardSizeULorMS = 0;
          rewardSizeULorMS        = 219;
          msPenalty               =3500;
          percentCorrectionTrials = .5;      
         
          case 'j352tt'  %started 7/26/17
-         requestRewardSizeULorMS = 43;
+         requestRewardSizeULorMS = 0;
          rewardSizeULorMS        = 219;
          msPenalty               =3500;
          percentCorrectionTrials = .5;      
         
          case 'j352rt'  %started 7/26/17
-         requestRewardSizeULorMS = 43;
+         requestRewardSizeULorMS = 0;
          rewardSizeULorMS        = 219;
          msPenalty               =3500;
          percentCorrectionTrials = .5;      
     
          case 'j352lt'  %started 7/26/17
-         requestRewardSizeULorMS = 43;
+         requestRewardSizeULorMS = 0;
          rewardSizeULorMS        = 219;
          msPenalty               =3500;
          percentCorrectionTrials = .5;  

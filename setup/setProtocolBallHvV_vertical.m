206797255b9e3b43b6b6b58e282b964b32ad18d5
Joze-Monster <niell.lab.orders@gmail.com>
2017-08-12 16:36:20 -0700

    zz 9 and ddd3
    joe

--- setup/setProtocolBallHvV_vertical.m
+++ setup/setProtocolBallHvV_vertical.m
@@ -56,7 +56,7 @@ end
          msPenalty               =3500;
          percentCorrectionTrials = .5;
          pixPerCycs              = [200];
-         normalizedPosition      = [0.38 0.62];
+         normalizedPosition      = [0.36 0.64];
          radius                 = .33; 
        
    case 'g62aaa11lt'  %started 4/25/17 %%start center 5/4/17 %%%full 5/24/17
@@ -90,7 +90,7 @@ end
        msPenalty               =3500;
        percentCorrectionTrials = .5;
        normalizedPosition      = [0.33 0.66];
-       radius                 = .31;        
+       radius                 = .30;        
        
    case 'g62aaa11rt'  %started 4/25/17 %%hvv center 5/10/17 %%%full 5/17/17
          requestRewardSizeULorMS = 0;

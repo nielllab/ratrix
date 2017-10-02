2fb4735d08647e8af2a2379b12d1d82d7840d205
nielllab - ryans comp <niell.lab.orders@gmail.com>
2017-10-02 10:02:39 -0700

    Aldis
    remove stop rewards

--- setup/setProtocolGoToBlack.m
+++ setup/setProtocolGoToBlack.m
@@ -29,25 +29,25 @@ switch subjIDs{1}
 
         
     case 'pvarch515nt'  %started 9/27/17
-         requestRewardSizeULorMS = 75;
+         requestRewardSizeULorMS = 0;
          rewardSizeULorMS        = 219;
          msPenalty               =3500;
          percentCorrectionTrials = .5;  
     
     case 'pvarch515tt'  %started 9/27/17
-         requestRewardSizeULorMS = 75;
+         requestRewardSizeULorMS = 0;
          rewardSizeULorMS        = 219;
          msPenalty               =3500;
          percentCorrectionTrials = .5;  
     
     case 'pvarch515rt'  %started 9/27/17
-         requestRewardSizeULorMS = 75;
+         requestRewardSizeULorMS = 0;
          rewardSizeULorMS        = 219;
          msPenalty               =3500;
          percentCorrectionTrials = .5;  
     
     case 'pvarch515lt'  %started 9/27/17
-         requestRewardSizeULorMS = 75;
+         requestRewardSizeULorMS = 0;
          rewardSizeULorMS        = 219;
          msPenalty               =3500;
          percentCorrectionTrials = .5;  

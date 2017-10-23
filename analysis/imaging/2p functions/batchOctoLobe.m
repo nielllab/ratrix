d2a2722559ee89513dcc29efa4ec3e966613c63f
Cris <crisniell@gmail.com>
2017-10-23 11:31:07 -0700

    octo-batch

---
+++ analysis/imaging/2p functions/batchOctoLobe.m
@@ -0,0 +1,15 @@
+dbstop if error
+pathname = '\\herzog\data';
+n = 0;
+
+
+n=n+1;
+files(n).expt = '102017';
+files(n).dye = 'cal520';
+files(n).path = '102017_cal520';
+files(n).stim = 'gratingsLoSF';
+files(n).location = 1;
+files(n).tif = 'Loc1_acq1_GratingsLowSF_T90m_102017003.tif';
+files(n).ttl = 'Loc1_acq1_GratingsLowSF_T90m_102017003-20171020T152326.mat';
+files(n).stimrec = 'Loc1_acq1_102017';
+files(n).notes = 'good loading and activity';

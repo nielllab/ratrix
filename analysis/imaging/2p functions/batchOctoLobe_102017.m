783bc13e4b390337a126bee2508db666a9f60bda
Gilliam <niell.lab.orders@gmail.com>
2017-10-24 11:10:51 -0700

    Batch file added
    102017 octo data

---
+++ analysis/imaging/2p functions/batchOctoLobe_102017.m
@@ -0,0 +1,149 @@
+dbstop if error
+pathname = '\\gilliam\D\Octo_Data\';
+n = 0;
+
+
+n=n+1;
+files(n).expt = '102017';
+files(n).dye = 'Cal520';
+files(n).path = '102017_Octopus_Cal520';
+files(n).stim = 'GratingsLowSF';
+files(n).location = 1;
+files(n).acqnum = 1;
+files(n).tif = 'Loc1_acq1_GratingsLowSF_T90m_102017003.tif';
+files(n).ttl = 'Loc1_acq1_GratingsLowSF_T90m_102017003-20171020T152326.mat';
+files(n).stimrec = 'Loc1_acq1_102017.mat';
+files(n).notes = 'good loading and activity';
+
+n=n+1;
+files(n).expt = '102017';
+files(n).dye = 'Cal520';
+files(n).path = '102017_Octopus_Cal520';
+files(n).stim = '6x4blocks';
+files(n).location = 1;
+files(n).acqnum = 2;
+files(n).tif = 'Loc1_acq2_6x4blocks_T90m_102017004.tif';
+files(n).ttl = 'Loc1_acq2_6x4blocks_T90m_102017005-20171020T154154.mat';
+files(n).stimrec = 'Loc1_acq2_102017.mat';
+files(n).notes = 'good loading and activity';
+
+n=n+1;
+files(n).expt = '102017';
+files(n).dye = 'Cal520';
+files(n).path = '102017_Octopus_Cal520';
+files(n).stim = '3x2blocks';
+files(n).location = 1;
+files(n).acqnum = 3;
+files(n).tif = 'Loc1_acq3_3x2blocks_T90m_102017005.tif';
+files(n).ttl = 'Loc1_acq3_3x2blocks_T90m_102017005-20171020T154423.mat';
+files(n).stimrec = 'Loc1_acq3_102017.mat';
+files(n).notes = 'good loading and activity';
+
+n=n+1;
+files(n).expt = '102017';
+files(n).dye = 'Cal520';
+files(n).path = '102017_Octopus_Cal520';
+files(n).stim = 'GratingsLowSF';
+files(n).location = 2;
+files(n).acqnum = 1;
+files(n).tif = 'Loc2_acq1_GratingsLowSF_upinYbackinX001.tif';
+files(n).ttl = 'Loc2_acq1_GratingsLowSF_upinYbackinX002-20171020T161025.mat';
+files(n).stimrec = 'Loc2_acq1_102017.mat';
+files(n).notes = 'good loading and activity';
+
+n=n+1;
+files(n).expt = '102017';
+files(n).dye = 'Cal520';
+files(n).path = '102017_Octopus_Cal520';
+files(n).stim = '6x4blocks';
+files(n).location = 2;
+files(n).acqnum = 2;
+files(n).tif = 'Loc2_acq2_6x4blocks_upinYbackinX002.tif';
+files(n).ttl = 'Loc2_acq2_6x4blocks_upinYbackinX003-20171020T161940.mat';
+files(n).stimrec = 'Loc2_acq2_102017.mat';
+files(n).notes = 'good loading and activity';
+
+n=n+1;
+files(n).expt = '102017';
+files(n).dye = 'Cal520';
+files(n).path = '102017_Octopus_Cal520';
+files(n).stim = '3x2blocks';
+files(n).location = 2;
+files(n).acqnum = 3;
+files(n).tif = 'Loc2_acq3_3x2blocks_upinYbackinX003.tif';
+files(n).ttl = 'Loc2_acq3_3x2blocks_upinYbackinX004-20171020T162857.mat';
+files(n).stimrec = 'Loc1_acq3_102017.mat';
+files(n).notes = 'good loading and activity';
+
+n=n+1;
+files(n).expt = '102017';
+files(n).dye = 'Cal520';
+files(n).path = '102017_Octopus_Cal520';
+files(n).stim = 'GratingsLowSF';
+files(n).location = 3;
+files(n).acqnum = 1;
+files(n).tif = 'Loc3_acq1_GratingsLowSF_upinYbackinXdownZ004.tif';
+files(n).ttl = 'Loc3_acq1_GratingsLowSF_upinYbackinXdownZ005-20171020T164048.mat';
+files(n).stimrec = 'Loc3_acq1_102017.mat';
+files(n).notes = 'good loading and activity';
+
+n=n+1;
+files(n).expt = '102017';
+files(n).dye = 'Cal520';
+files(n).path = '102017_Octopus_Cal520';
+files(n).stim = '6x4blocks';
+files(n).location = 3;
+files(n).acqnum = 2;
+files(n).tif = 'Loc3_acq2_6x4blocks_upinYbackinXdownZ005.tif';
+files(n).ttl = 'Loc3_acq2_6x4blocks_upinYbackinXdownZ006-20171020T165021.mat';
+files(n).stimrec = 'Loc3_acq2_102017.mat';
+files(n).notes = 'good loading and activity';
+
+n=n+1;
+files(n).expt = '102017';
+files(n).dye = 'Cal520';
+files(n).path = '102017_Octopus_Cal520';
+files(n).stim = '3x2blocks';
+files(n).location = 3;
+files(n).acqnum = 3;
+files(n).tif = 'Loc3_acq3_3x2blocks_upinYbackinXdownZ006.tif';
+files(n).ttl = 'Loc3_acq3_3x2blocks_upinYbackinXdownZ007-20171020T165956.mat';
+files(n).stimrec = 'Loc3_acq3_102017.mat';
+files(n).notes = 'good loading and activity';
+
+n=n+1;
+files(n).expt = '102017';
+files(n).dye = 'Cal520';
+files(n).path = '102017_Octopus_Cal520';
+files(n).stim = 'GratingsLowSF';
+files(n).location = 4;
+files(n).acqnum = 1;
+files(n).tif = 'Loc4_acq1_GratingsLowSF_upinYbackinXdownZ007.tif';
+files(n).ttl = 'Loc4_acq1_GratingsLowSF_upinYbackinXdownZ008-20171020T171516.mat';
+files(n).stimrec = 'Loc4_acq1_102017.mat';
+files(n).notes = 'good loading but activity looks odd';
+
+n=n+1;
+files(n).expt = '102017';
+files(n).dye = 'Cal520';
+files(n).path = '102017_Octopus_Cal520';
+files(n).stim = '6x4blocks';
+files(n).location = 4;
+files(n).acqnum = 2;
+files(n).tif = 'Loc4_acq2_6x4blocks_downinYbackinXdownZ008.tif';
+files(n).ttl = 'Loc4_acq2_6x4blocks_downinYbackinXdownZ009-20171020T172449.mat';
+files(n).stimrec = 'Loc4_acq2_102017.mat';
+files(n).notes = 'good loading but activity looks odd';
+
+n=n+1;
+files(n).expt = '102017';
+files(n).dye = 'Cal520';
+files(n).path = '102017_Octopus_Cal520';
+files(n).stim = '3x2blocks';
+files(n).location = 4;
+files(n).acqnum = 3;
+files(n).tif = 'Loc4_acq3_3x2blocks_downinYbackinXdownZ009.tif';
+files(n).ttl = 'Loc4_acq3_3x2blocks_downinYbackinXdownZ010-20171020T173358.mat';
+files(n).stimrec = 'Loc4_acq3_102017.mat';
+files(n).notes = 'good loading but activity looks odd';
+

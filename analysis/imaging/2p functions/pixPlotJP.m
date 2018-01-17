996863d285544d59dfad3e33c459b566f0baf1cb
Gilliam <niell.lab.orders@gmail.com>
2018-01-17 15:53:41 -0800

    JP merge
    merging Cris and David's to be functional

---
+++ analysis/imaging/2p functions/pixPlotJP.m
@@ -0,0 +1,16 @@
+
+pixFig = figure; set(gcf,'Name',figLabel);
+traceFig = figure; set(gcf,'Name',figLabel);
+for i = (1:npanel)+offset
+    meanimg = nanmedian(trialmean(:,:,stimOrder==i),3);
+    figure(pixFig); subplot(nrow,ncol,loc(i-offset));
+    imagesc(meanimg,range); axis equal; axis off; colormap jet
+    figure(traceFig); subplot(nrow,ncol,loc(i-offset));
+    plot(1:8, trialTcourse(:,stimOrder==i)); hold on; plot(nanmedian(trialTcourse(:,stimOrder==i),2),'g','Linewidth',2); xlim([1 8]);ylim(range)
+    
+end
+
+figure(pixFig);
+% colorbar;
+if exist('psfile','var'); set(gcf, 'PaperPositionMode', 'auto'); print('-dpsc',psfile,'-append'); end
+figure(traceFig); if exist('psfile','var'); set(gcf, 'PaperPositionMode', 'auto'); print('-dpsc',psfile,'-append'); end

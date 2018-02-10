9aba33e37f52561bf4a70b12561c06ffe3692209
Maxwell <niell.lab.orders@gmail.com>
2018-02-09 16:27:01 -0800

    last sync on maxwell

--- analysis/imaging/batchDfofMovieBlind.m
+++ analysis/imaging/batchDfofMovieBlind.m
@@ -8,26 +8,40 @@ for f = 1:length(files)
     f
     tic
     
-    if isfield(files(f),'rignum') && strcmp(files(f).rignum,'rig2')
-        rig =2;
-    else
-        rig=1;
-    end  
+%     if isfield(files(f),'rignum') && strcmp(files(f).rignum,'rig2')
+%         rig =2;
+%     else
+%         rig=1;
+%     end  
+%     
+%     if redo || isempty([pathname files(f).n5sec_16mw]) || ~exist([pathname files(f).n5sec_16mw],'file')
+%         try
+%             dfofMovie_Blue([datapathname files(f).n5sec_16mwdata],rig);
+%         catch exc
+%             sprintf('couldnt do %s',files(f).n5sec_16mwdata)
+%             nerr=nerr+1;
+%             errmsg{nerr}=sprintf('couldnt do %s',files(f).n5sec_16mwdata)
+%             errRpt{nerr}=getReport(exc,'extended')
+%         end
+%     else
+%         sprintf('skipping %s',files(f).n5sec_16mwdata)
+%     end   
     
-       if redo || isempty([pathname files(f).n5sec_16mw]) || ~exist([pathname files(f).n5sec_16mw],'file')
+    
+    if redo || isempty([pathname files(f).n5sec_3mw]) || ~exist([pathname files(f).n5sec_3mw],'file')
         try
-            dfofMovie_Blue([datapathname files(f).n5sec_16mwdata],rig);
+            dfofMovie_Blue([datapathname files(f).n5sec_3mwdata],rig);
         catch exc
-            sprintf('couldnt do %s',files(f).n5sec_16mwdata)
+            sprintf('couldnt do %s',files(f).n5sec_3mwdata)
             nerr=nerr+1;
-            errmsg{nerr}=sprintf('couldnt do %s',files(f).n5sec_16mwdata)
+            errmsg{nerr}=sprintf('couldnt do %s',files(f).n5sec_3mwdata)
             errRpt{nerr}=getReport(exc,'extended')
         end
     else
-        sprintf('skipping %s',files(f).n5sec_16mwdata)
+        sprintf('skipping %s',files(f).n5sec_3mwdata)
     end   
-
-
+    
+    
     if redo || isempty([pathname files(f).n5sec_750uw]) || ~exist([pathname files(f).n5sec_750uw],'file')
         try
             dfofMovie_Blue([datapathname files(f).n5sec_750uwdata],rig);
@@ -55,23 +69,36 @@ for f = 1:length(files)
         sprintf('skipping %s',files(f).n5sec_375uwdata)
     end
     
+%     
+%     if redo || isempty([pathname files(f).n1sec_16mw]) || ~exist([pathname files(f).n1sec_16mw],'file')
+%         try
+%             dfofMovie_Blue([datapathname files(f).n1sec_16mwdata],rig);
+%         catch exc
+%             sprintf('couldnt do %s',files(f).n1sec_16mwdata)
+%             nerr=nerr+1;
+%             errmsg{nerr}=sprintf('couldnt do %s',files(f).n1sec_16mwdata)
+%             errRpt{nerr}=getReport(exc,'extended')
+%         end
+%     else
+%         sprintf('skipping %s',files(f).n1sec_16mwdata)
+%     end 
     
-    if redo || isempty([pathname files(f).n1sec_16mw]) || ~exist([pathname files(f).n1sec_16mw],'file')
+    
+    if redo || isempty([pathname files(f).n1sec_3mw]) || ~exist([pathname files(f).n1sec_3mw],'file')
         try
-            dfofMovie_Blue([datapathname files(f).n1sec_16mwdata],rig);
+            dfofMovie_Blue([datapathname files(f).n1sec_3mwdata],rig);
         catch exc
-            sprintf('couldnt do %s',files(f).n1sec_16mwdata)
+            sprintf('couldnt do %s',files(f).n1sec_3mwdata)
             nerr=nerr+1;
-            errmsg{nerr}=sprintf('couldnt do %s',files(f).n1sec_16mwdata)
+            errmsg{nerr}=sprintf('couldnt do %s',files(f).n1sec_3mwdata)
             errRpt{nerr}=getReport(exc,'extended')
         end
     else
-        sprintf('skipping %s',files(f).n1sec_16mwdata)
-    end 
+        sprintf('skipping %s',files(f).n1sec_3mwdata)
+    end   
     
     
-    
-if redo | isempty([pathname files(f).n1sec_750uw]) | ~exist([pathname files(f).n1sec_750uw],'file')
+    if redo | isempty([pathname files(f).n1sec_750uw]) | ~exist([pathname files(f).n1sec_750uw],'file')
         try
             dfofMovie_Blue([datapathname files(f).n1sec_750uwdata],rig);
         catch exc
@@ -86,7 +113,7 @@ if redo | isempty([pathname files(f).n1sec_750uw]) | ~exist([pathname files(f).n
     
     
     
-if redo | isempty([pathname files(f).n1sec_375uw]) | ~exist([pathname files(f).n1sec_375uw],'file')
+     if redo | isempty([pathname files(f).n1sec_375uw]) | ~exist([pathname files(f).n1sec_375uw],'file')
         try
             dfofMovie_Blue([datapathname files(f).n1sec_375uwdata],rig);
         catch exc
@@ -100,19 +127,32 @@ if redo | isempty([pathname files(f).n1sec_375uw]) | ~exist([pathname files(f).n
     end
     
     
-    if redo | isempty([pathname files(f).n100msec_16mw]) | ~exist([pathname files(f).n100msec_16mw],'file')
+%     if redo | isempty([pathname files(f).n100msec_16mw]) | ~exist([pathname files(f).n100msec_16mw],'file')
+%         try
+%             dfofMovie_Blue([datapathname files(f).n100msec_16mwdata],rig);
+%         catch exc
+%             sprintf('couldnt do %s',files(f).n100msec_16mwdata)
+%             nerr=nerr+1;
+%             errmsg{nerr}=sprintf('couldnt do %s',files(f).n100msec_16mwdata)
+%             errRpt{nerr}=getReport(exc,'extended')
+%         end
+%     else
+%         sprintf('skipping %s',files(f).n100msec_16mwdata)
+%     end
+%     
+    
+    if redo || isempty([pathname files(f).n100msec_3mw]) || ~exist([pathname files(f).n100msec_3mw],'file')
         try
-            dfofMovie_Blue([datapathname files(f).n100msec_16mwdata],rig);
+            dfofMovie_Blue([datapathname files(f).n100msec_3mwdata],rig);
         catch exc
-            sprintf('couldnt do %s',files(f).n100msec_16mwdata)
+            sprintf('couldnt do %s',files(f).n100msec_3mwdata)
             nerr=nerr+1;
-            errmsg{nerr}=sprintf('couldnt do %s',files(f).n100msec_16mwdata)
+            errmsg{nerr}=sprintf('couldnt do %s',files(f).n100msec_3mwdata)
             errRpt{nerr}=getReport(exc,'extended')
         end
     else
-        sprintf('skipping %s',files(f).n100msec_16mwdata)
+        sprintf('skipping %s',files(f).n100msec_3mwdata)
     end
-    
     
     
     if redo | isempty([pathname files(f).n100msec_750uw]) | ~exist([pathname files(f).n100msec_750uw],'file')

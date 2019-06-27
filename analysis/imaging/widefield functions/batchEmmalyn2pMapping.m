cd1b6e0998a87a486107075e119a6b66f9b45f37
KCsPC <niell.lab.orders@gmail.com>
2019-06-27 14:50:47 -0700

    Create batchEmmalyn2pMapping.m
    Emmalyn

---
+++ analysis/imaging/widefield functions/batchEmmalyn2pMapping.m
@@ -0,0 +1,21 @@
+%% batchEmmalyn2pMapping
+
+dbstop if error
+pathname = 'E:\Emmalyn\2pMapping\';
+datapathname = 'E:\Emmalyn\2pMapping\';  
+outpathname = 'E:\Emmalyn\2pMapping\';
+n = 0;
+
+%%
+n=n+1;
+files(n).subj = 'G6H19p3TT';
+files(n).expt = '062719';
+files(n).topox =  '062719_G6H19p3TT_2PMAP_RIG2\062719_G6H19p3TT_2PMAP_RIG2_TOPOX\062719_G6H19p3TT_2PMAP_RIG2_TOPOXmaps.mat';
+files(n).topoxdata = '062719_G6H19p3TT_2PMAP_RIG2\062719_G6H19p3TT_2PMAP_RIG2_TOPOX\062719_G6H19p3TT_2PMAP_RIG2_TOPOX';
+files(n).topoy =  '062719_G6H19p3TT_2PMAP_RIG2\062719_G6H19p3TT_2PMAP_RIG2_TOPOY\062719_G6H19p3TT_2PMAP_RIG2_TOPOYmaps.mat';
+files(n).topoydata = '062719_G6H19p3TT_2PMAP_RIG2\062719_G6H19p3TT_2PMAP_RIG2_TOPOY\062719_G6H19p3TT_2PMAP_RIG2_TOPOY';
+files(n).rignum = 'rig2'; %%% or 'rig1'
+files(n).monitor = 'land'; %%% for topox and y
+files(n).label = 'camk2 gc6'; %%% or 'calb2'
+
+%%

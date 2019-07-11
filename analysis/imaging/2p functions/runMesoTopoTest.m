609742ad98f7f54d4d64e4f6ec3f6261bada2d9c
sendak <niell.lab.orders@gmail.com>
2019-07-10 17:03:08 -0700

    updated topo analysis from tif
    Phil, for mesoscope experiments

---
+++ analysis/imaging/2p functions/runMesoTopoTest.m
@@ -0,0 +1,31 @@
+%% script to run topo test for mesoscope
+close all; clear all;dbstop if error;warning off
+
+[fileName,pathname] = uigetfile('*.tif','topo file');
+cd(pathname)
+% visStimFile = uigetfile('*.mat','stimulus file');
+
+disp(fileName)
+% disp(visStimFile)
+
+%% get the first frame
+
+%DO WHATEVER ON VISSTIMFILE TO GET FIRST FRAME AND IF YOU WANT FRAME TIMES
+% first_frame = 470 %do this here
+% last_frame = first_frame + 2150;
+
+first_frame = 666 %do this here
+last_frame = first_frame + 1885;
+
+%% get the acquisition rate (resample if you want)
+
+acqrate = 10 %set this here;
+
+%% run the code
+map = {};
+map{1} = topo2pTestTif(fileName,acqrate,first_frame,last_frame);
+
+save([fileName(1:end-4) '_map.mat'],'map')
+frm = imread(fileName, 1);
+imwrite(frm,[fileName(1:end-4) '_frame.tif'])
+
--- analysis/imaging/2p functions/topo2pTestTif.m
+++ analysis/imaging/2p functions/topo2pTestTif.m
@@ -1,22 +1,15 @@
-%% topo2pTestTif
+function [outmap] = topo2pTestTif(fileName,acqrate,first_frame,last_frame)
 % this script runs a quick test for topox or topoy to get cycavg and
 % fourier map for data in tif format
 
-close all; clear all;dbstop if error;warning off
-
-framerate = 10 %imaging rate
-spatialBin = 4; %spatial downsample factor (map will look cleaner)
-
-fileName = uigetfile('*.tif','topo file');
-
-first_frame = 1; %feed this in as an argument? how does evan get this?
+spatialBin = 1; %spatial downsample factor (map will look cleaner)
 
 tiff_info = imfinfo(fileName); % return tiff structure, one element per image
 data = imread(fileName, first_frame) ; % read in first image
 %concatenate each successive tiff to tiff_stack
 disp('reading tif file, please wait...')
 tic
-for ii = first_frame+1:size(tiff_info, 1)
+for ii = first_frame+1:last_frame
     temp_tiff = imread(fileName, ii);
     data = cat(3 , data, temp_tiff);
 end
@@ -35,7 +28,7 @@ disp('finished resizing')
 
 %%% get the median image to calculate dfof
 medimg = double(median(data(:,:,10:10:end),3)); % was 10th percentile, changed to median to avoid zeros
-figure;imshow(medimg)
+figure;imagesc(medimg)
 axis off
 axis image
 title('median image')
@@ -51,7 +44,7 @@ toc
 disp('finished dfof')
 
 %%% generate pixel-wise fourier map
-dt = 1/framerate; %%% resampled time frame
+dt = 1/acqrate; %%% resampled time frame
 cycLength=10; %length of movie cycle in seconds (should be 10)
 cycLength = cycLength/dt; %length of movie cycle in frames
 map = 0;
@@ -59,6 +52,7 @@ for i= 1:size(dfof,3);
     map = map+dfof(:,:,i)*exp(2*pi*sqrt(-1)*i/cycLength);
 end
 map = map/size(dfof,3); map(isnan(map))=0;
+outmap = map;
 amp = abs(map); amp=amp/prctile(amp(:),98); amp(amp>1)=1;
 img = mat2im(mod(angle(map),2*pi),hsv,[pi/2  (2*pi -pi/4)]);
 img = img.*repmat(amp,[1 1 3]);

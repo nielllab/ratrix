ee05431c132e32d961c5e48990b892d921c085ca
crisniell <crisniell@gmail.com>
2020-08-06 17:45:15 -0700

    Update compileOctoRepeats.m

--- analysis/imaging/2p functions/compileOctoRepeats.m
+++ analysis/imaging/2p functions/compileOctoRepeats.m
@@ -1,3 +1,7 @@
+%%% load multiple sessions and compare mean responses
+%%% ideal for comparing effect of varying a parameter
+%%% cmn 2020
+
 clear all
 
 done = 0;
@@ -7,6 +11,8 @@ n=0;  %%% #number of files loaded
 psfile = 'C:\temp\TempFigs.ps';
 if exist(psfile,'file')==2;delete(psfile);end
 
+
+%%% load sessions
 while ~done
     
     %%% get filename
@@ -36,6 +42,8 @@ while ~done
             dF(:,n) = squeeze(median(nanmedian(dFrepeats,3),1));
             for i = 1:nstim
                 tc((i-1)*30 + (1:30),n) = nanmedian(trialTcourse(:,stimOrder==i),2);
+                pixResp(i,n) = nanmean(tc((i-1)*30 + (1:30),n),1);
+                cellResp(i,n) = nanmean(dF((i-1)*stimDur + (1:stimDur),n),1);
             end
             labels{n} = input('data label : ','s');
         end
@@ -72,6 +80,25 @@ set(gca,'XtickLabel',labels); ylabel('mean dF/F')
 title('average over all stim, cells');
 if exist('psfile','var'); set(gcf, 'PaperPositionMode', 'auto'); print('-dpsc',psfile,'-append'); end
 
+%%% analyze 6x4 spots - separate into on and off and only include 
+%%% top 3 stim conditions (to avoid stim locations outside visual field)
+if  nstim == 48;
+    for i = 1:n
+        off = sort(cellResp(1:24,i),'descend');
+        meanCellResp(1,i) = nanmean(off(1:3));
+        on = sort(cellResp(25:48,i),'descend');
+        meanCellResp(2,i) = nanmean(on(1:3));
+    end
+    figure
+    bar(meanCellResp');
+    set(gca,'XtickLabels',labels);
+    legend({'off','on'}); title('mean cell responses (top 3 stim)')
+    xlabel('condition'); ylabel('df/f')
+    if exist('psfile','var'); set(gcf, 'PaperPositionMode', 'auto'); print('-dpsc',psfile,'-append'); end
+
+end
+
+
 %%% plot pixel averages
 figure; hold on
 for i = 1:n
@@ -89,6 +116,24 @@ bar(mean(tc,1))
 set(gca,'XtickLabel',labels); ylabel('mean dF/F')
 title('average over all stim, pixels');
 if exist('psfile','var'); set(gcf, 'PaperPositionMode', 'auto'); print('-dpsc',psfile,'-append'); end
+
+%%% analyze 6x4 spots - separate into on and off and only include 
+%%% top 3 stim conditions (to avoid stim locations outside visual field)
+if  nstim == 48;
+    for i = 1:n
+        off = sort(pixResp(1:24,i),'descend');
+        meanPixResp(1,i) = nanmean(off(1:3));
+        on = sort(pixResp(25:48,i),'descend');
+        meanPixResp(2,i) = nanmean(on(1:3));
+    end
+    figure
+    bar(meanPixResp');
+    set(gca,'XtickLabels',labels);
+    legend({'off','on'}); title('mean pixel responses (top 3 stim)')
+    xlabel('condition'); ylabel('df/f')
+   if exist('psfile','var'); set(gcf, 'PaperPositionMode', 'auto'); print('-dpsc',psfile,'-append'); end
+ 
+end
 
 
 %%% save pdf

08b6845e4efd6a74442334e97356705672ebf145
Joze-Monster <niell.lab.orders@gmail.com>
2022-05-17 17:09:00 -0700

    KC updates

--- analysis/KC/group_analyses_code/plotCompareBehState_mnTrace.m
+++ analysis/KC/group_analyses_code/plotCompareBehState_mnTrace.m
@@ -104,6 +104,10 @@ for d = durat
 %                 
 %             yt = [-0.01 0 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1];
 %             set(gca,'YTick',yt)
+
+                ax = gca; % axes handle
+                ax.YAxis.Exponent = 0;
+
             
             legend(stateLegend)
             

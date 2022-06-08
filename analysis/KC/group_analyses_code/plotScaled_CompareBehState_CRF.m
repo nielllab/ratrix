fbd3a4e287ebbd2eeaaa79221db8daf0617ce2a4
Joze-Monster <niell.lab.orders@gmail.com>
2022-06-08 13:50:06 -0700

    KC updates

---
+++ analysis/KC/group_analyses_code/plotScaled_CompareBehState_CRF.m
@@ -0,0 +1,104 @@
+function plotScaled_CompareBehState_CRF(loBehState_allSessAllPtsAllDurs_CRF,hiBehState_allSessAllPtsAllDurs_CRF,durat,yMax,yMin,yLimit,x_axis,xMax,xMin,xLimit,stateLegend,uniqueContrasts,reigons,nGroup)
+
+figure
+%clear titleText
+%titleText = 'CRF: locomotion';
+%suptitle(sprintf('%s \n',titleText))
+
+%titleText = 'CRF: '; % making char variables for sprintf/title later
+%subjName = groupSubjName{1}; % will be same name if w/in mouse analysis...
+%date = groupDate{1};
+%suptitle(sprintf('%s',titleText,subjName)); % date, 
+
+for d = durat
+    
+    global visArea
+    for i = 1:4
+        
+        subplot(2,2,i) 
+        
+        % take mean across sessions, for ith point, lo beh state
+        loBehState_meanDthCRFacrossSess = mean(loBehState_allSessAllPtsAllDurs_CRF(d,:,i,:),4);
+        
+        % max of dth ith CRF
+        maxOfCRF = max(loBehState_meanDthCRFacrossSess);
+        
+        % scale the CRF to it's max
+        scaled_loBehState_meanDthCRFacrossSess = loBehState_meanDthCRFacrossSess/maxOfCRF;
+        
+        % get stdev across sessions
+        %loBehState_stdErrOverSess = std(squeeze(loBehState_allSessAllPtsAllDurs_CRF(d,:,i,:))')/sqrt(size(loBehState_allSessAllPtsAllDurs_CRF,4));
+        % stderr across mice
+        global numMice
+        %loBehState_stdErrOverSess = std(squeeze(loBehState_allSessAllPtsAllDurs_CRF(d,:,i,:))')/sqrt(numMice); 
+        %loBehState_stdErrOverSess = std(squeeze(loBehState_allSessAllPtsAllDurs_CRF(d,:,i,:))'); 
+        
+        plot(x_axis,scaled_loBehState_meanDthCRFacrossSess,'-b','lineWidth',1,'MarkerSize',3) 
+        
+%         if nGroup == 1
+%             
+%             plot(x_axis,loBehState_meanDthCRFacrossSess,'-b','lineWidth',1,'MarkerSize',3) 
+%         
+%         else 
+%             
+%             % plot for lo beh state
+%             errorbar(x_axis,loBehState_meanDthCRFacrossSess,loBehState_stdErrOverSess,'-b','lineWidth',1,'MarkerSize',3) 
+%         
+%         end 
+        
+        hold on 
+        
+        % take mean across sessions, for ith point, hi beh state
+        hiBehState_meanDthCRFacrossSess = mean(hiBehState_allSessAllPtsAllDurs_CRF(d,:,i,:),4);
+
+        % max of dth ith CRF
+        maxOfCRF = max(hiBehState_meanDthCRFacrossSess);
+        
+        % scale the CRF to it's max
+        scaled_hiBehState_meanDthCRFacrossSess = hiBehState_meanDthCRFacrossSess/maxOfCRF;
+        
+%         % get stderr across sessions
+%         %hiBehState_stdErrOverSess = std(squeeze(hiBehState_allSessAllPtsAllDurs_CRF(d,:,i,:))')/sqrt(size(hiBehState_allSessAllPtsAllDurs_CRF,4)); 
+%         % stderr across mice
+%         global numMice
+%         hiBehState_stdErrOverSess = std(squeeze(hiBehState_allSessAllPtsAllDurs_CRF(d,:,i,:))')/sqrt(numMice); 
+          
+        plot(x_axis,scaled_hiBehState_meanDthCRFacrossSess,'-r','lineWidth',1,'MarkerSize',3) 
+%         
+%           if nGroup == 1
+%             
+%             plot(x_axis,hiBehState_meanDthCRFacrossSess,'-r','lineWidth',1,'MarkerSize',3) 
+%             
+%         
+%         else 
+%             
+%             % plot for hi beh state
+%             errorbar(x_axis,hiBehState_meanDthCRFacrossSess,hiBehState_stdErrOverSess,'-r','lineWidth',1,'MarkerSize',3) 
+%             
+%         
+%         end
+        
+        grid on
+        
+        title(reigons{i})
+    
+        ylim(yLimit) 
+        xlim(xLimit)
+    
+        ylabel('df/f')
+        xlabel('contrast (%)')
+    
+        clear xt
+        xt={'0'; '3' ; '6' ; '12' ; '25' ; '50'; '100'} ; 
+        set(gca,'xtick',1:7); 
+        set(gca,'xticklabel',xt);
+        
+%         yt = [-0.01 0 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1];
+%         set(gca,'YTick',yt)
+        
+        legend(stateLegend,'Location','northwest')
+
+    end 
+
+end
+

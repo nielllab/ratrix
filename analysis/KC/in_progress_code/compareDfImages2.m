da82c49489b8b3f887e06ccb6f8dc4f92d6310ab
Joze-Monster <niell.lab.orders@gmail.com>
2022-06-22 13:27:22 -0700

    kc

--- analysis/KC/in_progress_code/compareDfImages2.m
+++ analysis/KC/in_progress_code/compareDfImages2.m
@@ -1,4 +1,4 @@
-function [tifPic] = compareDfImages(meanpolar,ypts,xpts,baselinedActIm,df,range)
+function [tifPic] = compareDfImages2(meanpolar,ypts,xpts,baselinedActIm,range)
 % UNTITLED2 Summary of this function goes here
 % Detailed explanation goes here
 
@@ -13,7 +13,7 @@ function [tifPic] = compareDfImages(meanpolar,ypts,xpts,baselinedActIm,df,range)
     tifPic = flip(tifPic,2);
     
     % plot
-    subplot(2,2,1)
+    subplot(1,3,1)
     imagesc(tifPic)
     %colormap copper
     
@@ -23,7 +23,7 @@ function [tifPic] = compareDfImages(meanpolar,ypts,xpts,baselinedActIm,df,range)
     
     % plot TOPO MAP w/AREA BOUNDARIES
     % meanpolar made in do_topography
-    subplot(2,2,2)
+    subplot(1,3,2)
     imagesc(polarMap(meanpolar{2},95))
     
     hold on
@@ -34,25 +34,25 @@ function [tifPic] = compareDfImages(meanpolar,ypts,xpts,baselinedActIm,df,range)
     set(gca,'Visible','off') 
     
     
-    % plot UnCroped Df
-    % std of df gives us a general sense of how much variation there is across al trials
-    % we use image to crop in next section
-    stdMap = std(df(:,:,1:10:end),[],3); % only take every 10 frames of df (don't need all, why?). taking std across 3rd dimension, time
-    
-    subplot(2,2,3)
-    imagesc(stdMap,range) 
-    
-    colormap jet
-    
-    axis equal
-    set(gca,'Visible','off') 
+%     % plot UnCroped Df
+%     % std of df gives us a general sense of how much variation there is across al trials
+%     % we use image to crop in next section
+%     stdMap = std(df(:,:,1:10:end),[],3); % only take every 10 frames of df (don't need all, why?). taking std across 3rd dimension, time
+%     
+%     subplot(1,2,3)
+%     imagesc(stdMap,range) 
+%     
+%     colormap jet
+%     
+%     axis equal
+%     set(gca,'Visible','off') 
     
     
     
     % plot IMAGE used for FOR PICKING POINTS
     % the function pickPeakActIm4Points() generates an output var called
     % baselinedActIm, which is needed for the figure to pick 5 points
-    subplot(2,2,4)
+    subplot(1,3,3)
     imagesc(baselinedActIm,range)
     
     colormap jet
@@ -103,7 +103,7 @@ function [tifPic] = compareDfImages(meanpolar,ypts,xpts,baselinedActIm,df,range)
     % print size of images
     sizeTifPic = size(tifPic)
     sizeTopoIm = size(polarMap(meanpolar{2},95))
-    sizeDfUnCropped = size(df) 
+    %sizeDfUnCropped = size(df) 
     sizePeakActIm = size(baselinedActIm)
     
      

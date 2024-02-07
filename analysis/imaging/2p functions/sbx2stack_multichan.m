45b24178c0b35b40fd9f330dd93b43eced5ad637
crisniell <crisniell@gmail.com>
2024-02-06 20:58:23 -0800

    Update sbx2stack_multichan.m

--- analysis/imaging/2p functions/sbx2stack_multichan.m
+++ analysis/imaging/2p functions/sbx2stack_multichan.m
@@ -1,12 +1,16 @@
 function sbx2stack_multichan(fname)
-%%% reads in a z-stack from scanbox and outputs a multi-channel tif stack
+%%% reads in a z-stack from scanbox and outputs tif stacks for each channel
 %%% automatically detects # of z planes and channels
 %%% averages multiple sections per plane
 %%% example usage sbxstack_multichan('stack_000_test')
+%%% outputs a separate grayscale tif stack for each channel
+%%% cmn 02-2024
+
+%%% notes from old version
 %%% output tif can be opened in FIJI using Import -> BioFormat
 %%% for 1-channel image, output is grayscale stack
 %%% for 2-channel image, output is an RGB stack (with empty 3rd color -> a result of Matlab imwrite constraints)
-%%% cmn 02-2024
+
 
 %%% get info from first image
 z = sbxread(fname,1,1);
@@ -27,15 +31,18 @@ for p = 1:nplane
     
     q = sbxread(fname,sched(p)+1,nsection);  %%% read in all sections at one plane
     mn = median(q,4);   %%% take median average of sections
-    if nch==2
-        mn(3,:,:)=0;    %%%  if two-color, need to add 3rd channel (for tif format)
-        mn = shiftdim(mn,1);  %%% shift color to 3rd dimension
-    end
+    %     if nch==2
+    %         mn(3,:,:)=0;    %%%  if two-color, need to add 3rd channel (for tif format)
+    %         mn = shiftdim(mn,1);  %%% shift color to 3rd dimension
+    %     end
     
     %%% save data into tif!
-    if(p==1)
-        imwrite(mn,[fname '.tif'],'tif');
-    else
-        imwrite(mn,[fname '.tif'],'tif','writemode','append');
+    for chan = 1:nch
+        if(p==1)
+            imwrite(squeeze(mn(chan,:,:)),[fname '_' num2str(chan) '.tif'],'tif');
+        else
+            imwrite(squeeze(mn(chan,:,:)),[fname '_' num2str(chan) '.tif'],'tif','writemode','append');
+        end
     end
+    
 end
